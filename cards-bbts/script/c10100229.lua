5ac2a0dc1b535245b663dc43f46a98e6480d8035
utso <cccygodev@gmail.com>
2018-07-11 16:06:54 +0200

    Add script and test for Bohrok Swarm Fusion (including summoning conditions for one of the Kaita).

---
+++ cards-bbts/script/c10100229.lua
@@ -0,0 +1,7 @@
+--Bohrok Kaita Ja
+function c10100229.initial_effect(c)
+	--Fusion Material
+	aux.AddFusionProcCode3(c,10100202,10100205,10100206,true,true)
+	c:EnableReviveLimit()
+end
+
---
+++ cards-bbts/tests/c10100229.lua
@@ -0,0 +1,7 @@
+--Bohrok Kaita Ja
+Debug.SetAIName("Unit Test")
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+Debug.AddCard(10100229,0,0,LOCATION_EXTRA,2,POS_FACEDOWN_ATTACK)
+Debug.ReloadFieldEnd()
