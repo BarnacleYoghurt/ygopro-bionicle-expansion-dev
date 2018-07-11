2bbfa0b8f08e3556376f322c903bafc07adca133
utso <cccygodev@gmail.com>
2018-07-11 16:06:54 +0200

    Add script and test for Bohrok Swarm Fusion (including summoning conditions for one of the Kaita).

---
+++ cards-bbts/script/c10100227.lua
@@ -0,0 +1,92 @@
+--Bohrok Swarm Fusion
+function c10100227.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c10100227.target1)
+	e1:SetOperation(c10100227.operation1)
+	c:RegisterEffect(e1)
+end
+function c10100227.filter1a(c,e)
+	return not c:IsImmuneToEffect(e)
+end
+function c10100227.filter1b(c,e,tp,m,f,chkf)
+	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x15c) and (not f or f(c))
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
+end
+function c10100227.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local chkf=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and PLAYER_NONE or tp
+		local mg1=Duel.GetFusionMaterial(tp)
+		local res=Duel.IsExistingMatchingCard(c10100227.filter1b,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
+		if not res then
+			local ce=Duel.GetChainMaterial(tp)
+			if ce~=nil then
+				local fgroup=ce:GetTarget()
+				local mg2=fgroup(ce,e,tp)
+				local mf=ce:GetValue()
+				res=Duel.IsExistingMatchingCard(c10100227.filter1b,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,chkf)
+			end
+		end
+		return res
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c10100227.operation1(e,tp,eg,ep,ev,re,r,rp)
+	local chkf=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and PLAYER_NONE or tp
+	local mg1=Duel.GetFusionMaterial(tp):Filter(c10100227.filter1a,nil,e)
+	local sg1=Duel.GetMatchingGroup(c10100227.filter1b,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
+	local mg2=nil
+	local sg2=nil
+	local ce=Duel.GetChainMaterial(tp)
+	if ce~=nil then
+		local fgroup=ce:GetTarget()
+		mg2=fgroup(ce,e,tp)
+		local mf=ce:GetValue()
+		sg2=Duel.GetMatchingGroup(c10100227.filter1b,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
+	end
+	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
+		local sg=sg1:Clone()
+		if sg2 then sg:Merge(sg2) end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local tg=sg:Select(tp,1,1,nil)
+		local tc=tg:GetFirst()
+		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
+			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
+			tc:SetMaterial(mat1)
+			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+			Duel.BreakEffect()
+			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+		else
+			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
+			local fop=ce:GetOperation()
+			fop(ce,e,tp,tc,mat2)
+		end
+		tc:CompleteProcedure()
+	end
+	--End Phase effect
+	local e1_1=Effect.CreateEffect(e:GetHandler())
+	e1_1:SetCategory(CATEGORY_TOHAND)
+	e1_1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1_1:SetRange(LOCATION_GRAVE)
+	e1_1:SetCode(EVENT_PHASE+PHASE_END)
+	e1_1:SetReset(RESET_PHASE+PHASE_END)
+	e1_1:SetCountLimit(1)
+	e1_1:SetTarget(c10100227.target1_1)
+	e1_1:SetOperation(c10100227.operation1_1)
+	e:GetHandler():RegisterEffect(e1_1)
+end
+function c10100227.filter1_1(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x15c) and c:IsAbleToHand()
+end
+function c10100227.target1_1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c10100227.filter1_1,tp,LOCATION_GRAVE,0,1,nil) end
+end
+function c10100227.operation1_1(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.SelectMatchingCard(tp,c10100227.filter1_1,tp,LOCATION_GRAVE,0,1,1,nil)
+	if g:GetCount() > 0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+	end
+end
