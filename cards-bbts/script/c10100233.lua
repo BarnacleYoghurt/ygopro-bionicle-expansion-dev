383f75dda291845398ef011fc06781b4b06eff67
utso <cccygodev@gmail.com>
2018-07-21 17:59:07 +0200

    Update Bahrag to new version and add Bahrag spell.

--- cards-bbts/script/c10100233.lua
+++ cards-bbts/script/c10100233.lua
@@ -3,6 +3,7 @@ if not bbts then
 end
 --Bahrag Cahdok, Queen of the Bohrok
 function c10100233.initial_effect(c)
+	aux.EnablePendulumAttribute(c)
 	--synchro summon
 	aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x15c),aux.NonTuner(Card.IsSetCard,0x15c),1)
 	c:EnableReviveLimit()
@@ -11,7 +12,8 @@ function c10100233.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetCountLimit(1)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,10100233)
 	e1:SetTarget(c10100233.target1)
 	e1:SetOperation(c10100233.operation1)
 	c:RegisterEffect(e1)
@@ -45,19 +47,20 @@ function c10100233.filter1(c)
 	return c:IsSetCard(0x15c) and c:IsFaceup() and c:IsCanTurnSet()
 end
 function c10100233.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100233.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(c10100233.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.SelectTarget(tp,c10100233.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
 function c10100233.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c10100233.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	if g:GetCount()>0 then
-		Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.ChangePosition(tc,POS_FACEDOWN_DEFENSE)
 	end
 end
 function c10100233.filter2(c,e,tp)
 	return c:IsSetCard(0x15c) and c:GetLevel()==2 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c10100233.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==1
+	return not Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,e:GetHandler())
 end
 function c10100233.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c10100233.filter2,tp,LOCATION_GRAVE,0,1,nil,e,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
@@ -69,11 +72,35 @@ function c10100233.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,c10100233.filter2,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if g:GetCount() > 0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		local tc=g:GetFirst()
+		local fid=e:GetHandler():GetFieldID()
+		tc:RegisterFlagEffect(10100233,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1,fid)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetCountLimit(1)
+		e1:SetLabel(fid)
+		e1:SetLabelObject(tc)
+		e1:SetCondition(c10100233.condition2_1)
+		e1:SetOperation(c10100233.operation2_1)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
 	end
+end
+function c10100233.condition2_1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	return tc:GetFlagEffectLabel(10100233)==e:GetLabel()
+end
+function c10100233.operation2_1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
 end
 function c10100233.filter3(c)
 	return c:IsFaceup() and c:IsCode(10100232)
 end
 function c10100233.condition3(e,tp,eg,ep,ev,re,r,rp)
+	Debug.Message('check protect')
+	Debug.Message(Duel.IsExistingMatchingCard(c10100233.filter3,tp,LOCATION_ONFIELD,0,1,nil))
 	return Duel.IsExistingMatchingCard(c10100233.filter3,tp,LOCATION_ONFIELD,0,1,nil)
 end
