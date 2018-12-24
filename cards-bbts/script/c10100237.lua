a74e3d176d21e9f8a35bf256fa77d8e5a169229b
utso <cccygodev@gmail.com>
2018-12-24 11:42:58 +0100

    Add Waikiru

---
+++ cards-bbts/script/c10100237.lua
@@ -0,0 +1,110 @@
+--Waikiru, Walrus Rahi
+function c10100237.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCost(c10100237.condition1)
+	e1:SetTarget(c10100237.target1)
+	e1:SetOperation(c10100237.operation1)
+	c:RegisterEffect(e1)
+	--Change BP
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_POSITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetTarget(c10100237.target2)
+	e2:SetOperation(c10100237.operation2)
+	e2:SetCountLimit(2)
+	c:RegisterEffect(e2)
+	--Buffs
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_BE_MATERIAL)
+	e3:SetCondition(c10100237.condition3)
+	e3:SetOperation(c10100237.operation3)
+	c:RegisterEffect(e3)
+end
+function c10100237.condition1(e,tp,eg,ep,ev,re,r,rp,chk)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+end
+function c10100237.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,LOCATION_SZONE)
+end
+function c10100237.operation1(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsRelateToEffect(e) then
+		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c10100237.filter2(c)
+	return c:IsFaceup() and c:IsSetCard(0x15a) -- and c:IsCanChangePosition()
+end
+function c10100237.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(c10100237.filter2,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectTarget(tp,c10100237.filter2,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+end
+function c10100237.operation2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		--Faceup attack -> faceup defense, faceup defense or facedown (either position) -> faceup attack
+		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
+	end
+end
+function c10100237.condition3(e,tp,eg,ep,ev,re,r,rp)
+	return r==REASON_SYNCHRO
+end
+function c10100237.operation3(e,tp,eg,ep,ev,re,r,rp)
+	local c=re:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(c10100237.value3_1)
+	e1:SetCountLimit(1)
+	e1:SetReset(RESET_EVENT+0x1fe0000)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_CHANGE_POS)
+	e2:SetCondition(c10100237.condition3_2)
+	e2:SetTarget(c10100237.operation3_2)
+	e2:SetReset(RESET_EVENT+0x1fe0000)
+	c:RegisterEffect(e2)
+	if not c:IsType(TYPE_EFFECT) then
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_ADD_TYPE)
+		e3:SetValue(TYPE_EFFECT)
+		e3:SetReset(RESET_EVENT+0x1fe0000)
+		c:RegisterEffect(e3,true)
+	end
+end
+function c10100237.value3_1(e,re,r,rp)
+	return e:GetHandler():IsPosition(POS_FACEUP_DEFENSE) and (bit.band(r,REASON_BATTLE)~=0 or bit.band(r,REASON_EFFECT)~=0)
+end
+function c10100237.condition3_2(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPosition(POS_FACEUP_ATTACK)
+end
+function c10100237.operation3_2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(100*c:GetLevel())
+	e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e1)
+end
