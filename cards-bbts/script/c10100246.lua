515a795cec6d82ba3e28a41ce91f2e6af5d01dc7
utso <cccygodev@gmail.com>
2019-06-09 23:47:36 +0200

    Add Kotu

---
+++ cards-bbts/script/c10100246.lua
@@ -0,0 +1,41 @@
+--Matoran Tender Kotu
+function c10100246.initial_effect(c)
+  --ATK down
+  local e1=Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_FIELD)
+  e1:SetRange(LOCATION_MZONE)
+  e1:SetTargetRange(0,LOCATION_MZONE)
+  e1:SetCode(EFFECT_UPDATE_ATTACK)
+  e1:SetCondition(c10100246.condition1)
+  e1:SetValue(-800)
+  c:RegisterEffect(e1)
+  --To hand
+  local e2=Effect.CreateEffect(c)
+  e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+  e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+  e2:SetCode(EVENT_SUMMON_SUCCESS)
+  e2:SetTarget(c10100246.target2)
+  e2:SetOperation(c10100246.operation2)
+  e2:SetCountLimit(1,10100246)
+  c:RegisterEffect(e2)
+end
+function c10100246.condition1(e)
+  local ph=Duel.GetCurrentPhase()
+  return ph >= PHASE_BATTLE_START and ph <= PHASE_BATTLE
+end
+function c10100246.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then
+    return Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+  end
+  local tc=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+  Duel.SetOperationInfo(0,CATEGORY_TOHAND,tc,1,1-tp,nil)
+end
+function c10100246.operation2(e,tp,eg,ep,ev,re,r,rp)
+  local tc=Duel.GetFirstTarget()
+  if e:GetHandler():IsRelateToEffect(e) and tc:IsRelateToEffect(e) then
+    if Duel.SendtoHand(tc,nil,REASON_EFFECT) then
+      Duel.Draw(tc:GetOwner(),1,REASON_EFFECT)
+    end
+  end
+end
+
