e4eb46549a3c4194b150b8a153dc33c9df19852e
utso <cccygodev@gmail.com>
2019-06-11 13:01:14 +0200

    Add Nuparu, Empty Bohrok Shell, Boxor (first versions).

---
+++ cards-bbts/script/c10100247.lua
@@ -0,0 +1,56 @@
+--Matoran Engineer Nuparu
+function c10100247.initial_effect(c)
+  --Build things in a cave with a box of scraps
+  local e1=Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_IGNITION)
+  e1:SetRange(LOCATION_MZONE)
+  e1:SetCost(c10100247.cost1)
+  e1:SetTarget(c10100247.target1)
+  e1:SetOperation(c10100247.operation1)
+  e1:SetCountLimit(1)
+  c:RegisterEffect(e1)
+end
+function c10100247.filter1a(c)
+  return c:IsRace(RACE_MACHINE) and c:IsAbleToRemoveAsCost() and Duel.IsExistingMatchingCard(c10100247.filter1c,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,c)
+end
+function c10100247.filter1b(c,e,tp,rc)
+  return c:IsRace(RACE_MACHINE) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SPECIAL,tp,false,false) 
+    and 
+    (
+      (not rc and Duel.IsExistingMatchingCard(c10100247.filter1c,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,c)) --If no GY card selected yet, check if grave has any applicable
+      or 
+      (rc and c10100247.filter1c(c,rc)) --If GY card selected, check if that card in particular is applicable
+    )
+end
+function c10100247.filter1c(c,rc)
+  return not c:IsCode(rc:GetCode()) and c:GetLevel() == rc:GetLevel()
+end
+function c10100247.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then
+    return Duel.IsExistingMatchingCard(c10100247.filter1a, tp, LOCATION_GRAVE, LOCATION_GRAVE, 1, nil)
+  end
+  local g=Duel.SelectMatchingCard(tp,c10100247.filter1a, tp, LOCATION_GRAVE, LOCATION_GRAVE, 1, 1, nil)
+  e:SetLabelObject(g:GetFirst())
+  Duel.Remove(g, POS_FACEUP, REASON_COST)
+end
+function c10100247.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then
+    return Duel.IsExistingMatchingCard(c10100247.filter1b, tp, LOCATION_DECK+LOCATION_HAND, 0, 1, nil, e, tp, e:GetLabelObject())
+  end
+  local g=Duel.GetMatchingGroup(c10100247.filter1b, tp, LOCATION_DECK+LOCATION_HAND, 0, nil, e, tp, e:GetLabelObject())
+  Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON, g, 1, 0, 0)
+end
+function c10100247.operation1(e,tp,eg,ep,ev,re,r,rp)
+  local g = Duel.SelectMatchingCard(tp, c10100247.filter1b, tp, LOCATION_DECK+LOCATION_HAND, 0, 1, 1, nil, e, tp, e:GetLabelObject())
+  if g:GetCount() > 0 then
+    Duel.SpecialSummon(g,SUMMON_TYPE_SPECIAL,tp,tp,false,false,POS_FACEUP_DEFENSE)
+    --Can't attack
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_CANNOT_ATTACK)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(1)
+    e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+    e:GetHandler():RegisterEffect(e1)
+  end
+end
---
+++ cards-bbts/script/c10100248.lua
@@ -0,0 +1,61 @@
+--Empty Bohrok Shell
+function c10100248.initial_effect(c)
+  --Search
+  local e1=Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+  e1:SetCode(EVENT_PHASE+PHASE_END)
+  e1:SetRange(LOCATION_HAND)
+  e1:SetCondition(c10100248.condition1)
+  e1:SetCost(c10100248.cost1)
+  e1:SetTarget(c10100248.target1)
+  e1:SetOperation(c10100248.operation1)
+  e1:SetCountLimit(1)
+  c:RegisterEffect(e1)
+  --Destroy
+  local e2=Effect.CreateEffect(c)
+  e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+  e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+  e2:SetCode(EVENT_REMOVE)
+  e2:SetRange(LOCATION_GRAVE)
+  e2:SetTarget(c10100248.target2)
+  e2:SetOperation(c10100248.operation2)
+  c:RegisterEffect(e2)
+end
+function c10100248.filter1(c)
+  return c:IsLevelBelow(4) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAbleToHand()
+end
+function c10100248.condition1(e,tp,eg,ep,ev,re,r,rp)
+  return e:GetHandlerPlayer() == tp
+end
+function c10100248.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then
+    return e:GetHandler():IsAbleToGraveAsCost()
+  end
+  Duel.SendtoGrave(e:GetHandler(), REASON_COST)
+end
+function c10100248.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then
+    return Duel.IsExistingMatchingCard(c10100248.filter1,tp,LOCATION_DECK,0,1,nil)
+  end
+  local g=Duel.GetMatchingGroup(c10100248.filter1,tp,LOCATION_DECK,0,nil)
+  Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c10100248.operation1(e,tp,eg,ep,ev,re,r,rp)
+  local g=Duel.SelectMatchingCard(tp,c10100248.filter1,tp,LOCATION_DECK,0,1,1,nil)
+  if g:GetCount()>0 then
+    Duel.SendtoHand(g,nil,REASON_EFFECT)
+  end
+end
+function c10100248.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then
+    return Duel.IsExistingTarget(Card.IsDestructable,tp,0,LOCATION_ONFIELD,1,nil)
+  end
+  local tc=Duel.SelectTarget(tp,Card.IsDestructable,tp,0,LOCATION_ONFIELD,1,1,nil)
+  Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,0,0)
+end
+function c10100248.operation2(e,tp,eg,ep,ev,re,r,rp)
+  local tc=Duel.GetFirstTarget()
+  if e:GetHandler():IsRelateToEffect(e) and tc:IsRelateToEffect(e) then
+    Duel.Destroy(tc,REASON_EFFECT)
+  end
+end
