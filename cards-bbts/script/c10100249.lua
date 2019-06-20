46855a1a54baa4cf4db83c6602e4a0df92b1a600
utso <cccygodev@gmail.com>
2019-06-20 16:32:08 +0200

    Try next variation of Boxor stats. There's also a curious issue with optional flip effects bypassing the monster effect block in actual play, but that seems to be a YGOPro bug or maybe a strange detail of rulings.

--- cards-bbts/script/c10100249.lua
+++ cards-bbts/script/c10100249.lua
@@ -1,79 +1,70 @@
 --The Boxor, Weapon of the Matoran
 function c10100249.initial_effect(c)
-  --Can't attack
-  local e1=Effect.CreateEffect(c)
-  e1:SetType(EFFECT_TYPE_SINGLE)
-  e1:SetCode(EFFECT_CANNOT_ATTACK)
-  e1:SetCondition(c10100249.condition1)
-  c:RegisterEffect(e1)
   --equip
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(c10100249.target1)
+	e1:SetOperation(c10100249.operation1)
+	c:RegisterEffect(e1)
+	--unequip
 	local e2=Effect.CreateEffect(c)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCategory(CATEGORY_EQUIP)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_MZONE)
+	e2:SetRange(LOCATION_SZONE)
 	e2:SetTarget(c10100249.target2)
 	e2:SetOperation(c10100249.operation2)
 	c:RegisterEffect(e2)
-	--unequip
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetTarget(c10100249.target3)
-	e3:SetOperation(c10100249.operation3)
-	c:RegisterEffect(e3)
 	--destroy sub
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_EQUIP)
-	e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e4:SetCode(EFFECT_DESTROY_SUBSTITUTE)
-	e4:SetValue(c10100249.value4)
-	c:RegisterEffect(e4)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_EQUIP)
+	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetCode(EFFECT_DESTROY_SUBSTITUTE)
+	e3:SetValue(c10100249.value3)
+	c:RegisterEffect(e3)
   --set ATK
-  local e5=Effect.CreateEffect(c)
-  e5:SetType(EFFECT_TYPE_EQUIP)
-  e5:SetCode(EFFECT_SET_BASE_ATTACK)
-  e5:SetValue(1750)
-  c:RegisterEffect(e5)
+  local e4=Effect.CreateEffect(c)
+  e4:SetType(EFFECT_TYPE_EQUIP)
+  e4:SetCode(EFFECT_SET_BASE_ATTACK)
+  e4:SetValue(2000)
+  c:RegisterEffect(e4)
   --Block effects
-  local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e6:SetRange(LOCATION_SZONE)
-	e6:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e6:SetCondition(c10100249.condition6)
-	e6:SetOperation(c10100249.operation6)
-	c:RegisterEffect(e6)
+  local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e5:SetCondition(c10100249.condition5)
+	e5:SetOperation(c10100249.operation5)
+	c:RegisterEffect(e5)
 	--eqlimit
-	local e7=Effect.CreateEffect(c)
-	e7:SetType(EFFECT_TYPE_SINGLE)
-	e7:SetCode(EFFECT_EQUIP_LIMIT)
-	e7:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e7:SetValue(c10100249.value7)
-	c:RegisterEffect(e7)
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetCode(EFFECT_EQUIP_LIMIT)
+	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e6:SetValue(c10100249.value6)
+	c:RegisterEffect(e6)
 end
-function c10100249.condition1(e)
-  return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_MZONE,0)>1
-end
-function c10100249.filter2(c)
+function c10100249.filter1(c)
 	local ct1,ct2=c:GetUnionCount()
 	return c:IsFaceup() and c:IsRace(RACE_WARRIOR) and ct2==0
 end
-function c10100249.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+function c10100249.target1(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then 
-    return e:GetHandler():GetFlagEffect(10100249)==0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingTarget(c10100249.filter2,tp,LOCATION_MZONE,0,1,c)
+    return e:GetHandler():GetFlagEffect(10100249)==0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingTarget(c10100249.filter1,tp,LOCATION_MZONE,0,1,c)
   end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,c10100249.filter2,tp,LOCATION_MZONE,0,1,1,c)
+	local g=Duel.SelectTarget(tp,c10100249.filter1,tp,LOCATION_MZONE,0,1,1,c)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
 	c:RegisterFlagEffect(10100249,RESET_EVENT+0x7e0000+RESET_PHASE+PHASE_END,0,1)
 end
-function c10100249.operation2(e,tp,eg,ep,ev,re,r,rp)
+function c10100249.operation1(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
-	if not tc:IsRelateToEffect(e) or not c10100249.filter2(tc) then
+	if not tc:IsRelateToEffect(e) or not c10100249.filter1(tc) then
 		Duel.SendtoGrave(c,REASON_EFFECT)
 		return
 	end
@@ -81,7 +72,7 @@ function c10100249.operation2(e,tp,eg,ep,ev,re,r,rp)
     aux.SetUnionState(c)
   end
 end
-function c10100249.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+function c10100249.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then 
     return c:GetFlagEffect(10100249)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,false)
@@ -89,32 +80,32 @@ function c10100249.target3(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 	c:RegisterFlagEffect(10100249,RESET_EVENT+0x7e0000+RESET_PHASE+PHASE_END,0,1)
 end
-function c10100249.operation3(e,tp,eg,ep,ev,re,r,rp)
+function c10100249.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 and c:IsCanBeSpecialSummoned(e,0,tp,true,false) then
 		Duel.SendtoGrave(c,REASON_RULE)
 	end
 end
-function c10100249.value4(e,re,r,rp)
+function c10100249.value3(e,re,r,rp)
 	return bit.band(r,REASON_BATTLE)~=0 or bit.band(r,REASON_EFFECT)~=0
 end
-function c10100249.condition6(e)
+function c10100249.condition5(e)
 	return Duel.GetAttacker()==e:GetHandler():GetEquipTarget()
 end
-function c10100249.operation6(e)
+function c10100249.operation5(e)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e1:SetTargetRange(0,1)
-	e1:SetValue(c10100249.value6_1)
+	e1:SetValue(c10100249.value5_1)
 	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
 	Duel.RegisterEffect(e1,tp)
 end
-function c10100249.value6_1(e,re,tp)
+function c10100249.value5_1(e,re,tp)
 	return not re:GetHandler():IsImmuneToEffect(e) and re:IsActiveType(TYPE_MONSTER)
 end
-function c10100249.value7(e,c)
+function c10100249.value6(e,c)
 	return (c:IsRace(RACE_WARRIOR)) or e:GetHandler():GetEquipTarget()==c
 end
