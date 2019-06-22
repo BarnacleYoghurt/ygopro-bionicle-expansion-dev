eaac2472bbe25cfd0d70229fb61ecfc1f75085c0
utso <cccygodev@gmail.com>
2019-06-22 23:51:18 +0200

    Add Exo-Toa ATK boost effect.

--- cards-bbts/script/c10100250.lua
+++ cards-bbts/script/c10100250.lua
@@ -46,6 +46,11 @@ function c10100250.initial_effect(c)
   c:RegisterEffect(e6)
   --Boost
   local e7=Effect.CreateEffect(c)
+  e7:SetType(EFFECT_TYPE_EQUIP)
+  e7:SetCode(EFFECT_SET_ATTACK)
+  e7:SetCondition(c10100250.condition7)
+  e7:SetValue(c10100250.value7)
+  c:RegisterEffect(e7)
 	--eqlimit
 	local e8=Effect.CreateEffect(c)
 	e8:SetType(EFFECT_TYPE_SINGLE)
@@ -104,6 +109,22 @@ end
 function c10100250.condition4(e,c)
   if c==nil then return true end
 	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c10100250.filter4,c:GetControler(),LOCATION_MZONE,0,1,nil)
+end
+function c10100250.condition7(e)
+  local ec=e:GetHandler():GetEquipTarget()
+  local a=Duel.GetAttacker()
+  local d=Duel.GetAttackTarget()
+  return a and d 
+    and (
+      (ec==a and d:IsAttackAbove(a:GetAttack())) 
+      or 
+      (ec==d and a:IsAttackAbove(d:GetAttack()))
+    )
+end
+function c10100250.value7(e,c)
+  local oc=Duel.GetAttackTarget()
+  if oc==c then oc=Duel.GetAttacker() end
+  return oc:GetAttack()+1000
 end
 function c10100250.value8(e,c)
 	return (c:IsSetCard(0x155)) or e:GetHandler():GetEquipTarget()==c
