4397c0aeb62f208c896344c59b7e000a6de361f6
utso <cccygodev@gmail.com>
2020-06-20 21:02:17 +0200

    Begin implementing Ga-Koro

--- cards-bcot/script/c10100026.lua
+++ cards-bcot/script/c10100026.lua
@@ -1,74 +1,21 @@
 --Ga-Koro, Village of Water
-function c10100026.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e0=Effect.CreateEffect(c)
-	e0:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
-	e0:SetOperation(c10100026.operation0)
 	c:RegisterEffect(e0)
-	--1 less tribute for WATER
+	--Extra lock
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_DECREASE_TRIBUTE)
-	e1:SetRange(LOCATION_SZONE)
-	e1:SetTargetRange(LOCATION_HAND,LOCATION_HAND)
-	e1:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_WATER))
-	e1:SetValue(1)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.target1)
 	c:RegisterEffect(e1)
-	--Negate
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(10100026,0))
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
-	e2:SetCode(EVENT_CHAINING)
-	e2:SetHintTiming(TIMING_DAMAGE_STEP)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e2:SetCondition(c10100026.condition2)
-	e2:SetCost(c10100026.cost2)
-	e2:SetTarget(c10100026.target2)
-	e2:SetOperation(c10100026.operation2)
-	e2:SetCountLimit(1)
-	c:RegisterEffect(e2)
 end
---e0 - Activate
-function c10100026.filter0a(c)
-	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER)
-end
-function c10100026.filter0b(c)
-	return c:IsCode(10100002) and c:IsAbleToHand()
-end
-function c10100026.operation0(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tc=Duel.GetFirstMatchingCard(c10100026.filter0b,tp,LOCATION_DECK,0,nil)
-	if Duel.IsExistingMatchingCard(c10100026.filter0a,tp,LOCATION_MZONE,0,1,nil) and tc then
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,tc)
-	end
-end
---e2 - Negate
-function c10100026.filter2(c)
-	return c:IsType(TYPE_EQUIP) and c:IsSetCard(0x158) and c:IsAbleToRemoveAsCost()
-end
-function c10100026.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return re:GetHandlerPlayer()==1-tp
-end
-function c10100026.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100026.filter2,tp,LOCATION_GRAVE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c10100026.filter2,tp,LOCATION_GRAVE,0,1,1,nil)
-	Duel.Remove(g,POS_FACEUP,REASON_COST)
-end
-function c10100026.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
-	end
-end
-function c10100026.operation2(e,tp,eg,ep,ev,re,r,rp)
-	Duel.NegateActivation(ev)
-	if re:GetHandler():IsRelateToEffect(re) then
-		Duel.Destroy(eg,REASON_EFFECT)
-	end
+function s.target1(e,c,sump,sumtype,sumpos,targetp,se)
+	return c:IsLocation(LOCATION_EXTRA) and not (c:IsAttribute(ATTRIBUTE_WATER))
 end
