009e473979e54e934b25908a2506e87d67bbfaf9
utso <cccygodev@gmail.com>
2020-06-21 00:01:05 +0200

    Implement Ga-Koro response block

--- cards-bcot/script/c10100026.lua
+++ cards-bcot/script/c10100026.lua
@@ -15,7 +15,22 @@ function s.initial_effect(c)
 	e1:SetTargetRange(1,0)
 	e1:SetTarget(s.target1)
 	c:RegisterEffect(e1)
+  --Response block
+  local e2=Effect.CreateEffect(c)
+  e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+  e2:SetCode(EVENT_CHAINING)
+  e2:SetRange(LOCATION_FZONE)
+  e2:SetOperation(s.operation2)
+  c:RegisterEffect(e2)
 end
 function s.target1(e,c,sump,sumtype,sumpos,targetp,se)
 	return c:IsLocation(LOCATION_EXTRA) and not (c:IsAttribute(ATTRIBUTE_WATER))
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+  if re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsAttribute(ATTRIBUTE_WATER) and Duel.GetCurrentChain()>1 and Duel.GetTurnPlayer()~=tp and ep==tp then
+		Duel.SetChainLimit(s.limit2_1)
+	end
+end
+function s.limit2_1(e,rp,tp)
+	return tp==rp
 end
