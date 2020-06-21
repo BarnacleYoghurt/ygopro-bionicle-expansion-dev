4b4c59d44d8451f5e444767a4b73a84bc8a3a0b8
utso <cccygodev@gmail.com>
2020-06-21 12:34:11 +0200

    Implement Ga-Koro purity condition and special summon effect

--- cards-bcot/script/c10100026.lua
+++ cards-bcot/script/c10100026.lua
@@ -20,11 +20,26 @@ function s.initial_effect(c)
   e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
   e2:SetCode(EVENT_CHAINING)
   e2:SetRange(LOCATION_FZONE)
+  e2:SetCondition(s.condition2)
   e2:SetOperation(s.operation2)
   c:RegisterEffect(e2)
+  --Special Summon
+  local e3=Effect.CreateEffect(c)
+  e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e3:SetType(EFFECT_TYPE_IGNITION)
+  e3:SetRange(LOCATION_FZONE)
+  e3:SetCost(s.cost3)
+  e3:SetTarget(s.target3)
+  e3:SetOperation(s.operation3)
+  e3:SetCountLimit(1,id)
+  c:RegisterEffect(e3)
 end
 function s.target1(e,c,sump,sumtype,sumpos,targetp,se)
 	return c:IsLocation(LOCATION_EXTRA) and not (c:IsAttribute(ATTRIBUTE_WATER))
+end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+  local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_MONSTER)
+  return g:GetCount()>0 and g:GetCount()==g:FilterCount(Card.IsAttribute,nil,ATTRIBUTE_WATER)
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
   if re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsAttribute(ATTRIBUTE_WATER) and Duel.GetCurrentChain()>1 and Duel.GetTurnPlayer()~=tp and ep==tp then
@@ -33,4 +48,51 @@ function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.limit2_1(e,rp,tp)
 	return tp==rp
+end
+function s.filter3a(c)
+  return c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
+end
+function s.filter3b(c,e,tp)
+  return c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SPECIAL,tp,false,false,POS_FACEUP_DEFENSE)
+end
+function s.cost3(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingMatchingCard(s.filter3a,tp,LOCATION_GRAVE,0,1,nil) end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+  local g=Duel.SelectMatchingCard(tp,s.filter3a,tp,LOCATION_GRAVE,0,1,1,nil)
+  e:SetLabel(g:GetFirst():GetRace())
+  Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingMatchingCard(s.filter3b,tp,LOCATION_HAND,0,1,nil,e,tp) end
+  local g=Duel.GetMatchingGroup(s.filter3a,tp,LOCATION_HAND,0,nil)
+  Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
+  local c=e:GetHandler()
+  if c:IsRelateToEffect(e) then
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,s.filter3b,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+    if g:GetCount() > 0 then
+      local tc=g:GetFirst()
+      if Duel.SpecialSummonStep(tc,SUMMON_TYPE_SPECIAL,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        tc:RegisterEffect(e1,true)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_DISABLE_EFFECT)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+        tc:RegisterEffect(e2,true)
+        local e3=Effect.CreateEffect(c)
+        e3:SetType(EFFECT_TYPE_SINGLE)
+        e3:SetCode(EFFECT_CHANGE_RACE)
+        e3:SetValue(e:GetLabel())
+        e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+        tc:RegisterEffect(e3,true)
+      end
+      Duel.SpecialSummonComplete()
+    end
+  end
 end
