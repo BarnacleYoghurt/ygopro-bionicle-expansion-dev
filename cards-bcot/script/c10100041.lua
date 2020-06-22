7f878d4d4f7bbb1694c2411daa508df8895a3d0e
utso <cccygodev@gmail.com>
2020-06-22 18:35:58 +0200

    Implement Rau

--- cards-bcot/script/c10100041.lua
+++ cards-bcot/script/c10100041.lua
@@ -1,94 +1,58 @@
+if not bcot then
+	dofile "expansions/util-bcot.lua"
+end
 --Noble Kanohi Rau
-function c10100041.initial_effect(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_EQUIP)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetTarget(c10100041.target1)
-	e1:SetOperation(c10100041.operation1)
-	c:RegisterEffect(e1)
-	--Equip limit
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_EQUIP_LIMIT)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetValue(c10100041.condition2)
-	c:RegisterEffect(e2)
-	--Confirm
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(10100041,0))
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCountLimit(1)
-	e3:SetCondition(c10100041.condition3)
-	e3:SetOperation(c10100041.operation3)
-	c:RegisterEffect(e3)
-	--Swap
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(10100041,1))
-	e4:SetCategory(CATEGORY_EQUIP)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
-	e4:SetRange(LOCATION_SZONE)
-	e4:SetCode(EVENT_FREE_CHAIN)
-	e4:SetCost(c10100041.cost4)
-	e4:SetTarget(c10100041.target4)
-	e4:SetOperation(c10100041.operation4)
-	e4:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	c:RegisterEffect(e4)
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c)
+  --Destroy if replaced
+  local e1=bcot.kanohi_selfdestruct(c)
+  c:RegisterEffect(e1)
+  --Translate 
+	local e2a=Effect.CreateEffect(c)
+	e2a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2a:SetCode(EVENT_CHAINING)
+	e2a:SetRange(LOCATION_SZONE)
+	e2a:SetCountLimit(1)
+	e2a:SetCondition(s.condition2a)
+	e2a:SetOperation(s.operation2a)
+	c:RegisterEffect(e2a)
+	local e2b=Effect.CreateEffect(c)
+	e2b:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2b:SetCode(EVENT_CHAIN_SOLVING)
+	e2b:SetRange(LOCATION_SZONE)
+	e2b:SetCountLimit(1)
+  e2b:SetCondition(s.condition2b)
+	e2b:SetOperation(s.operation2b)
+	c:RegisterEffect(e2b)
+  --Recycle
+  local e3=bcot.kanohi_revive(c,10100018)
+  e3:SetDescription(aux.Stringid(id,0))
+  e3:SetCountLimit(1,id)
+  c:RegisterEffect(e3)
 end
---e1 - Activate
-function c10100041.filter1(c)
-	return c:IsFaceup() and (c:IsSetCard(0x155) or c:IsSetCard(0x156)) and not c:GetEquipGroup():IsExists(Card.IsSetCard,1,nil,0x158)
+function s.condition2a(e,tp,eg,ep,ev,re,r,rp)
+  local ec=e:GetHandler():GetEquipTarget()
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS):IsContains(ec)
 end
-function c10100041.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c10100041.filter1(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c10100041.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	Duel.SelectTarget(tp,c10100041.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+function s.operation2a(e,tp,eg,ep,ev,re,r,rp)
+	re:GetHandler():RegisterFlagEffect(id,RESET_CHAIN,0,1)
 end
-function c10100041.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		Duel.Equip(tp,c,tc)
-	end
+function s.condition2b(e,tp,eg,ep,ev,re,r,rp)
+	return re:GetHandler():GetFlagEffect(id)>0
 end
---e2 - Equip Limit
-function c10100041.condition2(e,c)
-	return c:IsSetCard(0x155) or c:IsSetCard(0x156)
+function s.operation2b(e,tp,eg,ep,ev,re,r,rp)
+	local g=Group.CreateGroup()
+	Duel.ChangeTargetCard(ev,g)
+	Duel.ChangeChainOperation(ev,s.operation2b_1)
 end
---e3 - Confirm
-function c10100041.condition3(e)
-	local tp=e:GetHandlerPlayer()
-	return Duel.GetFieldGroupCount(1-tp,LOCATION_HAND,0,nil)>Duel.GetFieldGroupCount(tp,LOCATION_HAND,0,nil)
-end
-function c10100041.operation3(e,tp,eg,ep,ev,re,r,rp)
-	Duel.ConfirmCards(tp, Duel.GetFieldGroup(1-tp,LOCATION_HAND,0,nil):RandomSelect(1-tp,1))
-end
---e4 - Swap
-function c10100041.filter4(c,ec)
-	return c:IsSetCard(0x158) and c:CheckEquipTarget(ec)
-end
-function c10100041.cost4(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckLPCost(tp,800) end
-	Duel.PayLPCost(tp,800)
-end
-function c10100041.target4(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100041.filter4,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,nil,e:GetHandler():GetEquipTarget()) end
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
-end
-function c10100041.operation4(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local ec=c:GetEquipTarget()
-	if Duel.SendtoGrave(c,REASON_EFFECT) then
-		Duel.BreakEffect()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-		local g=Duel.SelectMatchingCard(tp,c10100041.filter4,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,ec)
-		if g:GetCount()>0 then
-			Duel.Equip(tp,g:GetFirst(),ec)
-		end
-	end
+function s.operation2b_1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_MZONE,0,1,1,nil)
+  if g:GetCount()>0 then
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
+    local s=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0)
+    local nseq=math.log(s,2)
+    Duel.MoveSequence(g:GetFirst(),nseq)
+  end
 end
