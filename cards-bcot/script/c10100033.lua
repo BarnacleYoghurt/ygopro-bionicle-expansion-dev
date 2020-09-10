ff515d9df502ca1fc75fc01812675d5853cfdb35
utso <cccygodev@gmail.com>
2020-09-10 23:54:03 +0200

    Implement Taipu

--- cards-bcot/script/c10100033.lua
+++ cards-bcot/script/c10100033.lua
@@ -1,37 +1,56 @@
 --C.C. Matoran Taipu
-function c10100033.initial_effect(c)
-	--Raise ATK
-	local e1 = Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100033,0))
-	e1:SetCategory(CATEGORY_ATKCHANGE)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetRange(LOCATION_GRAVE)
-	e1:SetCost(c10100033.cost1)
-	e1:SetTarget(c10100033.target1)
-	e1:SetOperation(c10100033.operation1)
-	e1:SetCountLimit(1,10100033)
-	c:RegisterEffect(e1)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon
+  local e1=Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_FIELD)
+  e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+  e1:SetCode(EFFECT_SPSUMMON_PROC)
+  e1:SetRange(LOCATION_HAND)
+  e1:SetOperation(s.operation1)
+  e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+  c:RegisterEffect(e1)
+  --Raise ATK
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+  e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+  e2:SetCondition(s.condition2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+	c:RegisterEffect(e2)
 end
---e1 - Raise ATK
-function c10100033.filter1(c)
-	return c:IsFaceup() and c:IsRace(RACE_WARRIOR)
+function s.target1_1(e,c)
+  return not c:IsSetCard(0x1157)
 end
-function c10100033.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
-	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp,c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+  e1:SetTarget(s.target1_1)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
 end
-function c10100033.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100033.filter1,tp,LOCATION_MZONE,0,1,nil) end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+  return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsFaceup() and chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c10100033.filter1,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function c10100033.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(1000)
-	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 	tc:RegisterEffect(e1)
+  local e1b=e1:Clone()
+  e1b:SetCode(EFFECT_UPDATE_DEFENSE)
+  tc:RegisterEffect(e1b)
 end
