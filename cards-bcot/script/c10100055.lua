d36495546948ad029e9f1cef688ae487288cd517
utso <cccygodev@gmail.com>
2020-11-30 12:44:30 +0100

    Suva: Implement multi-attribute effect

--- cards-bcot/script/c10100055.lua
+++ cards-bcot/script/c10100055.lua
@@ -1,88 +1,23 @@
 --Suva
-function c10100055.initial_effect(c)
-	--spsummon
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c10100055.condition1)
-	e1:SetOperation(c10100055.operation1)
-	c:RegisterEffect(e1)
-	--No Response
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_BE_MATERIAL)
-	e2:SetCondition(c10100055.condition2)
-	e2:SetOperation(c10100055.operation2)
-	c:RegisterEffect(e2)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Multi-attribute
+  local e1a=Effect.CreateEffect(c)
+	e1a:SetType(EFFECT_TYPE_SINGLE)
+	e1a:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1a:SetCode(EFFECT_ADD_ATTRIBUTE)
+	e1a:SetRange(LOCATION_HAND)
+	e1a:SetValue(ATTRIBUTE_WIND+ATTRIBUTE_WATER+ATTRIBUTE_FIRE+ATTRIBUTE_EARTH)
+	c:RegisterEffect(e1a)
+  local e1b=e1a:Clone()
+  e1b:SetRange(LOCATION_MZONE)
+  e1b:SetCondition(s.condition1)
+  c:RegisterEffect(e1b)
 end
---e1 - spsummon
-function c10100055.filter1(c)
-	return c:IsSetCard(0x155) and c:IsType(TYPE_MONSTER) and not c:IsPublic()
+function s.filter1(c)
+  return c:IsFaceup() and c:IsSetCard(0xb05)
 end
-function c10100055.condition1(e,c)
-	if c==nil then return true end
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c10100055.filter1,c:GetControler(),LOCATION_HAND,0,1,nil)
-end
-function c10100055.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100055.filter1,tp,LOCATION_HAND,0,1,nil) end
-end
-function c10100055.operation1(e,tp,eg,ep,ev,re,r,rp,c)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local g=Duel.SelectMatchingCard(tp,c10100055.filter1,tp,LOCATION_HAND,0,1,1,nil)
-	Duel.ConfirmCards(1-tp,g)
-	Duel.ShuffleHand(tp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCode(EFFECT_CANNOT_SUMMON)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	e1:SetTarget(c10100055.target1_1)
-	e1:SetLabelObject(g:GetFirst())
-	e1:SetTargetRange(1,0)
-	Duel.RegisterEffect(e1,tp)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_CANNOT_MSET)
-	Duel.RegisterEffect(e2,tp)
-end
-function c10100055.target1_1(e,c)
-	return c~=e:GetLabelObject()
-end
---e2 - No Response
-function c10100055.filter2(c)
-	return c:IsSetCard(0x158) and c:IsType(TYPE_EQUIP) and c:IsFaceup()
-end
-function c10100055.condition2(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return r==REASON_SUMMON and c:GetReasonCard():IsSetCard(0x155)
-end
-function c10100055.operation2(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SetChainLimitTillChainEnd(c10100055.chainlimit2_2)
-	local c=e:GetHandler()
-	local sc=c:GetReasonCard()
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetOperation(c10100055.operation2_1)
-	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-	sc:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_CHAINING)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetOperation(c10100055.operation2_2)
-	e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-	c:RegisterEffect(e2)
-end
-function c10100055.chainlimit2_2(e,rp,tp)
-	return tp==rp
-end
-function c10100055.operation2_1(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SetChainLimitTillChainEnd(c10100055.chainlimit2_2)
-end
-function c10100055.operation2_2(e,tp,eg,ep,ev,re,r,rp)
-	if re:IsHasType(EFFECT_TYPE_ACTIVATE) then
-		Duel.SetChainLimit(c10100055.chainlimit2_2)
-	end
+function s.condition1(e)
+  local tp=e:GetHandlerPlayer()
+  return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_FZONE,0,1,nil)
 end
---
+++ cards-bcot/tests/c10100055.lua
@@ -0,0 +1,8 @@
+--Suva
+Debug.SetAIName("Unit Test")
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+Debug.AddCard(10100055,0,0,LOCATION_HAND,2,POS_FACEUP_ATTACK)
+Debug.AddCard(10100025,0,0,LOCATION_HAND,2,POS_FACEUP_ATTACK)
+Debug.ReloadFieldEnd()
