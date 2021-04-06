59d0bc4f08a9efa0c275d9a2f843caaca2257184
utso <cccygodev@gmail.com>
2021-04-06 19:30:13 +0200

    Implement Pohatu

--- cards-bcot/script/c10100004.lua
+++ cards-bcot/script/c10100004.lua
@@ -1,59 +1,53 @@
+if not bcot then
+	dofile "expansions/util-bcot.lua"
+end
 --Toa Mata Pohatu
-function c10100004.initial_effect(c)
-	--Equip
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100004,0))
-	e1:SetCategory(CATEGORY_EQUIP)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetTarget(c10100004.target1)
-	e1:SetOperation(c10100004.operation1)
+local s,id=GetID()
+function s.initial_effect(c)
+	 --Tribute from hand
+  local e1=bcot.toa_mata_tribute(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	c:RegisterEffect(e1)
 	--S/T Destroy
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(10100004,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DESTROY)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTarget(c10100004.target2)
-	e2:SetOperation(c10100004.operation2)
+	e2:SetCondition(s.condition2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+  e2:SetCountLimit(1)
 	c:RegisterEffect(e2)
 end
---e1 - Equip
-function c10100004.filter1(c,ec)
-	return c:IsType(TYPE_EQUIP)	and c:IsCode(10100010) and c:CheckEquipTarget(ec)
+function s.filter2a(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP)
 end
-function c10100004.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingMatchingCard(c10100004.filter1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e:GetHandler()) end
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_DECK)
+function s.filter2b(c)
+	return c:IsFaceup() and c:IsRace(RACE_ROCK)
 end
-function c10100004.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or c:IsFacedown() or not c:IsRelateToEffect(e) then return end
-	Duel.Hint(HINT_SELECTMSG,tp,nil)
-	local g=Duel.SelectMatchingCard(tp,c10100004.filter1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,c)
-	if g:GetCount()>0 and g:GetFirst():CheckEquipTarget(c) then
-		Duel.Equip(tp,g:GetFirst(),c)
-	end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+  return eg:IsExists(Card.IsPreviousLocation,1,nil,LOCATION_EXTRA)
 end
---e2 - S/T Destroy
-function c10100004.filter2(c)
-	return c:IsDestructable() and c:IsType(TYPE_SPELL+TYPE_TRAP)
-end
-function c10100004.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and c10100004.filter2(chkc) and chkc~=e:GetHandler() end
-	if chk==0 then return Duel.IsExistingTarget(c10100004.filter2,tp,0,LOCATION_ONFIELD,1,e:GetHandler()) end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and s.filter2(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter2a,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,c10100004.filter2,tp,0,LOCATION_ONFIELD,1,1,e:GetHandler())
+	local g=Duel.SelectTarget(tp,s.filter2a,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c10100004.operation2(e,tp,eg,ep,ev,re,r,rp)
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and c10100004.filter2(tc) then
+	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
+    if Duel.IsExistingMatchingCard(s.filter2b,tp,LOCATION_MZONE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+      Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+      local g=Duel.SelectMatchingCard(tp,s.filter2a,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+      if g:GetCount()>0 then
+        Duel.Destroy(g,REASON_EFFECT)
+      end
+    end
 	end
 end
