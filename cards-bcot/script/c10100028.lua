a9768b4dcf4c4d05a36b25d266810a0fdc2cb0be
utso <cccygodev@gmail.com>
2021-04-07 08:52:58 +0200

    Po-Koro: Add missing cost, fix material check (all, not any), add smarter client hint

--- cards-bcot/script/c10100028.lua
+++ cards-bcot/script/c10100028.lua
@@ -14,6 +14,7 @@ function s.initial_effect(c)
   e1:SetCode(EVENT_SPSUMMON_SUCCESS)
   e1:SetRange(LOCATION_FZONE)
 	e1:SetCondition(s.condition1)
+	e1:SetCost(s.cost1)
   e1:SetTarget(s.target1)
   e1:SetOperation(s.operation1)
   e1:SetCountLimit(1,id)
@@ -28,10 +29,19 @@ function s.initial_effect(c)
 	mcheck:SetValue(s.value_mcheck)
 	c:RegisterEffect(mcheck)
 end
+function s.filter1(c)
+  return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_WARRIOR) and c:IsAbleToRemoveAsCost()
+end
 function s.condition1(e,tp,eg,ep,ev,re,r,rp)
 	return eg:GetCount()==1 
     and (eg:GetFirst():GetSummonType()&(SUMMON_TYPE_FUSION|SUMMON_TYPE_SYNCHRO|SUMMON_TYPE_XYZ|SUMMON_TYPE_LINK))>0
     and eg:GetFirst():GetFlagEffect(id)~=0
+end
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_GRAVE,0,1,nil) end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+  local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_GRAVE,0,1,1,nil)
+  Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
   if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,id+10000,0,0,TYPES_TOKEN,0,1,RACE_ROCK,ATTRIBUTE_EARTH) end
@@ -40,6 +50,13 @@ function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
   local st=eg:GetFirst():GetSummonType()&(SUMMON_TYPE_FUSION|SUMMON_TYPE_SYNCHRO|SUMMON_TYPE_XYZ|SUMMON_TYPE_LINK)
+  local desc={
+    [SUMMON_TYPE_FUSION]  = 1,
+    [SUMMON_TYPE_SYNCHRO] = 2,
+    [SUMMON_TYPE_XYZ]     = 3,
+    [SUMMON_TYPE_LINK]    = 4
+  } --Lookup table for client hints depending on summon type
+  
   local c=e:GetHandler()
   local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -52,7 +69,7 @@ function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterEffect(e1,tp)
 	local e2=Effect.CreateEffect(e:GetHandler())
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,desc[st]))
 	e2:SetReset(RESET_PHASE+PHASE_END)
 	e2:SetTargetRange(1,0)
 	Duel.RegisterEffect(e2,tp)
@@ -69,7 +86,7 @@ function s.filter_mcheck(c)
 	return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_WARRIOR) and c:IsType(TYPE_MONSTER)
 end
 function s.value_mcheck(e,c)
-	if c:GetMaterial():IsExists(s.filter_mcheck,1,nil) then
+	if c:GetMaterial():GetCount()>0 and c:GetMaterial():FilterCount(s.filter_mcheck,nil)==c:GetMaterial():GetCount() then
 		c:RegisterFlagEffect(id,RESET_EVENT+((RESETS_STANDARD|RESET_OVERLAY)&~RESET_TOFIELD)+RESET_PHASE+PHASE_END,0,1)
 	end
 end
