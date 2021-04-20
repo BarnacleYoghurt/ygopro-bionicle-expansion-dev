6dcb993c151079a4f0b175178eea8a1e8d6fd975
utso <cccygodev@gmail.com>
2021-04-20 09:36:47 +0200

    Onewa: Implement redesigned draw/banish effect

--- cards-bcot/script/c10100020.lua
+++ cards-bcot/script/c10100020.lua
@@ -8,6 +8,7 @@ function s.initial_effect(c)
   local e1=Effect.CreateEffect(c)
   e1:SetCategory(CATEGORY_DRAW+CATEGORY_REMOVE)
   e1:SetType(EFFECT_TYPE_IGNITION)
+  e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
   e1:SetRange(LOCATION_MZONE)
   e1:SetCost(s.cost1)
   e1:SetTarget(s.target1)
@@ -18,7 +19,7 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-  e2:SetProperty(EFFECT_FLAG_DELAY)
+  e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
   e2:SetRange(LOCATION_MZONE)
   e2:SetCode(EVENT_SPSUMMON_SUCCESS)
   e2:SetCondition(aux.zptcon(s.filter2a))
@@ -33,36 +34,45 @@ end
 function s.check0(g,lc)
   return g:IsExists(s.filter0,1,nil)
 end
-function s.filter1(c)
-  return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAbleToRemoveAsCost()
+function s.filter1a(c,tp)
+  return c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost() and Duel.IsExistingTarget(s.filter1b,tp,LOCATION_GRAVE,0,1,c)
+end
+function s.filter1b(c)
+  return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsLevelBelow(4) and c:IsAbleToHand()
 end
 function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_GRAVE,0,1,nil) end
+  if chk==0 then return Duel.IsExistingMatchingCard(s.filter1a,tp,LOCATION_GRAVE,0,1,nil,tp) end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-  local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_GRAVE,0,1,1,nil)
+  local g=Duel.SelectMatchingCard(tp,s.filter1a,tp,LOCATION_GRAVE,0,1,1,nil,tp)
   Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.IsPlayerCanDraw(1-tp,1) end --Might need a check for ability to banish, but the cost mostly takes care of that
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter1b(chkc) end
+  if chk==0 then return true end --Check for target already done by cost; might need a check for ability to banish, but the cost also mostly takes care of that
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local tg=Duel.SelectTarget(tp,s.filter1b,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,tg,tg:GetCount(),0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_ALL,1)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,PLAYER_ALL,1)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-  local h1=Duel.Draw(tp,1,REASON_EFFECT)
-	local h2=Duel.Draw(1-tp,1,REASON_EFFECT)
-	if h1>0 or h2>0 then Duel.BreakEffect() end
-	if h1>0 then
-		Duel.ShuffleHand(tp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-		local g1=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_HAND,0,1,1,nil)
-    Duel.Remove(g1,POS_FACEUP,REASON_EFFECT)
-	end
-	if h2>0 then 
-		Duel.ShuffleHand(1-tp)
-    Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_REMOVE)
-		local g2=Duel.SelectMatchingCard(1-tp,Card.IsAbleToRemove,1-tp,LOCATION_HAND,0,1,1,nil)
-    Duel.Remove(g2,POS_FACEUP,REASON_EFFECT)
-	end
+  local pct=Duel.GetFieldGroupCount(tp,LOCATION_HAND+LOCATION_ONFIELD,0)
+  local oct=Duel.GetFieldGroupCount(1-tp,LOCATION_HAND+LOCATION_ONFIELD,0)
+  
+  if oct>pct then
+    Duel.Draw(tp,1,REASON_EFFECT)
+  elseif pct>oct then
+    Duel.Draw(1-tp,1,REASON_EFFECT)
+  end
+  
+  local tc=Duel.GetFirstTarget()
+  if tc:IsRelateToEffect(e) then
+    if Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
+      Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+      local g1=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_HAND,0,1,1,nil)
+      Duel.Remove(g1,POS_FACEUP,REASON_EFFECT)
+    end
+  end
 end
 function s.filter2a(c)
   return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_EARTH)
