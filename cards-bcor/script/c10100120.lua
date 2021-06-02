c4153446832ba5879daa943ef59ac8e1d5700c7e
utso <cccygodev@gmail.com>
2021-06-02 23:36:50 +0200

    Bog Snake: Update script for EDOPro

--- cards-bcor/script/c10100120.lua
+++ cards-bcor/script/c10100120.lua
@@ -1,102 +1,103 @@
 --Bog Snake, Rahi
-function c10100120.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--pendulum summon
-	aux.EnablePendulumAttribute(c)
+	Pendulum.AddProcedure(c)
 	--Special Summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100120,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetCost(c10100120.condition1)
-	e1:SetTarget(c10100120.target1)
-	e1:SetOperation(c10100120.operation1)
+	e1:SetCost(s.condition1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
 	c:RegisterEffect(e1)
 	--Draw
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(10100120,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DRAW)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetRange(LOCATION_PZONE)
 	e2:SetCode(EVENT_DAMAGE)
-	e2:SetCondition(c10100120.condition2)
-	e2:SetTarget(c10100120.target2)
-	e2:SetOperation(c10100120.operation2)
-	e2:SetCountLimit(1,10100120)
+	e2:SetCondition(s.condition2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+	e2:SetCountLimit(1,id)
 	c:RegisterEffect(e2)
 	--Damage
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_BE_MATERIAL)
-	e3:SetCondition(c10100120.condition3)
-	e3:SetOperation(c10100120.operation3)
+	e3:SetCondition(s.condition3)
+	e3:SetOperation(s.operation3)
 	c:RegisterEffect(e3)
 end
-function c10100120.condition1(e,tp,eg,ep,ev,re,r,rp)
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
 end
-function c10100120.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,LOCATION_SZONE)
 end
-function c10100120.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
 		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function c10100120.condition2(e,tp,eg,ep,ev,re,r,rp)
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
 	return ep~=tp and bit.band(r,REASON_EFFECT)~=0
 end
-function c10100120.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
-function c10100120.operation2(e,tp,eg,ep,ev,re,r,rp)
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsRelateToEffect(e) then
 		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 		Duel.Draw(p,d,REASON_EFFECT)
 	end
 end
-function c10100120.condition3(e,tp,eg,ep,ev,re,r,rp)
+function s.condition3(e,tp,eg,ep,ev,re,r,rp)
 	return r==REASON_SYNCHRO
 end
-function c10100120.operation3(e,tp,eg,ep,ev,re,r,rp)
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
 	local c=re:GetHandler()
 	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetDescription(aux.Stringid(10100120,2))
+	e1:SetDescription(aux.Stringid(id,2))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetReset(RESET_EVENT+0x1fe0000)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
-	e1:SetOperation(c10100120.operation3_1)
+	e1:SetOperation(s.operation3_1)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(e:GetHandler())
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_CHAIN_SOLVED)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetReset(RESET_EVENT+0x1fe0000)
-	e2:SetCondition(c10100120.condition3_2)
-	e2:SetOperation(c10100120.operation3_2)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e2:SetCondition(s.condition3_2)
+	e2:SetOperation(s.operation3_2)
 	c:RegisterEffect(e2)
 	if not c:IsType(TYPE_EFFECT) then
 		local e3=Effect.CreateEffect(e:GetHandler())
 		e3:SetType(EFFECT_TYPE_SINGLE)
 		e3:SetCode(EFFECT_ADD_TYPE)
 		e3:SetValue(TYPE_EFFECT)
-		e3:SetReset(RESET_EVENT+0x1fe0000)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
 		c:RegisterEffect(e3,true)
 	end
 end
-function c10100120.operation3_1(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():RegisterFlagEffect(10100120,RESET_EVENT+0x1fc0000+RESET_CHAIN,0,1)
+function s.operation3_1(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+(RESETS_STANDARD&~RESET_TURN_SET)+RESET_CHAIN,0,1)
 end
-function c10100120.condition3_2(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp and e:GetHandler():GetFlagEffect(10100120)~=0
+function s.condition3_2(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp and e:GetHandler():GetFlagEffect(id)~=0
 end
-function c10100120.operation3_2(e,tp,eg,ep,ev,re,r,rp)
+function s.operation3_2(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,0,e:GetHandler():GetCode())
 	Duel.Damage(1-tp,300,REASON_EFFECT)
 end
