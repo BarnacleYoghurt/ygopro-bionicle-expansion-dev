11748def275fd363c640c00fa07df2154852370c
utso <cccygodev@gmail.com>
2021-06-03 12:04:26 +0200

    Daikau, Devastation of the Rahi: Update scripts for EDOPro

--- cards-bcor/script/c10100156.lua
+++ cards-bcor/script/c10100156.lua
@@ -1,57 +1,58 @@
 --Daikau, Floral Rahi
-function c10100156.initial_effect(c)
-	aux.EnablePendulumAttribute(c)
+local s,id=GetID()
+function s.initial_effect(c)
+	Pendulum.AddProcedure(c)
 	--Reduce ATK
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100156,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetCost(c10100156.cost1)
-	e1:SetTarget(c10100156.target1)
-	e1:SetOperation(c10100156.operation1)
-	e1:SetCountLimit(1,10100156)
+	e1:SetCost(s.cost1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
+	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
 	--Special Summon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(10100156,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetTarget(c10100156.target2)
-	e2:SetOperation(c10100156.operation2)
-	e2:SetCountLimit(1,11100156)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+	e2:SetCountLimit(1,id+1000000)
 	c:RegisterEffect(e2)	
 	--Destroy
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(10100156,2))
+	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_DESTROY)
 	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_REMOVE)
-	e3:SetCost(c10100156.cost3)
-	e3:SetTarget(c10100156.target3)
-	e3:SetOperation(c10100156.operation3)
-	e3:SetCountLimit(1,11100156)
+	e3:SetCost(s.cost3)
+	e3:SetTarget(s.target3)
+	e3:SetOperation(s.operation3)
+	e3:SetCountLimit(1,id+1000000)
 	c:RegisterEffect(e3)
 end
-function c10100156.filter1(c)
-	return c:IsSetCard(0x15a) and c:IsType(TYPE_PENDULUM) and c:IsAbleToGraveAsCost()
+function s.filter1(c)
+	return c:IsSetCard(0xb06) and c:IsType(TYPE_PENDULUM) and c:IsAbleToGraveAsCost()
 end
-function c10100156.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100156.filter1,tp,LOCATION_DECK,0,1,nil) end
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c10100156.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		e:SetLabel(g:GetFirst():GetAttack())
 		Duel.SendtoGrave(g,REASON_COST)
 	end
 end
-function c10100156.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
 end
-function c10100156.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsRelateToEffect(e) then
 		local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
 		local tc=g:GetFirst()
@@ -60,45 +61,45 @@ function c10100156.operation1(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
 			e1:SetValue(-e:GetLabel())
-			e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 			tc:RegisterEffect(e1)
 			tc=g:GetNext()
 		end
 	end
 end
-function c10100156.filter2(c,e,tp)
+function s.filter2(c,e,tp)
 	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c10100156.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100156.filter2,tp,LOCATION_GRAVE,0,1,e:GetHandler(),e,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_GRAVE,0,1,e:GetHandler(),e,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
-function c10100156.operation2(e,tp,eg,ep,ev,re,r,rp)
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,c10100156.filter2,tp,LOCATION_GRAVE,0,1,1,e:GetHandler(),e,tp)
+		local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_GRAVE,0,1,1,e:GetHandler(),e,tp)
 		if g:GetCount()>0 then
 			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		end
 	end
 end
-function c10100156.filter3a(c)
-	return c:IsSetCard(0x15a) and c:IsDiscardable()
+function s.filter3a(c)
+	return c:IsSetCard(0xb06) and c:IsDiscardable()
 end
-function c10100156.filter3b(c)
+function s.filter3b(c)
 	return c:IsFaceup() and c:IsAttackBelow(2000) and c:IsDestructable()
 end
-function c10100156.cost3(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100156.filter3a,tp,LOCATION_HAND,0,1,nil) end
-	Duel.DiscardHand(tp,c10100156.filter3a,1,1,REASON_COST+REASON_DISCARD,nil)
+function s.cost3(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter3a,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,s.filter3a,1,1,REASON_COST+REASON_DISCARD,nil)
 end
-function c10100156.target3(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100156.filter3b,tp,0,LOCATION_MZONE,1,nil) end
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter3b,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,c10100156.filter3b,tp,0,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.filter3b,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c10100156.operation3(e,tp,eg,ep,ev,re,r,rp)
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	Duel.Destroy(tc,REASON_EFFECT)
 end
