931b4c97f8cffe6b3ce6d6d7053f1f4790255298
utso <cccygodev@gmail.com>
2021-06-05 14:56:03 +0200

    Mana Ko, Manas, Mangaia, Mata Nui Cow: Update scripts for EDOPro

--- cards-bcor/script/c10100134.lua
+++ cards-bcor/script/c10100134.lua
@@ -1,62 +1,63 @@
 --Mata Nui Cow, Rahi
-function c10100134.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--synchro summon
-	aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x15a),aux.NonTuner(nil),1)
+	Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xb06),1,1,Synchro.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
 	--Destroy S/T
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100134,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(c10100134.condition1)
-	e1:SetTarget(c10100134.target1)
-	e1:SetOperation(c10100134.operation1)
+	e1:SetCondition(s.condition1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
 	c:RegisterEffect(e1)
 	--To Hand
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(10100134,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetCondition(c10100134.condition2)
-	e2:SetTarget(c10100134.target2)
-	e2:SetOperation(c10100134.operation2)
+	e2:SetCondition(s.condition2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
 	c:RegisterEffect(e2)
 end
-function c10100134.filter1a(c)
-	return c:IsSetCard(0x15a) and c:IsFaceup()
+function s.filter1a(c)
+	return c:IsSetCard(0xb06) and c:IsFaceup()
 end
-function c10100134.filter1b(c)
+function s.filter1b(c)
 	return c:IsDestructable() and c:IsType(TYPE_SPELL+TYPE_TRAP)
 end
-function c10100134.condition1(e,tp,eg,ep,ev,re,r,rp)
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SYNCHRO
 end
-function c10100134.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(c10100134.filter1b,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-	local g=Duel.GetMatchingGroup(c10100134.filter1b,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(s.filter1b,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	local g=Duel.GetMatchingGroup(s.filter1b,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c10100134.operation1(e,tp,eg,ep,ev,re,r,rp)	
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)	
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,c10100134.filter1b,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,Duel.GetMatchingGroupCount(c10100134.filter1a,tp,LOCATION_MZONE,0,nil),nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter1b,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,Duel.GetMatchingGroupCount(s.filter1a,tp,LOCATION_MZONE,0,nil),nil)
 	Duel.Destroy(g,REASON_EFFECT)
 end
-function c10100134.filter2(c)
-	return c:IsSetCard(0x15a) and c:IsLevelBelow(4) and c:IsAbleToHand()
+function s.filter2(c)
+	return c:IsSetCard(0xb06) and c:IsLevelBelow(4) and c:IsAbleToHand()
 end
-function c10100134.condition2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.condition2(e,tp,eg,ep,ev,re,r,rp,chk)
 	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
-function c10100134.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(c10100134.filter2,tp,LOCATION_GRAVE,0,1,nil) end
-	local g=Duel.GetMatchingGroup(c10100134.filter2,tp,LOCATION_GRAVE,0,nil)
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(s.filter2,tp,LOCATION_GRAVE,0,1,nil) end
+	local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_GRAVE,0,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
-function c10100134.operation2(e,tp,eg,ep,ev,re,r,rp)
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c10100134.filter2,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_GRAVE,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
--- cards-bcor/script/c10100157.lua
+++ cards-bcor/script/c10100157.lua
@@ -1,7 +1,8 @@
 --Mana Ko, Guardian Rahi
-function c10100157.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Synchro Summon
-	aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x15a),aux.NonTuner(Card.IsSetCard,0x15a),1)
+	Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xb06),1,1,Synchro.NonTuner(Card.IsSetCard,0xb06),1,99)
 	c:EnableReviveLimit()
 	--Control cannot switch
 	local e1=Effect.CreateEffect(c)
@@ -14,75 +15,75 @@ function c10100157.initial_effect(c)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(0,LOCATION_MZONE)
 	e2:SetCode(EFFECT_SET_ATTACK_FINAL)
-	e2:SetCondition(c10100157.condition2)
-	e2:SetValue(c10100157.value2)
+	e2:SetCondition(s.condition2)
+	e2:SetValue(s.value2)
 	c:RegisterEffect(e2)
 	--Enable effects
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EFFECT_MATERIAL_CHECK)
-	e3:SetValue(c10100157.value3)
+	e3:SetValue(s.value3)
 	c:RegisterEffect(e3)
 	--Summon Material
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(10100157,0))
+	e4:SetDescription(aux.Stringid(id,0))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e4:SetCode(EVENT_LEAVE_FIELD)
-	e4:SetCondition(c10100157.condition4)
-	e4:SetTarget(c10100157.target4)
-	e4:SetOperation(c10100157.operation4)
+	e4:SetCondition(s.condition4)
+	e4:SetTarget(s.target4)
+	e4:SetOperation(s.operation4)
 	e4:SetLabelObject(e3)
 	c:RegisterEffect(e4)
 	--Protection
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_SINGLE)
 	e5:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e5:SetCondition(c10100157.condition5)
+	e5:SetCondition(s.condition5)
 	e5:SetValue(aux.tgoval)
 	e5:SetLabelObject(e3)
 	c:RegisterEffect(e5)
 	local e5a=e5:Clone()
 	e5a:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e5a:SetValue(c10100157.value5a)
+	e5a:SetValue(s.value5a)
 	c:RegisterEffect(e5a)	
 end
-function c10100157.value2(e,c)
+function s.value2(e,c)
 	return c:GetAttack()/2
 end
-function c10100157.condition2(e)
+function s.condition2(e)
 	return Duel.GetTurnPlayer()~=e:GetHandlerPlayer() and Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE
 end
-function c10100157.filter3(c)
+function s.filter3(c)
 	return not c:IsType(TYPE_TUNER)
 end
-function c10100157.value3(e,c)
+function s.value3(e,c)
 	local g=c:GetMaterial()
-	local ct=g:FilterCount(c10100157.filter3,nil)
+	local ct=g:FilterCount(s.filter3,nil)
 	e:SetLabel(ct)
 	if ct==1 then
-		e:SetLabelObject(g:Filter(c10100157.filter3,nil):GetFirst())
+		e:SetLabelObject(g:Filter(s.filter3,nil):GetFirst())
 	elseif ct>1 then
 	end
 end
-function c10100157.condition4(e,tp,eg,ep,ev,re,r,rp)
+function s.condition4(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetLabelObject():GetLabel()==1
 end
-function c10100157.target4(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target4(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=e:GetLabelObject():GetLabelObject()
 	if chk==0 then return tc and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and tc:IsLocation(LOCATION_GRAVE) and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tc,1,tp,LOCATION_GRAVE)
 end
-function c10100157.operation4(e,tp,eg,ep,ev,re,r,rp)
+function s.operation4(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=e:GetLabelObject():GetLabelObject()
 	if tc and not tc:IsType(TYPE_TUNER) and tc:IsLocation(LOCATION_GRAVE) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function c10100157.value5a(e,re,rp)
+function s.value5a(e,re,rp)
 	return rp~=e:GetHandlerPlayer()
 end
-function c10100157.condition5(e,tp,eg,ep,ev,re,r,rp)
+function s.condition5(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetLabelObject():GetLabel()>1
 end
