37569f35169c4073885922f756b69cc7cab40932
utso <cccygodev@gmail.com>
2021-06-06 18:55:04 +0200

    Kongu, Onepu, Matoro: Update scripts for EDOPro

--- cards-bcor/script/c10100149.lua
+++ cards-bcor/script/c10100149.lua
@@ -1,46 +1,47 @@
 --Matoran Translator Matoro
-function c10100149.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Flag
 	local e0=Effect.CreateEffect(c)
 	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e0:SetCode(EVENT_SUMMON_SUCCESS)
-	e0:SetOperation(c10100149.operation0)
+	e0:SetOperation(s.operation0)
 	c:RegisterEffect(e0)
 	--Special Summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100149,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(c10100149.condition1)
-	e1:SetCost(c10100149.cost1)
-	e1:SetTarget(c10100149.target1)
-	e1:SetOperation(c10100149.operation1)
-	e1:SetCountLimit(1,10100149)
+	e1:SetCondition(s.condition1)
+	e1:SetCost(s.cost1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
+	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
 end
-function c10100149.operation0(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():RegisterFlagEffect(10100149,RESET_EVENT+0x1ec0000+RESET_PHASE+PHASE_END,0,1)
+function s.operation0(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+(RESETS_STANDARD&~(RESET_TURN_SET|RESET_TEMP_REMOVE))+RESET_PHASE+PHASE_END,0,1)
 end
-function c10100149.filter1(c,e,tp)
-	return c:IsSetCard(0x157) and c:GetLevel()==2 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsCode(10100149)
+function s.filter1(c,e,tp)
+	return c:IsSetCard(0xb01) and c:GetLevel()==2 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsCode(id)
 end
-function c10100149.condition1(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetFlagEffect(10100149)>0
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(id)>0
 end
-function c10100149.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsReleasable() end
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
-function c10100149.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100149.filter1,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	local g=Duel.GetMatchingGroup(c10100149.filter1,tp,LOCATION_GRAVE,0,nil,e,tp)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	local g=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_GRAVE,0,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,2,tp,LOCATION_GRAVE)
 end
-function c10100149.operation1(e,tp,eg,ep,ev,re,r,rp)	
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)	
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c10100149.filter1,tp,LOCATION_GRAVE,0,1,2,nil,e,tp)	
+	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_GRAVE,0,1,2,nil,e,tp)	
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)	
 	end
--- cards-bcor/script/c10100150.lua
+++ cards-bcor/script/c10100150.lua
@@ -1,75 +1,76 @@
 --Matoran Pilot Kongu
-function c10100150.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Equip
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100150,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(c10100150.condition1)
-	e1:SetTarget(c10100150.target1)
-	e1:SetOperation(c10100150.operation1)
+	e1:SetCondition(s.condition1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
 	c:RegisterEffect(e1)
 	--Destroy monster
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(10100150,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_BATTLE_DAMAGE)	
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCondition(c10100150.condition2)
-	e2:SetTarget(c10100150.target2)
-	e2:SetOperation(c10100150.operation2)
+	e2:SetCondition(s.condition2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
 	e2:SetLabelObject(e1)
 	c:RegisterEffect(e2)
 	--Special Summon
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(10100150,2))
+	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_TO_GRAVE)
-	e3:SetCondition(c10100150.condition3)
-	e3:SetTarget(c10100150.target3)
-	e3:SetOperation(c10100150.operation3)
+	e3:SetCondition(s.condition3)
+	e3:SetTarget(s.target3)
+	e3:SetOperation(s.operation3)
 	e3:SetLabelObject(e1)
 	c:RegisterEffect(e3)
 end
-function c10100150.filter1(c)
-	return c:IsRace(RACE_BEAST+RACE_WINDBEAST)
+function s.filter1(c)
+	return c:IsRace(RACE_BEAST+RACE_WINGEDBEAST)
 end
-function c10100150.condition1(e,tp,eg,ep,ev,re,r,rp)
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
 	local ec=e:GetLabelObject()
-	return not (ec and ec:GetFlagEffect(10100150)~=0)
+	return not (ec and ec:GetFlagEffect(id)~=0)
 end
-function c10100150.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingTarget(c10100150.filter1,tp,LOCATION_MZONE,0,1,nil) end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,c10100150.filter1,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
 end
-function c10100150.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsRace(RACE_BEAST+RACE_WINDBEAST) then
+	if tc:IsRelateToEffect(e) and tc:IsRace(RACE_BEAST+RACE_WINGEDBEAST) then
 		if c:IsFaceup() and c:IsRelateToEffect(e) then
 			if Duel.Equip(tp,tc,c,false) then
 				--Add Equip limit
-				tc:RegisterFlagEffect(10100150,RESET_EVENT+0x1fe0000,0,0)
+				tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,0)
 				e:SetLabelObject(tc)
 				local e1=Effect.CreateEffect(c)
 				e1:SetType(EFFECT_TYPE_SINGLE)
 				e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
 				e1:SetCode(EFFECT_EQUIP_LIMIT)
-				e1:SetReset(RESET_EVENT+0x1fe0000)
-				e1:SetValue(c10100150.limit1_1)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+				e1:SetValue(s.limit1_1)
 				tc:RegisterEffect(e1)
 				local e2=Effect.CreateEffect(c)
 				e2:SetType(EFFECT_TYPE_EQUIP)
 				e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_OWNER_RELATE+EFFECT_FLAG_SET_AVAILABLE)
 				e2:SetCode(EFFECT_DIRECT_ATTACK)
-				e2:SetReset(RESET_EVENT+0x1fe0000)
+				e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 				e2:SetValue(1)
 				tc:RegisterEffect(e2)
 			end
@@ -78,39 +79,39 @@ function c10100150.operation1(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c10100150.limit1_1(e,c)
+function s.limit1_1(e,c)
 	return e:GetOwner()==c
 end
-function c10100150.filter2(c,atk)
+function s.filter2(c,atk)
 	return c:IsFaceup() and c:IsDefenseBelow(atk) and c:IsDestructable()
 end
-function c10100150.condition2(e,tp,eg,ep,ev,re,r,rp)
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
 	local ec=e:GetLabelObject():GetLabelObject()
-	return ec and ec:IsHasCardTarget(e:GetHandler()) and ec:GetFlagEffect(10100150)~=0
+	return ec and ec:IsHasCardTarget(e:GetHandler()) and ec:GetFlagEffect(id)~=0
 end
-function c10100150.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ec=e:GetLabelObject():GetLabelObject()
-	if chk==0 then return Duel.IsExistingTarget(c10100150.filter2,tp,0,LOCATION_MZONE,1,nil,ec:GetAttack()) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter2,tp,0,LOCATION_MZONE,1,nil,ec:GetAttack()) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,c10100150.filter2,tp,0,LOCATION_MZONE,1,1,nil,ec:GetAttack())
+	local g=Duel.SelectTarget(tp,s.filter2,tp,0,LOCATION_MZONE,1,1,nil,ec:GetAttack())
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c10100150.operation2(e,tp,eg,ep,ev,re,r,rp)
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
 	end
 end
-function c10100150.condition3(e,tp,eg,ep,ev,re,r,rp)
+function s.condition3(e,tp,eg,ep,ev,re,r,rp)
 	local ec=e:GetLabelObject():GetLabelObject()
 	return ec and e:GetHandler():IsReason(REASON_DESTROY) and ec:IsLocation(LOCATION_SZONE) and ec:GetPreviousEquipTarget()==e:GetHandler()
 end
-function c10100150.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ec=e:GetLabelObject():GetLabelObject()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and ec and ec:IsReason(REASON_LOST_TARGET) and ec:IsLocation(LOCATION_GRAVE) and ec:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,ec,1,tp,LOCATION_GRAVE)
 end
-function c10100150.operation3(e,tp,eg,ep,ev,re,r,rp)
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
 	local ec=e:GetLabelObject():GetLabelObject()
 	if ec and ec:GetPreviousEquipTarget()==e:GetHandler() and ec:IsLocation(LOCATION_GRAVE) then
 		Duel.SpecialSummon(ec,0,tp,tp,false,false,POS_FACEUP)
