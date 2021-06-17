3ea556eefea15cf4d85d32bb17d9816fb9e27589
utso <cccygodev@gmail.com>
2021-06-17 18:54:50 +0200

    Dikapi: Protoype (untested) implementation of ugly hack to make Synchro with face-up ED work in EDOPro

--- cards-bcor/script/c10100114.lua
+++ cards-bcor/script/c10100114.lua
@@ -15,18 +15,32 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation1)
 	c:RegisterEffect(e1)
 	--Extra Synchro
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_SYNCHRO_MATERIAL_CUSTOM)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2:SetCondition(s.condition2)
-	e2:SetTarget(s.target2)
-	e2:SetValue(1)
-	e2:SetOperation(s.operation2)
-	c:RegisterEffect(e2)
-end
-function s.filter1(c)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0xb06) and c:IsLevelAbove(1) and c:IsAbleToRemoveAsCost()
+	local synproc=Effect.CreateEffect(c) --Alternate Synchro Procedure
+	synproc:SetType(EFFECT_TYPE_FIELD)
+	synproc:SetDescription(aux.Stringid(id, 1))
+	synproc:SetCode(EFFECT_SPSUMMON_PROC)
+	synproc:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	synproc:SetRange(LOCATION_EXTRA)
+	synproc:SetCondition(s.synproc_condition)
+	synproc:SetTarget(s.synproc_target)
+	synproc:SetOperation(Synchro.Operation)
+	synproc:SetValue(SUMMON_TYPE_SYNCHRO)
+	local e2a=Effect.CreateEffect(c) --Grant synproc to all Synchros in ED
+	e2a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+	e2a:SetRange(LOCATION_MZONE)
+	e2a:SetTargetRange(LOCATION_EXTRA,0)
+	e2a:SetCondition(s.condition2)
+	e2a:SetTarget(s.target2)
+	e2a:SetLabelObject(synproc)
+	c:RegisterEffect(e2a)
+	local e2b=Effect.CreateEffect(c) --Hand Synchro (works with ED Pendulums thanks to synproc)
+	e2b:SetType(EFFECT_TYPE_SINGLE)
+	e2b:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2b:SetCode(EFFECT_HAND_SYNCHRO)
+	e2b:SetCondition(s.condition2)
+	e2b:SetValue(s.value2)
+	e2b:SetLabel(id)
+	c:RegisterEffect(e2b)
 end
 function s.condition1(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SYNCHRO
@@ -46,48 +60,81 @@ function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		if e:GetHandler():RegisterEffect(e1) then
 			Duel.BreakEffect()
-			Duel.Damage(tp,lv*300,REASON_EFFECT)		
-		end	
+			Duel.Damage(tp,lv*300,REASON_EFFECT)
+		end
 	end
 end
-function s.filter2a(c,syncard,tuner,f)
-	return c:IsCanBeSynchroMaterial(syncard,tuner) and (f==nil or f(c))
+
+function s.synproc_getBaseSynproc(sc) --Get the Synchro Summon Procedure of sc (false if none registered)
+	local sc=e:GetHandler()
+	local ssprocs = {sc:GetCardEffect(EFFECT_SPSUMMON_PROC)}
+	for _,ssproc in ipairs(ssproc) do
+		if ssproc:GetDescription()==1172 then
+			return ssproc
+		end
+	end
+	return false
 end
-function s.filter2b(c,syncard,tuner,f,g,lv,minc,maxc)
-	if c:IsCanBeSynchroMaterial(syncard,tuner) and (f==nil or f(c)) and c:IsFaceup() and c:IsType(TYPE_PENDULUM) and c:IsSetCard(0xb06) then
-		lv=lv-c:GetLevel()
-		if lv<0 then return false end
-		if lv==0 then return minc==1 end
-		return g and g:CheckWithSumEqual(Card.GetSynchroLevel,lv,minc-1,maxc-1,syncard)
-	else return false end
+function s.synproc_runWithExtraHand(fun,...) --Run function with LOCATION_HAND "constant" altered to also include LOCATION_EXTRA
+	local origval = LOCATION_HAND
+	LOCATION_HAND = LOCATION_HAND + LOCATION_EXTRA --I deserve to be trampled by a herd of Kikanalo for this
+	local res = fun(...)
+	LOCATION_HAND = origval
+	return res
 end
+function s.synproc_condition(e,c,smat,mg,min,max)
+	local fun = s.synproc_getBaseSynproc(e:GetHandler()):GetCondition()
+	return s.synproc_runWithExtraHand(fun,e,c,smat,mg,min,max)
+end
+function s.synproc_target(e,tp,eg,ep,ev,re,r,rp,chk,c,smat,mg,min,max)
+	local fun = s.synproc_getBaseSynproc(e:GetHandler()):GetTarget()
+	return s.synproc_runWithExtraHand(fun,e,tp,eg,ep,ev,re,r,rp,chk,c,smat,mg,min,max)
+end
+
 function s.condition2(e)
-  Debug.Message("cond2")
 	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SYNCHRO
 end
-function s.target2(e,syncard,f,minc,maxc)
-  Debug.Message("tg2")
-	local c=e:GetHandler()
-	local tp=syncard:GetControler()
-	local lv=syncard:GetLevel()-c:GetLevel()
-	if lv<=0 then return false end
-	local g1=Duel.GetMatchingGroup(s.filter2a,tp,LOCATION_MZONE,LOCATION_MZONE,c,syncard,c,f)
-	return g1:CheckWithSumEqual(Card.GetSynchroLevel,lv,minc,maxc,syncard)
-		or Duel.IsExistingMatchingCard(s.filter2b,tp,LOCATION_EXTRA,0,1,nil,syncard,c,f,nil,lv,minc,maxc)
+function s.target2(e,c)
+	return c:IsFacedown() and s.synproc_getBaseSynproc(c) --Only cards that can be synchro summoned
 end
-function s.operation2(e,tp,eg,ep,ev,re,r,rp,syncard,f,minc,maxc)
-	local c=e:GetHandler()
-	local lv=syncard:GetLevel()-c:GetLevel()
-	local g1=Duel.GetMatchingGroup(s.filter2a,tp,LOCATION_MZONE,LOCATION_MZONE,c,syncard,c,f)
-	local g2=Duel.GetMatchingGroup(s.filter2b,tp,LOCATION_EXTRA,0,nil,syncard,c,f,nil,lv,minc,maxc)
-	if not g1:CheckWithSumEqual(Card.GetSynchroLevel,lv,minc,maxc,syncard)
-		or (g2:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1))) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-		local sg=g2:Select(tp,1,1,nil)
-		Duel.SetSynchroMaterial(sg)
+function s.value2(e,c,sc)
+	if c:IsLocation(LOCATION_EXTRA) and c:IsFaceup() and c:IsType(TYPE_PENDULUM) and c:IsSetCard(0xb06) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_HAND_SYNCHRO+EFFECT_SYNCHRO_CHECK)
+		e1:SetLabel(id)
+		e1:SetTarget(s.target2_1)
+		c:RegisterEffect(e1)
+		return true
+	else return false end
+end
+function s.chk(c)
+	if not c:IsHasEffect(EFFECT_HAND_SYNCHRO+EFFECT_SYNCHRO_CHECK) then return false end
+	local te={c:GetCardEffect(EFFECT_HAND_SYNCHRO+EFFECT_SYNCHRO_CHECK)}
+	for i=1,#te do
+		local e=te[i]
+		if e:GetLabel()~=id then return false end
+	end
+	return true
+end
+function s.chk2(c)
+	if not c:IsHasEffect(EFFECT_HAND_SYNCHRO) or c:IsHasEffect(EFFECT_HAND_SYNCHRO+EFFECT_SYNCHRO_CHECK) then return false end
+	local te={c:GetCardEffect(EFFECT_HAND_SYNCHRO)}
+	for i=1,#te do
+		local e=te[i]
+		if e:GetLabel()==id then return true end
+	end
+	return false
+end
+function s.target2_1(e,c,sg,tg,ntg,tsg,ntsg)
+	if c then
+		local res=true
+		if sg:IsExists(s.chk,1,c) or (not tg:IsExists(s.chk2,1,c) and not ntg:IsExists(s.chk2,1,c)
+			and not sg:IsExists(s.chk2,1,c)) then return false end
+		local trg=tg:Filter(s.chk,nil)
+		local ntrg=ntg:Filter(s.chk,nil)
+		return res,trg,ntrg
 	else
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-		local sg=g1:SelectWithSumEqual(tp,Card.GetSynchroLevel,lv,minc,maxc,syncard)
-		Duel.SetSynchroMaterial(sg)
+		return true
 	end
 end
