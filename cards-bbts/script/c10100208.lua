46247eb931b3d1e948eebc37c8c161413df03e18
utso <cccygodev@gmail.com>
2021-06-18 18:05:22 +0200

    Krana Vu: Update script for EDOPro

--- cards-bbts/script/c10100208.lua
+++ cards-bbts/script/c10100208.lua
@@ -1,46 +1,48 @@
 if not bbts then
 	dofile "expansions/util-bbts.lua"
 end
+local s,id=GetID()
 --Krana Vu, Surveyor
-function c10100208.initial_effect(c)
+function s.initial_effect(c)
 	--Negate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
-  e1:SetDescription(aux.Stringid(10100208,0))
+  e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_QUICK_O)
+  e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCode(EVENT_CHAINING)
-	e1:SetCondition(c10100208.condition1)
-	e1:SetCost(c10100208.cost1)
-	e1:SetTarget(c10100208.target1)
-	e1:SetOperation(c10100208.operation1)
-	e1:SetCountLimit(1,10100208)
+	e1:SetCondition(s.condition1)
+	e1:SetCost(s.cost1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
+	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
 	--Revive
 	local e2=bbts.krana_revive(c)
 	c:RegisterEffect(e2)
 end
-function c10100208.filter1(c,tp)
-  return c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsControler(tp) and c:IsSetCard(0x15c)
+function s.filter1(c,tp)
+  return c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsControler(tp) and c:IsSetCard(0xb08)
 end
-function c10100208.condition1(e,tp,eg,ep,ev,re,r,rp)
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
 	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
 	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-	return Duel.IsChainNegatable(ev) and g:IsExists(c10100208.filter1,1,nil,tp)
+	return Duel.IsChainNegatable(ev) and g:IsExists(s.filter1,1,nil,tp)
 end
-function c10100208.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
   if chk==0 then return c:IsAbleToGraveAsCost() end
 	Duel.SendtoGrave(c,REASON_COST)
 end
-function c10100208.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 	end
 end
-function c10100208.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
 		Duel.Destroy(eg,REASON_EFFECT)
 	end
