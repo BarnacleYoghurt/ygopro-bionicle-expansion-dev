fc8e178ac377e0154234092fdc5eeed331cc9003
utso <cccygodev@gmail.com>
2021-06-18 09:28:41 +0200

    Dikapi: Replace horrible hack with less horrible hack

--- cards-bcor/script/c10100114.lua
+++ cards-bcor/script/c10100114.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation1)
 	c:RegisterEffect(e1)
 	--Extra Synchro
-  local smat=Group.FromCards(c) --Force using this card in alternate procedure
+  local smat=Group.FromCards(c) --To force using this card in alternate procedure
   smat:KeepAlive()
 	local synproc=Effect.CreateEffect(c) --Alternate Synchro Procedure
 	synproc:SetType(EFFECT_TYPE_FIELD)
@@ -28,28 +28,20 @@ function s.initial_effect(c)
 	synproc:SetOperation(Synchro.Operation)
   synproc:SetLabelObject(smat)
 	synproc:SetValue(SUMMON_TYPE_SYNCHRO)
-	local e2a=Effect.CreateEffect(c) --Grant synproc to all Synchros in ED
-	e2a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
-	e2a:SetRange(LOCATION_MZONE)
-	e2a:SetTargetRange(LOCATION_EXTRA,0)
-	e2a:SetCondition(s.condition2)
-	e2a:SetTarget(s.target2)
-	e2a:SetLabelObject(synproc)
-	c:RegisterEffect(e2a)
-	local e2b=Effect.CreateEffect(c) --Hand Synchro (works with ED Pendulums thanks to synproc)
-	e2b:SetType(EFFECT_TYPE_SINGLE)
-	e2b:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2b:SetCode(id)
-	e2b:SetCondition(s.condition2)
-	e2b:SetValue(s.value2)
-	e2b:SetLabel(id)
-	c:RegisterEffect(e2b)
-  local e2c=Effect.CreateEffect(c) --Allow no other materials when using ED
-  e2c:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2c:SetType(EFFECT_TYPE_SINGLE)
-	e2c:SetCode(EFFECT_SYNCHRO_MATERIAL_CUSTOM)
-	e2c:SetOperation(s.operation2)
-	c:RegisterEffect(e2c)
+	local e2=Effect.CreateEffect(c) --Grant synproc to all Synchros in ED
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_EXTRA,0)
+	e2:SetCondition(s.condition2)
+	e2:SetTarget(s.target2)
+	e2:SetLabelObject(synproc)
+	c:RegisterEffect(e2)
+  local e3=Effect.CreateEffect(c) --Allow no other materials when using ED
+  e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_SYNCHRO_MATERIAL_CUSTOM)
+	e3:SetOperation(s.operation3)
+	c:RegisterEffect(e3)
 end
 function s.condition1(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SYNCHRO
@@ -83,76 +75,35 @@ function s.synproc_getBaseSynproc(sc) --Get the Synchro Summon Procedure of sc (
 	end
 	return false
 end
-function s.synproc_runWithExtraHand(fun,...) --Run function with LOCATION_HAND "constant" altered to also include LOCATION_EXTRA
-	local hand_origval = LOCATION_HAND
-  local hsyn_origval = EFFECT_HAND_SYNCHRO
-	LOCATION_HAND = LOCATION_HAND + LOCATION_EXTRA --I deserve to be trampled by a herd of Kikanalo for this
-  EFFECT_HAND_SYNCHRO = id
-	local res = fun(...)
-	LOCATION_HAND = hand_origval
-  EFFECT_HAND_SYNCHRO = hsyn_origval
-	return res
+function s.synproc_filter(c)
+  return c:IsFaceup() and c:IsType(TYPE_PENDULUM) and c:IsSetCard(0xb06)
 end
 function s.synproc_condition(e,c,smat,mg,min,max)
 	local fun = s.synproc_getBaseSynproc(e:GetHandler()):GetCondition()
   if not smat then smat=Group.CreateGroup() end
+  if not mg then mg=Group.CreateGroup() end
   smat:Merge(e:GetLabelObject())
-	return s.synproc_runWithExtraHand(fun,e,c,smat,mg,min,max)
+  mg:Merge(e:GetLabelObject())
+  mg:Merge(Duel.GetMatchingGroup(s.synproc_filter,e:GetHandlerPlayer(),LOCATION_EXTRA,0,nil))
+	return fun(e,c,smat,mg,min,max)
 end
 function s.synproc_target(e,tp,eg,ep,ev,re,r,rp,chk,c,smat,mg,min,max)
 	local fun = s.synproc_getBaseSynproc(e:GetHandler()):GetTarget()
   if not smat then smat=Group.CreateGroup() end
+  if not mg then mg=Group.CreateGroup() end
   smat:Merge(e:GetLabelObject())
-	return s.synproc_runWithExtraHand(fun,e,tp,eg,ep,ev,re,r,rp,chk,c,smat,mg,min,max)
+  mg:Merge(e:GetLabelObject())
+  mg:Merge(Duel.GetMatchingGroup(s.synproc_filter,e:GetHandlerPlayer(),LOCATION_EXTRA,0,nil))
+	return fun(e,tp,eg,ep,ev,re,r,rp,chk,c,smat,mg,min,max)
 end
 
 function s.condition2(e)
 	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SYNCHRO
 end
 function s.target2(e,c)
-	return c:IsFacedown() and s.synproc_getBaseSynproc(c) --Only cards that can be synchro summoned
+	return not (c:IsFaceup() and c:IsType(TYPE_PENDULUM)) and s.synproc_getBaseSynproc(c) --Only cards that can be synchro summoned
 end
-function s.value2(e,c,sc)
-	if c:IsLocation(LOCATION_EXTRA) and c:IsFaceup() and c:IsType(TYPE_PENDULUM) and c:IsSetCard(0xb06) then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_HAND_SYNCHRO+EFFECT_SYNCHRO_CHECK)
-		e1:SetLabel(id)
-		e1:SetTarget(s.target2_1)
-		c:RegisterEffect(e1)
-		return true
-	else return false end
-end
-function s.operation2(e,tg,ntg,sg,lv,sc,tp)
+
+function s.operation3(e,tg,ntg,sg,lv,sc,tp)
 	return (not sg:IsExists(Card.IsLocation,1,nil,LOCATION_EXTRA)) or sg:GetCount()==2,false
-end
-function s.chk(c)
-	if not c:IsHasEffect(EFFECT_HAND_SYNCHRO+EFFECT_SYNCHRO_CHECK) then return false end
-	local te={c:GetCardEffect(EFFECT_HAND_SYNCHRO+EFFECT_SYNCHRO_CHECK)}
-	for i=1,#te do
-		local e=te[i]
-		if e:GetLabel()~=id then return false end
-	end
-	return true
-end
-function s.chk2(c)
-	if not c:IsHasEffect(EFFECT_HAND_SYNCHRO) or c:IsHasEffect(EFFECT_HAND_SYNCHRO+EFFECT_SYNCHRO_CHECK) then return false end
-	local te={c:GetCardEffect(EFFECT_HAND_SYNCHRO)}
-	for i=1,#te do
-		local e=te[i]
-		if e:GetLabel()==id then return true end
-	end
-	return false
-end
-function s.target2_1(e,c,sg,tg,ntg,tsg,ntsg)
-	if c then
-		local res=true
-		if sg:IsExists(s.chk,1,c) or (not tg:IsExists(s.chk2,1,c) and not ntg:IsExists(s.chk2,1,c)
-			and not sg:IsExists(s.chk2,1,c)) then return false end
-		local trg=tg:Filter(s.chk,nil)
-		local ntrg=ntg:Filter(s.chk,nil)
-		return res,trg,ntrg
-	else
-		return true
-	end
 end
