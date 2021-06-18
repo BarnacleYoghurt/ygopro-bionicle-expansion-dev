fac3be28d92ff8776458052e2cc452e283935086
utso <cccygodev@gmail.com>
2021-06-18 23:35:02 +0200

    Bohrok Confrontation, Invasion, Nest: Update scripts for EDOPro

--- cards-bbts/script/c10100222.lua
+++ cards-bbts/script/c10100222.lua
@@ -1,5 +1,6 @@
 --Bohrok Confrontation
-function c10100222.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--operation1
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
@@ -7,48 +8,48 @@ function c10100222.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(TIMING_DAMAGE_STEP)
-	e1:SetCondition(c10100222.condition1)
-	e1:SetCost(c10100222.cost1)
-	e1:SetTarget(c10100222.target1)
-	e1:SetOperation(c10100222.operation1)
+	e1:SetCondition(s.condition1)
+	e1:SetCost(s.cost1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
 	c:RegisterEffect(e1)
 end
-function c10100222.filter1a(c)
-	return c:IsSetCard(0x15d) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
+function s.filter1a(c)
+	return c:IsSetCard(0xb09) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
 end
-function c10100222.filter1b(c)
-	return c:IsSetCard(0x15c) and c:IsFaceup()
+function s.filter1b(c)
+	return c:IsSetCard(0xb08) and c:IsFaceup()
 end
-function c10100222.condition1(e,tp,eg,ep,ev,re,r,rp)
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
 end
-function c10100222.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100222.filter1a,tp,LOCATION_DECK,0,1,nil) end
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1a,tp,LOCATION_DECK,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c10100222.filter1a,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter1a,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoGrave(g,REASON_EFFECT)
 	end 
 end
-function c10100222.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(c10100222.filter1b,tp,LOCATION_MZONE,0,1,nil) end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(s.filter1b,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c10100222.filter1b,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,s.filter1b,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function c10100222.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and c10100222.filter1b(tc) then
+	if tc:IsRelateToEffect(e) and s.filter1b(tc) then
 		local e1a=Effect.CreateEffect(e:GetHandler())
 		e1a:SetType(EFFECT_TYPE_SINGLE)
 		e1a:SetCode(EFFECT_UPDATE_ATTACK)
 		e1a:SetValue(700)
-		e1a:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e1a:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1a)
 		local e1b=Effect.CreateEffect(e:GetHandler())
 		e1b:SetType(EFFECT_TYPE_SINGLE)
 		e1b:SetCode(EFFECT_UPDATE_DEFENSE)
 		e1b:SetValue(700)
-		e1b:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e1b:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1b)
 	end
 end
--- cards-bbts/script/c10100223.lua
+++ cards-bbts/script/c10100223.lua
@@ -1,67 +1,68 @@
 --Bohrok Invasion
-function c10100223.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e0=Effect.CreateEffect(c)
 	e0:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
-	e0:SetTarget(c10100223.target0)
-	e0:SetOperation(c10100223.operation0)
+	e0:SetTarget(s.target0)
+	e0:SetOperation(s.operation0)
 	c:RegisterEffect(e0)
 	--ATK gain
 	local e1=Effect.CreateEffect(c)
   e1:SetCategory(CATEGORY_ATKCHANGE)
-  e1:SetDescription(aux.Stringid(10100223,0))
+  e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetCode(EVENT_DESTROYED)
-	e1:SetCondition(c10100223.condition1)
-	e1:SetTarget(c10100223.target1)
-	e1:SetOperation(c10100223.operation1)
+	e1:SetCondition(s.condition1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
 	c:RegisterEffect(e1)
 	--DEF gain
 	local e2=Effect.CreateEffect(c)
   e2:SetCategory(CATEGORY_DEFCHANGE)
-  e2:SetDescription(aux.Stringid(10100223,1))
+  e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetHintTiming(TIMING_DAMAGE_STEP)
-	e2:SetCost(c10100223.cost2)
-	e2:SetTarget(c10100223.target2)
-	e2:SetOperation(c10100223.operation2)
+	e2:SetCost(s.cost2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
 	c:RegisterEffect(e2)
 end
-function c10100223.filter0(c,e,tp)
-	return c:IsSetCard(0x15c) and c:GetLevel()==4 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE)
+function s.filter0(c,e,tp)
+	return c:IsSetCard(0xb08) and c:GetLevel()==4 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE)
 end
-function c10100223.target0(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target0(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	e:SetLabel(0)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c10100223.filter0,tp,LOCATION_GRAVE,0,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(10100223,2)) then
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter0,tp,LOCATION_GRAVE,0,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
 		e:SetLabel(1)
-		local g=Duel.GetMatchingGroup(c10100223.filter0,tp,LOCATION_GRAVE,0,nil,e,tp)
+		local g=Duel.GetMatchingGroup(s.filter0,tp,LOCATION_GRAVE,0,nil,e,tp)
 		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 	end
 end
-function c10100223.operation0(e,tp,eg,ep,ev,re,r,rp)
+function s.operation0(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetLabel()>0 then
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,c10100223.filter0,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+		local g=Duel.SelectMatchingCard(tp,s.filter0,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 		if g:GetCount() > 0 then
 			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
 			Duel.ConfirmCards(1-tp,g)
 		end
 	end
 end
-function c10100223.filter1a(c)
-	return c:IsSetCard(0x15c) and c:IsFaceup()
+function s.filter1a(c)
+	return c:IsSetCard(0xb08) and c:IsFaceup()
 end
-function c10100223.filter1b(c,p)
+function s.filter1b(c,p)
 	return c:GetPreviousControler()==p
 end
-function c10100223.condition1(e,tp,eg,ep,ev,re,r,rp)
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
 	local rc=nil
 	if re then
 		rc=re:GetHandler()
@@ -72,44 +73,44 @@ function c10100223.condition1(e,tp,eg,ep,ev,re,r,rp)
 			rc=Duel.GetAttackTarget()
 		end
 	end
-	return rc and eg:IsExists(c10100223.filter1b,1,nil,1-tp) and rc:IsSetCard(0x15c)
+	return rc and eg:IsExists(s.filter1b,1,nil,1-tp) and rc:IsSetCard(0xb08)
 end
-function c10100223.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100223.filter1a,tp,LOCATION_MZONE,0,1,nil) end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1a,tp,LOCATION_MZONE,0,1,nil) end
 end
-function c10100223.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
   if not e:GetHandler():IsRelateToEffect(e) then return end
-	local g=Duel.GetMatchingGroup(c10100223.filter1a,tp,LOCATION_MZONE,0,nil)
+	local g=Duel.GetMatchingGroup(s.filter1a,tp,LOCATION_MZONE,0,nil)
 	local tc=g:GetFirst()
 	while tc do
     local e1=Effect.CreateEffect(e:GetHandler())
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetCode(EFFECT_UPDATE_ATTACK)
     e1:SetValue(400)
-    e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 		tc=g:GetNext()
 	end
 end
-function c10100223.filter2(c)
-	return c:IsSetCard(0x15c) and c:IsFaceup()
+function s.filter2(c)
+	return c:IsSetCard(0xb08) and c:IsFaceup()
 end
-function c10100223.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
-function c10100223.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100223.filter2,tp,LOCATION_MZONE,0,1,nil) end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE,0,1,nil) end
 end
-function c10100223.operation2(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c10100223.filter2,tp,LOCATION_MZONE,0,nil)
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_MZONE,0,nil)
 	local tc=g:GetFirst()
 	while tc do
     local e1=Effect.CreateEffect(e:GetHandler())
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetCode(EFFECT_UPDATE_DEFENSE)
     e1:SetValue(800)
-    e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 		tc=g:GetNext()
 	end
