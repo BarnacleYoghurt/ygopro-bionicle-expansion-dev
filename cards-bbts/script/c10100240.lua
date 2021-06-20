12838ce561bd085ea3f0a6a047595261a8a6b739
utso <cccygodev@gmail.com>
2021-06-20 14:22:22 +0200

    Mata Nui Fishing Bird, Keras: Update scripts for EDOPro

--- cards-bbts/script/c10100240.lua
+++ cards-bbts/script/c10100240.lua
@@ -1,53 +1,54 @@
 --Mata Nui Fishing Bird, Rahi
-function c10100240.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Banish
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_REMOVE)
-	e1:SetDescription(aux.Stringid(10100240,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(c10100240.cost1)
-	e1:SetTarget(c10100240.target1)
-	e1:SetOperation(c10100240.operation1)
-	e1:SetCountLimit(1,10100240)
+	e1:SetCost(s.cost1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
+	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
 end
 
-function c10100240.filter1a(c)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x15a) and c:IsAbleToRemoveAsCost()
+function s.filter1a(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0xb06) and c:IsAbleToRemoveAsCost()
 end
-function c10100240.filter1b(c)
+function s.filter1b(c)
 	return c:IsType(TYPE_MONSTER) and c:IsFaceup() and c:IsAttackAbove(2000)
 end
-function c10100240.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() and Duel.IsExistingMatchingCard(c10100240.filter1a,tp,LOCATION_GRAVE,0,1,nil) end
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() and Duel.IsExistingMatchingCard(s.filter1a,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c10100240.filter1a,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter1a,tp,LOCATION_GRAVE,0,1,1,nil)
 	g:AddCard(e:GetHandler())
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function c10100240.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,nil) end	
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
-	if g:IsExists(c10100240.filter1b,1,nil) and Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_HAND,1,nil) then
+	if g:IsExists(s.filter1b,1,nil) and Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_HAND,1,nil) then
 		local g2=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_HAND,nil)
 		Duel.SetOperationInfo(0,CATEGORY_REMOVE,g2,1,0,0)
 	end
 end
-function c10100240.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-  local cond=c10100240.filter1b(tc)
+  local cond=s.filter1b(tc)
 	if tc:IsRelateToEffect(e) and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT+REASON_TEMPORARY)>0 then
-		tc:RegisterFlagEffect(10100240,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_PHASE+PHASE_END)
-		e1:SetCondition(c10100240.condition1_1)
-		e1:SetOperation(c10100240.operation1_1)
+		e1:SetCondition(s.condition1_1)
+		e1:SetOperation(s.operation1_1)
 		e1:SetLabelObject(tc)
 		e1:SetCountLimit(1)
 		e1:SetReset(RESET_PHASE+PHASE_END)
@@ -61,9 +62,9 @@ function c10100240.operation1(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c10100240.condition1_1(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetLabelObject():GetFlagEffect(10100240)~=0
+function s.condition1_1(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetLabelObject():GetFlagEffect(id)~=0
 end
-function c10100240.operation1_1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation1_1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.ReturnToField(e:GetLabelObject())
 end
--- cards-bbts/script/c10100241.lua
+++ cards-bbts/script/c10100241.lua
@@ -1,42 +1,43 @@
 --Keras, Crab Rahi
-function c10100241.initial_effect(c)
-	aux.EnablePendulumAttribute(c)
+local s,id=GetID()
+function s.initial_effect(c)
+	Pendulum.AddProcedure(c)
 	--ATK up
 	local e1=Effect.CreateEffect(c)
   e1:SetCategory(CATEGORY_DESTROY+CATEGORY_ATKCHANGE)
-	e1:SetDescription(aux.Stringid(10100241,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_PZONE)
 	e1:SetCode(EVENT_BATTLE_START)
-	e1:SetCondition(c10100241.condition1)
-  e1:SetTarget(c10100241.target1)
-	e1:SetOperation(c10100241.operation1)
-	e1:SetCountLimit(1,10100241)
+	e1:SetCondition(s.condition1)
+  e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
+	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
 	--Immune
 	local e2=Effect.CreateEffect(c)
 	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
-	e2:SetDescription(aux.Stringid(10100241,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetTarget(c10100241.target2)
-	e2:SetOperation(c10100241.operation2)
-	e2:SetCountLimit(1,11100241)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+	e2:SetCountLimit(1,id+1000000)
 	c:RegisterEffect(e2)
 	--Destroy
 	local e3=Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_DESTROY)
-	e3:SetDescription(aux.Stringid(10100241,2))
+	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
 	e3:SetCode(EVENT_REMOVE)
-	e3:SetTarget(c10100241.target3)
-	e3:SetOperation(c10100241.operation3)
-	e3:SetCountLimit(1,11100241)
+	e3:SetTarget(s.target3)
+	e3:SetOperation(s.operation3)
+	e3:SetCountLimit(1,id+1000000)
 	c:RegisterEffect(e3)
 end
-function c10100241.condition1(e,tp,eg,ep,ev,re,r,rp)
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
   local at=Duel.GetAttackTarget()
   if not at then return false end
@@ -44,11 +45,11 @@ function c10100241.condition1(e,tp,eg,ep,ev,re,r,rp)
   e:SetLabelObject(a)
   return a:IsControler(tp) and a:IsFaceup() and a:IsAttackBelow(1000)
 end
-function c10100241.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
   if chk==0 then return true end
   Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
 end
-function c10100241.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
   local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.Destroy(c,REASON_EFFECT)>0 then
     local tc=e:GetLabelObject()
@@ -56,43 +57,43 @@ function c10100241.operation1(e,tp,eg,ep,ev,re,r,rp)
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetCode(EFFECT_UPDATE_ATTACK)
     e1:SetValue(1400)
-    e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
     tc:RegisterEffect(e1)
   end
 end
-function c10100241.filter2a(c)
+function s.filter2a(c)
 	return c:IsFaceup() and c:IsLevelBelow(4)
 end
-function c10100241.filter2b(e,te)
+function s.filter2b(e,te)
 	return te:IsActiveType(TYPE_SPELL+TYPE_TRAP)
 end
-function c10100241.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(c10100241.filter2a,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(s.filter2a,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g = Duel.SelectTarget(tp,c10100241.filter2a,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	local g = Duel.SelectTarget(tp,s.filter2a,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
-function c10100241.operation2(e,tp,eg,ep,ev,re,r,rp)
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and c10100241.filter2a(tc) then
+	if tc:IsRelateToEffect(e) and s.filter2a(tc) then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 		e1:SetRange(LOCATION_MZONE)
 		e1:SetCode(EFFECT_IMMUNE_EFFECT)
-		e1:SetValue(c10100241.filter2b)
-		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e1:SetValue(s.filter2b)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	end
 end
-function c10100241.filter3(c)
+function s.filter3(c)
   return c:IsFacedown() and c:IsDestructable()
 end
-function c10100241.target3(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(c10100241.filter3,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(s.filter3,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g = Duel.SelectTarget(tp,c10100241.filter3,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	local g = Duel.SelectTarget(tp,s.filter3,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 end
-function c10100241.operation3(e,tp,eg,ep,ev,re,r,rp)
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
