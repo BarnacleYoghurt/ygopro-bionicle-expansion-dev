ee65a2c1ef5e7d343428b77aa476581fa58e52f9
utso <cccygodev@gmail.com>
2021-06-20 15:56:22 +0200

    Bohrok Servant, Matoran/Turaga/Toa of the Swarm: Update scripts for EDOPro

--- cards-bbts/script/c10100243.lua
+++ cards-bbts/script/c10100243.lua
@@ -1,33 +1,34 @@
 --Matoran of The Swarm
-function c10100243.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
   --Attach from GY
   local e1=Effect.CreateEffect(c)
-  e1:SetDescription(aux.Stringid(10100243,0))
+  e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
   e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetRange(LOCATION_GRAVE)
 	e1:SetCode(EVENT_FLIP)
-	e1:SetCondition(c10100243.condition1)
-	e1:SetOperation(c10100243.operation1)
+	e1:SetCondition(s.condition1)
+	e1:SetOperation(s.operation1)
 	c:RegisterEffect(e1)
   --Attach from hand
   local e2=Effect.CreateEffect(c)
-  e2:SetDescription(aux.Stringid(10100243,1))
+  e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_HAND)
 	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetCost(c10100243.cost2)
-	e2:SetTarget(c10100243.target2)
-	e2:SetOperation(c10100243.operation2)
+	e2:SetCost(s.cost2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
 	c:RegisterEffect(e2)
 end
-function c10100243.condition1(e,tp,eg,ep,ev,re,r,rp)
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
 	if eg:GetCount()==0 then return false end
   local fc=eg:GetFirst()
   return fc:IsType(TYPE_XYZ) and fc:IsControler(tp)
 end
-function c10100243.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
   if eg:GetCount()>0 then
     local c=e:GetHandler()
     local tc=eg:GetFirst()
@@ -36,37 +37,37 @@ function c10100243.operation1(e,tp,eg,ep,ev,re,r,rp)
     end
   end
 end
-function c10100243.filter2a(c)
-  return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x15d) and c:IsAbleToGraveAsCost()
+function s.filter2a(c)
+  return c:IsType(TYPE_MONSTER) and c:IsSetCard(0xb09) and c:IsAbleToGraveAsCost()
 end
-function c10100243.filter2b(c)
+function s.filter2b(c)
   return c:IsType(TYPE_XYZ) and c:IsFaceup()
 end
-function c10100243.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100243.filter2a,tp,LOCATION_DECK,0,1,nil) end
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2a,tp,LOCATION_DECK,0,1,nil) end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c10100243.filter2a,tp,LOCATION_DECK,0,1,1,nil)
-  Duel.SendtoGrave(g, REASON_COST)
+	local g=Duel.SelectMatchingCard(tp,s.filter2a,tp,LOCATION_DECK,0,1,1,nil)
+  Duel.SendtoGrave(g,REASON_COST)
 end
-function c10100243.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return not e:GetHandler():IsStatus(STATUS_CHAINING) and Duel.IsExistingTarget(c10100243.filter2b,tp,0,LOCATION_MZONE,1,nil) end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not e:GetHandler():IsStatus(STATUS_CHAINING) and Duel.IsExistingTarget(s.filter2b,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,c10100243.filter2b,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,s.filter2b,tp,0,LOCATION_MZONE,1,1,nil)
 end
-function c10100243.operation2(e,tp,eg,ep,ev,re,r,rp)
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
   local c=e:GetHandler()
   local tc=Duel.GetFirstTarget()
   if tc:IsRelateToEffect(e) and c:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
     Duel.Overlay(tc,Group.FromCards(c))
     local e1=Effect.CreateEffect(tc)
-    e1:SetDescription(aux.Stringid(10100243,2))
+    e1:SetDescription(aux.Stringid(id,2))
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-    e1:SetRange(RANGE_MZONE)
+    e1:SetRange(LOCATION_MZONE)
     e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-    e1:SetCondition(c10100243.condition2_1)
+    e1:SetCondition(s.condition2_1)
     e1:SetValue(0)
-    e1:SetReset(RESET_EVENT+0x1fe0000)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
     tc:RegisterEffect(e1,true)
     local e1b=e1:Clone()
     e1b:SetCode(EFFECT_SET_DEFENSE_FINAL)
@@ -76,18 +77,18 @@ function c10100243.operation2(e,tp,eg,ep,ev,re,r,rp)
       e2:SetType(EFFECT_TYPE_SINGLE)
       e2:SetCode(EFFECT_CHANGE_TYPE)
       e2:SetValue(tc:GetType()+TYPE_EFFECT)
-      e2:SetReset(RESET_EVENT+0x1fe0000)
+      e2:SetReset(RESET_EVENT+RESETS_STANDARD)
       tc:RegisterEffect(e2)
     end
     local e3=Effect.CreateEffect(c)
     e3:SetType(EFFECT_TYPE_SINGLE)
     e3:SetCode(EFFECT_CANNOT_TRIGGER)
-    e3:SetReset(RESET_EVENT+0x1fe0000)
+    e3:SetReset(RESET_EVENT+RESETS_STANDARD)
     tc:RegisterEffect(e3)
   end
 end
-function c10100243.condition2_1(e)
+function s.condition2_1(e)
 	local ph=Duel.GetCurrentPhase()
 	local bc=e:GetHandler():GetBattleTarget()
-	return (ph==PHASE_DAMAGE or ph==PHASE_DAMAGE_CAL) and bc and bc:IsSetCard(0x15c)
+	return (ph==PHASE_DAMAGE or ph==PHASE_DAMAGE_CAL) and bc and bc:IsSetCard(0xb08)
 end
--- cards-bbts/script/c10100244.lua
+++ cards-bbts/script/c10100244.lua
@@ -1,50 +1,51 @@
 --Turaga of The Swarm
-function c10100244.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
   --synchro summon
   local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
-  e1:SetDescription(aux.Stringid(10100244,0))
+  e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_HAND)
   e1:SetCode(EVENT_FREE_CHAIN)
   e1:SetHintTiming(TIMING_SUMMON+TIMING_SPSUMMON)
-	e1:SetCost(c10100244.cost1)
-	e1:SetTarget(c10100244.target1)
-	e1:SetOperation(c10100244.operation1)
+	e1:SetCost(s.cost1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
   e1:SetCountLimit(1)
 	c:RegisterEffect(e1)
   --take control
   local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_CONTROL)
-  e2:SetDescription(aux.Stringid(10100244,1))
+  e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
   e2:SetCode(EVENT_BE_MATERIAL)
-	e2:SetCondition(c10100244.condition2)
-	e2:SetTarget(c10100244.target2)
-	e2:SetOperation(c10100244.operation2)
+	e2:SetCondition(s.condition2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
 	c:RegisterEffect(e2)
 end
-function c10100244.filter1a(c)
-  return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x15d) and c:IsAbleToGraveAsCost()
+function s.filter1a(c)
+  return c:IsType(TYPE_MONSTER) and c:IsSetCard(0xb09) and c:IsAbleToGraveAsCost()
 end
-function c10100244.filter1b(c)
+function s.filter1b(c)
   return c:IsType(TYPE_TUNER) and c:IsFaceup() and c:IsAbleToRemove()
 end
-function c10100244.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100244.filter1a,tp,LOCATION_DECK,0,1,nil) end
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1a,tp,LOCATION_DECK,0,1,nil) end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c10100244.filter1a,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter1a,tp,LOCATION_DECK,0,1,1,nil)
   Duel.SendtoGrave(g,REASON_COST)
 end
-function c10100244.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(c10100244.filter1b,tp,0,LOCATION_MZONE,1,nil) and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,1-tp) end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(s.filter1b,tp,0,LOCATION_MZONE,1,nil) and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,1-tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectTarget(tp,c10100244.filter1b,tp,0,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.filter1b,tp,0,LOCATION_MZONE,1,1,nil)
   Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,LOCATION_HAND)
 end
-function c10100244.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
   local c=e:GetHandler()
   local tc=Duel.GetFirstTarget()
   if tc:IsRelateToEffect(e) then 
@@ -61,29 +62,29 @@ function c10100244.operation1(e,tp,eg,ep,ev,re,r,rp)
     end
   end
 end
-function c10100244.condition2(e,tp,eg,ep,ev,re,r,rp)
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsLocation(LOCATION_GRAVE) and r==REASON_SYNCHRO
 end
-function c10100244.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	local rc=e:GetHandler():GetReasonCard()
   if chk==0 then return true end
   Duel.SetOperationInfo(0,CATEGORY_CONTROL,rc,1,0,0)
 end
-function c10100244.operation2(e,tp,eg,ep,ev,re,r,rp)
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local rc=e:GetHandler():GetReasonCard()
   
   if rc:IsFaceup() and rc:IsLocation(LOCATION_MZONE) then
     Duel.GetControl(rc,tp,PHASE_END,1)
     
     local e1=Effect.CreateEffect(rc)
-    e1:SetDescription(aux.Stringid(10100244,2))
+    e1:SetDescription(aux.Stringid(id,2))
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-    e1:SetRange(RANGE_MZONE)
+    e1:SetRange(LOCATION_MZONE)
     e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-    e1:SetCondition(c10100244.condition2_1)
+    e1:SetCondition(s.condition2_1)
     e1:SetValue(0)
-    e1:SetReset(RESET_EVENT+0x1fe0000)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
     rc:RegisterEffect(e1,true)
     local e1b=e1:Clone()
     e1b:SetCode(EFFECT_SET_DEFENSE_FINAL)
@@ -93,13 +94,13 @@ function c10100244.operation2(e,tp,eg,ep,ev,re,r,rp)
       e2:SetType(EFFECT_TYPE_SINGLE)
       e2:SetCode(EFFECT_CHANGE_TYPE)
       e2:SetValue(rc:GetType()+TYPE_EFFECT)
-      e2:SetReset(RESET_EVENT+0x1fe0000)
+      e2:SetReset(RESET_EVENT+RESETS_STANDARD)
       rc:RegisterEffect(e2)
     end
   end
 end
-function c10100244.condition2_1(e)
+function s.condition2_1(e)
 	local ph=Duel.GetCurrentPhase()
 	local bc=e:GetHandler():GetBattleTarget()
-	return (ph==PHASE_DAMAGE or ph==PHASE_DAMAGE_CAL) and bc and bc:IsSetCard(0x15c)
+	return (ph==PHASE_DAMAGE or ph==PHASE_DAMAGE_CAL) and bc and bc:IsSetCard(0xb08)
 end
