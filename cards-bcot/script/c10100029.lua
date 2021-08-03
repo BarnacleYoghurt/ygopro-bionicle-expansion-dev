1ec2f79b76545d602cd663fc904acb5703f668cc
utso <cccygodev@gmail.com>
2021-08-04 00:22:57 +0200

    Begin implementing Ko-Koro

--- cards-bcot/script/c10100029.lua
+++ cards-bcot/script/c10100029.lua
@@ -1,84 +1,113 @@
 --Ko-Koro, Village of Ice
-function c10100029.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e0=Effect.CreateEffect(c)
-	e0:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
-	e0:SetOperation(c10100029.operation0)
 	c:RegisterEffect(e0)
-	--1 less tribute for WATER
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_DECREASE_TRIBUTE)
-	e1:SetRange(LOCATION_SZONE)
-	e1:SetTargetRange(LOCATION_HAND,LOCATION_HAND)
-	e1:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_WATER))
-	e1:SetValue(1)
-	c:RegisterEffect(e1)
-	--Gain LP
+	--No attacks
+  local e1=Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_FIELD)
+  e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+  e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+  e1:SetRange(LOCATION_SZONE)
+  e1:SetTargetRange(0,1)
+  e1:SetCondition(s.condition1)
+  c:RegisterEffect(e1)
+  --Protection
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(10100029,0))
-	e2:SetCategory(CATEGORY_RECOVER+CATEGORY_ATKCHANGE)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetType(EFFECT_TYPE_FIELD)
+  e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+  e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetCode(EVENT_DAMAGE)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetCondition(c10100029.condition2)
-	e2:SetCost(c10100029.cost2)
-	e2:SetTarget(c10100029.target2)
-	e2:SetOperation(c10100029.operation2)
-	e2:SetCountLimit(1)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+  e2:SetCondition(s.condition2)
+	e2:SetValue(aux.indoval)
 	c:RegisterEffect(e2)
+  
+  aux.GlobalCheck(s,function()
+    --Attacks this turn
+		s[0]=0
+		s[1]=0
+    --Attacks last turn
+		s[2]=0
+		s[3]=0
+    --Removed monsters + effect damage this turn (SP to EP)
+    s[4]=0
+    s[5]=0
+    --Removed monsters + effect damage last turn (SP to EP +DP of this turn)
+    s[6]=0
+    s[7]=0
+    --Removed monsters + effect damage two turns ago (SP to EP +DP of last turn)
+    s[8]=0
+    s[9]=0
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ATTACK_ANNOUNCE)
+		ge1:SetOperation(s.checkop_attacks)
+		Duel.RegisterEffect(ge1,0)
+    local ge2=Effect.CreateEffect(c)
+		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge2:SetCode(EVENT_LEAVE_FIELD)
+		ge2:SetOperation(s.checkop_leave)
+		Duel.RegisterEffect(ge2,0)
+		aux.AddValuesReset(function()
+			local p=Duel.GetTurnPlayer()
+			s[p+2]=s[p]
+			s[p]=0
+      s[4+p+4]=s[4+p+2]
+      s[4+p+2]=s[4+p]
+			s[4+p]=0
+      Debug.Message("Reset done")
+		end)
+	end)
 end
---e0 - Activate
-function c10100029.filter0a(c)
-	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER)
+function s.checkop_attacks(e,tp,eg,ep,ev,re,r,rp)
+	s[ep]=s[ep]+1
 end
-function c10100029.filter0b(c)
-	return c:IsCode(10100005) and c:IsAbleToHand()
+function s.checkfilter_leave(c,p)
+  return c:IsPreviousControler(p) and c:IsReason(REASON_EFFECT)
 end
-function c10100029.operation0(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tc=Duel.GetFirstMatchingCard(c10100029.filter0b,tp,LOCATION_DECK,0,nil)
-	if Duel.IsExistingMatchingCard(c10100029.filter0a,tp,LOCATION_MZONE,0,1,nil) and tc then
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,tc)
-	end
+function s.checkop_leave(e,tp,eg,ep,ev,re,r,rp)
+  if rp==tp then
+    local ct=eg:FilterCount(s.checkfilter_leave,nil,1-tp)
+    if Duel.GetCurrentPhase()==PHASE_DRAW then
+      s[4+tp+2]=s[4+tp+2]+ct
+    else
+      Debug.Message("count up")
+      s[4+tp]=s[4+tp]+ct
+    end
+  end
 end
---e2 - Gain LP
-function c10100029.filter2a(c)
-	return c:IsType(TYPE_EQUIP) and c:IsSetCard(0x158) and c:IsAbleToRemoveAsCost()
+
+function s.filter(c)
+  return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER)
 end
-function c10100029.filter2b(c)
-	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER)
+function s.condition(e)
+  local tp=e:GetHandlerPlayer()
+  local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+  return g:GetCount()>0 and g:GetCount()==g:FilterCount(s.filter,nil)
 end
-function c10100029.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return ep==tp
+function s.condition1(e)
+  local tp=e:GetHandlerPlayer()
+  return s.condition(e) and s[tp+2]==0
 end
-function c10100029.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100029.filter2a,tp,LOCATION_GRAVE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c10100029.filter2a,tp,LOCATION_GRAVE,0,1,1,nil)
-	Duel.Remove(g,POS_FACEUP,REASON_COST)
-end
-function c10100029.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,ev)
-	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,nil,1,0,ev)
-end
-function c10100029.operation2(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.Recover(tp,ev,REASON_EFFECT) then
-		Duel.BreakEffect()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-		local g=Duel.SelectMatchingCard(tp,c10100029.filter2b,tp,LOCATION_MZONE,0,1,1,nil)
-		if g:GetCount()>0 then
-			local tc=g:GetFirst()
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(ev)
-			e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-			tc:RegisterEffect(e1)
-		end
-	end
+function s.condition2(e)
+  local tp=e:GetHandlerPlayer()
+  local sum
+  if Duel.GetTurnPlayer()~=tp then
+    Debug.Message("opp turn")
+    sum=s[4+tp]+s[4+tp+2]
+    Debug.Message(sum)
+  elseif Duel.GetCurrentPhase()==PHASE_DRAW then
+    Debug.Message("own turn - pre-SP")
+    sum=s[4+tp]+s[4+tp+2]+s[4+tp+4]
+    Debug.Message(sum)
+  else
+    Debug.Message("own turn - post-SP")
+    sum=s[4+tp]
+    Debug.Message(sum)
+  end
+  return s.condition(e) and sum==0
 end
