24a504fa3521fded683d863b3245379e47c8087a
utso <cccygodev@gmail.com>
2021-08-03 09:09:23 +0200

    Implement Kopaka

--- cards-bcot/script/c10100005.lua
+++ cards-bcot/script/c10100005.lua
@@ -1,51 +1,63 @@
+if not bcot then
+	dofile "expansions/util-bcot.lua"
+end
 --Toa Mata Kopaka
-function c10100005.initial_effect(c)
-	--Equip
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100005,0))
-	e1:SetCategory(CATEGORY_EQUIP)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetTarget(c10100005.target1)
-	e1:SetOperation(c10100005.operation1)
+local s,id=GetID()
+function s.initial_effect(c)
+  --Tribute from hand
+  local e1=bcot.toa_mata_tribute(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	c:RegisterEffect(e1)
-	--No destruction
+	--Strike bacc & protecc
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(10100005,1))
-	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+  e2:SetDescription(aux.Stringid(id,1))
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_LEAVE_FIELD)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetOperation(c10100005.operation2)
+	e2:SetCondition(s.condition2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+  e2:SetCountLimit(1)
 	c:RegisterEffect(e2)
 end
---e1 - Equip
-function c10100005.filter1(c,ec)
-	return c:IsType(TYPE_EQUIP)	and c:IsCode(10100011) and c:CheckEquipTarget(ec)
+function s.filter2(c,tp)
+  return c:IsPreviousControler(tp) and c:IsReason(REASON_EFFECT)
 end
-function c10100005.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingMatchingCard(c10100005.filter1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e:GetHandler()) end
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_DECK)
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+  return rp==1-tp and eg:IsExists(s.filter2,1,nil,tp)
 end
-function c10100005.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or c:IsFacedown() or not c:IsRelateToEffect(e) then return end
-	Duel.Hint(HINT_SELECTMSG,tp,nil)
-	local g=Duel.SelectMatchingCard(tp,c10100005.filter1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,c)
-	if g:GetCount()>0 and g:GetFirst():CheckEquipTarget(c) then
-		Duel.Equip(tp,g:GetFirst(),c)
-	end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  local c=e:GetHandler()
+  if chk==0 then return c:IsPosition(POS_FACEUP_ATTACK) and c:IsCanChangePosition() end
 end
---e2 - No destruction
-function c10100005.operation2(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+  local c=e:GetHandler()
+  if Duel.ChangePosition(c,POS_FACEUP_DEFENSE) > 0 then
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local tg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+    if tg:GetCount()>0 then
+      Duel.Remove(tg,POS_FACEUP,REASON_EFFECT)
+    end
+  end
+  
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_INDESTRUCTABLE)
-	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e1:SetTarget(aux.TRUE)
+	e1:SetTargetRange(LOCATION_ONFIELD,0)
+  e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+  e1:SetCondition(s.condition2_1)
+	e1:SetTarget(s.target2_1)
 	e1:SetValue(1)
+  e1:SetLabelObject(c)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
+  c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+end
+function s.condition2_1(e)
+  local c=e:GetLabelObject()
+  return c:GetFlagEffect(id)~=0 and c:IsPosition(POS_FACEUP_DEFENSE)
+end
+function s.target2_1(e,c)
+  return c~=e:GetHandler()
 end
