9c1bec0ce1344f533496da3c7997848c0778a6df
utso <cccygodev@gmail.com>
2021-08-04 20:45:17 +0200

    Implement Kopeke

--- cards-bcot/script/c10100035.lua
+++ cards-bcot/script/c10100035.lua
@@ -1,47 +1,62 @@
 --C.C. Matoran Kopeke
-function c10100035.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Search
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100035,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+  e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetTarget(c10100035.target1)
-	e1:SetOperation(c10100035.operation1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
 	c:RegisterEffect(e1)
+  local e1b=e1:Clone()
+  e1b:SetCode(EVENT_FLIP)
+  c:RegisterEffect(e1b)
 	--To Defense
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_DESTROY_REPLACE)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTarget(c10100035.condition2)
-	e2:SetOperation(c10100035.operation2)
+	e2:SetTarget(s.target2)
+  e2:SetValue(s.value2)
+	e2:SetOperation(s.operation2)
 	c:RegisterEffect(e2)
 end
---e1 - Search
-function c10100035.filter1(c)
-	return c:GetLevel()==2 and c:IsRace(RACE_WARRIOR) and c:IsAbleToHand()
+function s.filter1(c)
+	return c:IsLevel(2) and c:IsRace(RACE_WARRIOR) and not c:IsCode(id) and c:IsAbleToHand()
 end
-function c10100035.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100035.filter1,tp,LOCATION_DECK,0,1,nil) end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c10100035.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c10100035.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+		if Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+      Duel.ConfirmCards(1-tp,g)
+      Duel.BreakEffect()
+      Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+      local rg=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
+      Duel.SendtoDeck(rg,nil,0,REASON_COST)
+    end
 	end
 end
---e2 - To Defense
-function c10100035.condition2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsFaceup() and e:GetHandler():IsAttackPos() end
-	return Duel.SelectYesNo(tp,aux.Stringid(10100035,1))
+function s.filter2(c,tp)
+  return c:IsLocation(LOCATION_MZONE) and c:IsControler(tp) and c:IsFaceup() and c:IsSetCard(0x1b01) and not c:IsReason(REASON_REPLACE)
 end
-function c10100035.operation2(e,tp,eg,ep,ev,re,r,rp)
-	Duel.ChangePosition(e:GetHandler(),POS_FACEUP_DEFENSE)
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  local c=e:GetHandler()
+	if chk==0 then return c:IsFaceup() and c:IsCanTurnSet() and eg:IsExists(s.filter2,1,nil,tp) and not eg:IsContains(c) end
+	return Duel.SelectYesNo(tp,aux.Stringid(id,1))
+end
+function s.value2(e,c)
+	return s.filter2(c,e:GetHandlerPlayer())
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChangePosition(e:GetHandler(),POS_FACEDOWN_DEFENSE)
 end
