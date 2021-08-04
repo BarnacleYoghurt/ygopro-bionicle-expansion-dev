b4046cebb93a3892482b1a9332f88a17bdd15708
utso <cccygodev@gmail.com>
2021-08-04 08:11:18 +0200

    Ko-Koro: Fully implement effect protection conditions

--- cards-bcot/script/c10100029.lua
+++ cards-bcot/script/c10100029.lua
@@ -16,15 +16,19 @@ function s.initial_effect(c)
   e1:SetCondition(s.condition1)
   c:RegisterEffect(e1)
   --Protection
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-  e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-  e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
-  e2:SetCondition(s.condition2)
-	e2:SetValue(aux.indoval)
-	c:RegisterEffect(e2)
+	local e2a=Effect.CreateEffect(c)
+	e2a:SetType(EFFECT_TYPE_FIELD)
+  e2a:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+  e2a:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2a:SetRange(LOCATION_SZONE)
+	e2a:SetTargetRange(LOCATION_MZONE,0)
+  e2a:SetCondition(s.condition2)
+	e2a:SetValue(aux.indoval)
+	c:RegisterEffect(e2a)
+	local e2b=e2a:Clone()
+  e2b:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+  e2b:SetValue(aux.tgoval)
+  c:RegisterEffect(e2b)
   
   aux.GlobalCheck(s,function()
     --Attacks this turn
@@ -52,14 +56,19 @@ function s.initial_effect(c)
 		ge2:SetCode(EVENT_LEAVE_FIELD)
 		ge2:SetOperation(s.checkop_leave)
 		Duel.RegisterEffect(ge2,0)
+    local ge3=Effect.CreateEffect(c)
+    ge3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    ge3:SetCode(EVENT_DAMAGE)
+    ge3:SetOperation(s.checkop_damage)
+    Duel.RegisterEffect(ge3,0)
 		aux.AddValuesReset(function()
-			local p=Duel.GetTurnPlayer()
-			s[p+2]=s[p]
-			s[p]=0
-      s[4+p+4]=s[4+p+2]
-      s[4+p+2]=s[4+p]
-			s[4+p]=0
-      Debug.Message("Reset done")
+			for p=0,1 do
+        s[p+2]=s[p]
+        s[p]=0
+        s[4+p+4]=s[4+p+2]
+        s[4+p+2]=s[4+p]
+        s[4+p]=0
+      end
 		end)
 	end)
 end
@@ -75,8 +84,16 @@ function s.checkop_leave(e,tp,eg,ep,ev,re,r,rp)
     if Duel.GetCurrentPhase()==PHASE_DRAW then
       s[4+tp+2]=s[4+tp+2]+ct
     else
-      Debug.Message("count up")
       s[4+tp]=s[4+tp]+ct
+    end
+  end
+end
+function s.checkop_damage(e,tp,eg,ep,ev,re,r,rp)
+  if ep~=tp and (r&REASON_EFFECT)~=0 then
+    if Duel.GetCurrentPhase()==PHASE_DRAW then
+      s[4+tp+2]=s[4+tp+2]+1
+    else
+      s[4+tp]=s[4+tp]+1
     end
   end
 end
@@ -97,17 +114,11 @@ function s.condition2(e)
   local tp=e:GetHandlerPlayer()
   local sum
   if Duel.GetTurnPlayer()~=tp then
-    Debug.Message("opp turn")
     sum=s[4+tp]+s[4+tp+2]
-    Debug.Message(sum)
   elseif Duel.GetCurrentPhase()==PHASE_DRAW then
-    Debug.Message("own turn - pre-SP")
     sum=s[4+tp]+s[4+tp+2]+s[4+tp+4]
-    Debug.Message(sum)
   else
-    Debug.Message("own turn - post-SP")
     sum=s[4+tp]
-    Debug.Message(sum)
   end
   return s.condition(e) and sum==0
 end
