e1950c355bf5fcdaa656877eb7f23015a0621f03
utso <cccygodev@gmail.com>
2021-08-07 23:39:25 +0200

    Ko-Koro: Targeting protection now grouped with attack protection, destruction protection has weaker conditions

--- cards-bcot/script/c10100029.lua
+++ cards-bcot/script/c10100029.lua
@@ -6,29 +6,30 @@ function s.initial_effect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
-	--No attacks
-  local e1=Effect.CreateEffect(c)
-  e1:SetType(EFFECT_TYPE_FIELD)
-  e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-  e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
-  e1:SetRange(LOCATION_SZONE)
-  e1:SetTargetRange(0,1)
-  e1:SetCondition(s.condition1)
-  c:RegisterEffect(e1)
-  --Protection
-	local e2a=Effect.CreateEffect(c)
-	e2a:SetType(EFFECT_TYPE_FIELD)
-  e2a:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-  e2a:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e2a:SetRange(LOCATION_SZONE)
-	e2a:SetTargetRange(LOCATION_MZONE,0)
-  e2a:SetCondition(s.condition2)
-	e2a:SetValue(aux.indoval)
-	c:RegisterEffect(e2a)
-	local e2b=e2a:Clone()
-  e2b:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-  e2b:SetValue(aux.tgoval)
-  c:RegisterEffect(e2b)
+	--No attacks/targeting
+  local e1a=Effect.CreateEffect(c)
+  e1a:SetType(EFFECT_TYPE_FIELD)
+  e1a:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+  e1a:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+  e1a:SetRange(LOCATION_SZONE)
+  e1a:SetTargetRange(LOCATION_MZONE,0)
+  e1a:SetCondition(s.condition1)
+  e1a:SetValue(aux.imval2)
+  c:RegisterEffect(e1a)
+	local e1b=e1a:Clone()
+  e1b:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+  e1b:SetValue(aux.tgoval)
+  c:RegisterEffect(e1b)
+  --Destruction protection
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+  e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+  e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+  e2:SetCondition(s.condition2)
+	e2:SetValue(aux.indoval)
+	c:RegisterEffect(e2)
   --Negate
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
@@ -47,13 +48,13 @@ function s.initial_effect(c)
     --Attacks last turn
 		s[2]=0
 		s[3]=0
-    --Removed monsters + effect damage this turn (SP to EP)
+    --Destroyed/banished monsters this turn (SP to EP)
     s[4]=0
     s[5]=0
-    --Removed monsters + effect damage last turn (SP to EP +DP of this turn)
+    --Destroyed/banished monsters last turn (SP to EP +DP of this turn)
     s[6]=0
     s[7]=0
-    --Removed monsters + effect damage two turns ago (SP to EP +DP of last turn)
+    --Destroyed/banished monsters two turns ago (SP to EP +DP of last turn)
     s[8]=0
     s[9]=0
 		local ge1=Effect.CreateEffect(c)
@@ -63,13 +64,13 @@ function s.initial_effect(c)
 		Duel.RegisterEffect(ge1,0)
     local ge2=Effect.CreateEffect(c)
 		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_LEAVE_FIELD)
+		ge2:SetCode(EVENT_DESTROYED)
 		ge2:SetOperation(s.checkop_leave)
 		Duel.RegisterEffect(ge2,0)
     local ge3=Effect.CreateEffect(c)
     ge3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-    ge3:SetCode(EVENT_DAMAGE)
-    ge3:SetOperation(s.checkop_damage)
+    ge3:SetCode(EVENT_REMOVE)
+    ge3:SetOperation(s.checkop_leave)
     Duel.RegisterEffect(ge3,0)
 		aux.AddValuesReset(function()
 			for p=0,1 do
@@ -98,15 +99,6 @@ function s.checkop_leave(e,tp,eg,ep,ev,re,r,rp)
       s[4+tp+2]=s[4+tp+2]+ct
     else
       s[4+tp]=s[4+tp]+ct
-    end
-  end
-end
-function s.checkop_damage(e,tp,eg,ep,ev,re,r,rp)
-  if ep~=tp and (r&REASON_EFFECT)~=0 then
-    if Duel.GetCurrentPhase()==PHASE_DRAW then
-      s[4+tp+2]=s[4+tp+2]+1
-    else
-      s[4+tp]=s[4+tp]+1
     end
   end
 end
