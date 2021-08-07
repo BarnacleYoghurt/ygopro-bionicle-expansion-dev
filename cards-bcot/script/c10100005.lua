4d3ded0980f27243bfcb4995ceb7ea22bb07e303
utso <cccygodev@gmail.com>
2021-08-07 22:10:10 +0200

    Kopaka: Implement attack protection and simpler trigger effect

--- cards-bcot/script/c10100005.lua
+++ cards-bcot/script/c10100005.lua
@@ -8,56 +8,65 @@ function s.initial_effect(c)
   local e1=bcot.toa_mata_tribute(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	c:RegisterEffect(e1)
-	--Strike bacc & protecc
-	local e2=Effect.CreateEffect(c)
-  e2:SetDescription(aux.Stringid(id,1))
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_LEAVE_FIELD)
+  --Change position
+  local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_PHASE+PHASE_BATTLE)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
 	e2:SetCondition(s.condition2)
-	e2:SetTarget(s.target2)
 	e2:SetOperation(s.operation2)
-  e2:SetCountLimit(1)
 	c:RegisterEffect(e2)
+  --Attack protection
+  local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e3:SetCondition(s.condition3)
+	e3:SetValue(s.value3)
+	c:RegisterEffect(e3)
+	--Strike bacc & protecc
+	local e4=Effect.CreateEffect(c)
+  e4:SetDescription(aux.Stringid(id,1))
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(s.condition4)
+	e4:SetTarget(s.target4)
+	e4:SetOperation(s.operation4)
+  e4:SetCountLimit(1)
+	c:RegisterEffect(e4)
+end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetAttackedCount()>0
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsAttackPos() then
+		Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+	end
 end
 function s.filter2(c,tp)
   return c:IsPreviousControler(tp) and c:IsReason(REASON_EFFECT)
 end
-function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+function s.condition3(e)
+	return e:GetHandler():IsDefensePos()
+end
+function s.value3(e,c)
+	return c~=e:GetHandler()
+end
+function s.condition4(e,tp,eg,ep,ev,re,r,rp)
   return rp==1-tp and eg:IsExists(s.filter2,1,nil,tp)
 end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-  local c=e:GetHandler()
-  if chk==0 then return c:IsPosition(POS_FACEUP_ATTACK) and c:IsCanChangePosition() end
+function s.target4(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,nil) end
 end
-function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-  local c=e:GetHandler()
-  if Duel.ChangePosition(c,POS_FACEUP_DEFENSE) > 0 then
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local tg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
-    if tg:GetCount()>0 then
-      Duel.Remove(tg,POS_FACEUP,REASON_EFFECT)
-    end
+function s.operation4(e,tp,eg,ep,ev,re,r,rp)
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+  local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,1,nil)
+  if g:GetCount()>0 then
+    Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
   end
-  
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_INDESTRUCTABLE)
-	e1:SetTargetRange(LOCATION_ONFIELD,0)
-  e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-  e1:SetCondition(s.condition2_1)
-	e1:SetTarget(s.target2_1)
-	e1:SetValue(1)
-  e1:SetLabelObject(c)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-  c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
-end
-function s.condition2_1(e)
-  local c=e:GetLabelObject()
-  return c:GetFlagEffect(id)~=0 and c:IsPosition(POS_FACEUP_DEFENSE)
-end
-function s.target2_1(e,c)
-  return c~=e:GetHandler()
 end
