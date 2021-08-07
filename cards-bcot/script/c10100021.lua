2b99dfc0c4fec4e62eb810e1416be2516a509231
utso <cccygodev@gmail.com>
2021-08-07 22:53:50 +0200

    Nuju: Convert flipping effect to bouncing effect

--- cards-bcot/script/c10100021.lua
+++ cards-bcot/script/c10100021.lua
@@ -4,7 +4,7 @@ function s.initial_effect(c)
 	--Link Summon
 	Link.AddProcedure(c,nil,2,2,s.check0)
 	c:EnableReviveLimit()
-	--Flip
+	--Flip/Bounce
   local e1=Effect.CreateEffect(c)
   e1:SetDescription(aux.Stringid(id,0))
   e1:SetType(EFFECT_TYPE_IGNITION)
@@ -33,29 +33,19 @@ end
 function s.filter1(c)
   return c:IsFaceup() and c:IsCanTurnSet()
 end
-function s.rescon1(sg,e,tp,mg)
-  return sg:IsExists(Card.IsControler,1,nil,tp) and sg:IsExists(Card.IsControler,1,nil,1-tp)
-end
-function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsAbleToHand() end
 	local rg=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	if chk==0 then return aux.SelectUnselectGroup(rg,e,tp,2,2,s.rescon1,0) end
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,rg,2,0,0)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingTarget(Card.IsAbleToHand,tp,0,LOCATION_MZONE,1,nil) end
+	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_MZONE,1,1,nil)
+  Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local rg=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	local g=aux.SelectUnselectGroup(rg,e,tp,2,2,s.rescon1,1,tp,HINTMSG_POSCHANGE)
-	if #g==2 then
-		Duel.HintSelection(g,true)
-		Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
-    for tc in aux.Next(g) do
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetDescription(3313)
-			e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
-			tc:RegisterEffect(e1)
-		end
+	local tc=Duel.GetFirstTarget()
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+  local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil)
+	if Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)>0 and tc:IsRelateToEffect(e) then
+    Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
 end
 function s.filter2(c)
