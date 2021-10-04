b9147ee5848ece58e30700d8e5bda2f57a4a5880
utso <cccygodev@gmail.com>
2021-10-04 16:37:36 +0200

    Implement Lewa

--- cards-bcot/script/c10100006.lua
+++ cards-bcot/script/c10100006.lua
@@ -1,53 +1,45 @@
+if not bcot then
+	dofile "expansions/util-bcot.lua"
+end
 --Toa Mata Lewa
-function c10100006.initial_effect(c)
-	--Equip
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100006,0))
-	e1:SetCategory(CATEGORY_EQUIP)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetTarget(c10100006.target1)
-	e1:SetOperation(c10100006.operation1)
+local s,id=GetID()
+function s.initial_effect(c)
+  --Tribute from hand
+  local e1=bcot.toa_mata_tribute(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	c:RegisterEffect(e1)
-	--Draw
-	local e2=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100006,1))
-	e2:SetCategory(CATEGORY_DRAW)
-	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetTarget(c10100006.target2)
-	e2:SetOperation(c10100006.operation2)
-	c:RegisterEffect(e2)
+	--Bounce
+  local e2=Effect.CreateEffect(c)
+  e2:SetDescription(aux.Stringid(id,1))
+  e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+  e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+  e2:SetRange(LOCATION_MZONE)
+  e2:SetCondition(s.condition2)
+  e2:SetTarget(s.target2)
+  e2:SetOperation(s.operation2)
+  e2:SetCountLimit(1)
+  c:RegisterEffect(e2)
 end
---e1 - Equip
-function c10100006.filter1(c,ec)
-	return c:IsType(TYPE_EQUIP)	and c:IsCode(10100012) and c:CheckEquipTarget(ec)
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+  return eg:IsExists(Card.IsSummonLocation,1,nil,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK)
 end
-function c10100006.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingMatchingCard(c10100006.filter1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e:GetHandler()) end
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_DECK)
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+  local g1=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+  Duel.SetOperationInfo(0,CATEGORY_TOHAND,g1,1,0,0)
 end
-function c10100006.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or c:IsFacedown() or not c:IsRelateToEffect(e) then return end
-	Duel.Hint(HINT_SELECTMSG,tp,nil)
-	local g=Duel.SelectMatchingCard(tp,c10100006.filter1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,c)
-	if g:GetCount()>0 and g:GetFirst():CheckEquipTarget(c) then
-		Duel.Equip(tp,g:GetFirst(),c)
-	end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+  local tc=Duel.GetFirstTarget()
+  if tc:IsRelateToEffect(e) then
+    if Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsPreviousControler(tp) then
+      Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+      local g2=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+      if g2:GetCount()>0 then
+        Duel.SendtoHand(g2,nil,REASON_EFFECT)
+      end
+    end
+  end
 end
---e2 - Draw
-function c10100006.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(1)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-end
-function c10100006.operation2(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Draw(p,d,REASON_EFFECT)
-end
+    
