496642dba43cea77e58f0a5bbd1bc5481642cf1b
utso <cccygodev@gmail.com>
2021-10-04 21:36:12 +0200

    Implement Turaga Matau

--- cards-bcot/script/c10100022.lua
+++ cards-bcot/script/c10100022.lua
@@ -1,57 +1,74 @@
 --Turaga Matau
-function c10100022.initial_effect(c)
-	--Equip
+local s,id=GetID()
+function s.initial_effect(c)
+	--Link Summon
+	Link.AddProcedure(c,nil,2,2,s.check0)
+	c:EnableReviveLimit()
+	--Special Summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100022,0))
-	e1:SetCategory(CATEGORY_EQUIP)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetTarget(c10100022.target1)
-	e1:SetOperation(c10100022.operation1)
-	c:RegisterEffect(e1)
-	local e1a=e1:Clone()
-	e1a:SetCode(EVENT_SPSUMMON_SUCCESS)
-	c:RegisterEffect(e1a)
-	local e1b=e1:Clone()
-	e1b:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-	c:RegisterEffect(e1b)
-	--Draw
+	e1:SetDescription(aux.Stringid(id,0))
+  e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e1:SetType(EFFECT_TYPE_IGNITION)
+  e1:SetRange(LOCATION_MZONE)
+  e1:SetCondition(s.condition1)
+  e1:SetTarget(s.target1)
+  e1:SetOperation(s.operation1)
+  e1:SetCountLimit(1,id)
+  c:RegisterEffect(e1)
+	--Recycle
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(10100022,1))
-	e2:SetCategory(CATEGORY_DRAW)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+  e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetTarget(c10100022.target2)
-	e2:SetOperation(c10100022.operation2)
-	e2:SetCountLimit(1,10100022)
+  e2:SetCondition(s.condition2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+	e2:SetCountLimit(1,id+1000000)
 	c:RegisterEffect(e2)
 end
---e1 - Equip
-function c10100022.filter1(c,ec)
-	return c:IsType(TYPE_EQUIP)	and c:IsCode(10100045) and c:CheckEquipTarget(ec)
+function s.filter0(c)
+  return c:IsAttribute(ATTRIBUTE_WIND) and c:IsRace(RACE_WARRIOR)
 end
-function c10100022.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingMatchingCard(c10100022.filter1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e:GetHandler()) end
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_DECK)
+function s.check0(g,lc)
+  return g:IsExists(s.filter0,1,nil)
 end
-function c10100022.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or c:IsFacedown() or not c:IsRelateToEffect(e) then return end
-	Duel.Hint(HINT_SELECTMSG,tp,nil)
-	local g=Duel.SelectMatchingCard(tp,c10100022.filter1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,c)
-	if g:GetCount()>0 and g:GetFirst():CheckEquipTarget(c) then
-		Duel.Equip(tp,g:GetFirst(),c)
-	end
+function s.filter1a(c)
+  return c:IsFaceup() and c:IsType(TYPE_EFFECT)
 end
---e2 - Draw
-function c10100022.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(1)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+function s.filter1b(c,e,tp)
+  return c:IsAttribute(ATTRIBUTE_WIND) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c10100022.operation2(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Draw(p,d,REASON_EFFECT)
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+  return not Duel.IsExistingMatchingCard(s.filter1a,tp,LOCATION_MZONE,0,1,e:GetHandler())
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.filter1b,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+    if g:GetCount()>0 then
+      Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    end
+  end
+end
+function s.filter2(c)
+  return c:IsAttribute(ATTRIBUTE_WIND) and c:IsAbleToHand()
+end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+  return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_GRAVE,0,1,nil) end
+  Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+  local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_GRAVE,0,1,1,nil)
+  if g:GetCount()>0 then
+    Duel.SendtoHand(g,nil,REASON_EFFECT)
+  end
 end
