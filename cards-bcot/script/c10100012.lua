7ec5bc3abd93849c2fbe67ade7f4cec30e763b7e
utso <cccygodev@gmail.com>
2021-10-04 21:53:47 +0200

    Implement Miru

--- cards-bcot/script/c10100012.lua
+++ cards-bcot/script/c10100012.lua
@@ -1,87 +1,32 @@
+if not bcot then
+	dofile "expansions/util-bcot.lua"
+end
 --Great Kanohi Miru
-function c10100012.initial_effect(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_EQUIP)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetTarget(c10100012.target1)
-	e1:SetOperation(c10100012.operation1)
-	c:RegisterEffect(e1)
-	--Equip limit
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c)
+  --Destroy if replaced
+  local e1=bcot.kanohi_selfdestruct(c)
+  c:RegisterEffect(e1)
+	--Negate targeting effects
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_EQUIP_LIMIT)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetValue(c10100012.condition2)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_CHAIN_SOLVING)
+  e2:SetRange(LOCATION_SZONE)
+	e2:SetCondition(s.condition2)
+  e2:SetOperation(s.operation2)
 	c:RegisterEffect(e2)
-	--Immune to monsters
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_EQUIP)
-	e3:SetCode(EFFECT_IMMUNE_EFFECT)
-	e3:SetValue(c10100012.value3)
+	--Search
+  local e3=bcot.kanohi_search(c,10100006)
+  e3:SetDescription(aux.Stringid(id,0))
+  e3:SetCountLimit(1,id)
 	c:RegisterEffect(e3)
-	--Swap
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(10100012,0))
-	e4:SetCategory(CATEGORY_EQUIP)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
-	e4:SetRange(LOCATION_SZONE)
-	e4:SetCode(EVENT_FREE_CHAIN)
-	e4:SetCost(c10100012.cost4)
-	e4:SetTarget(c10100012.target4)
-	e4:SetOperation(c10100012.operation4)
-	e4:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	c:RegisterEffect(e4)
 end
---e1 - Activate
-function c10100012.filter1(c)
-	return c:IsFaceup() and c:IsSetCard(0x155) and not c:GetEquipGroup():IsExists(Card.IsSetCard,1,nil,0x158)
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+	if not (bcot.greatkanohi_con(e) and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET)) then return false end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return g and g:IsContains(e:GetHandler():GetEquipTarget()) and ep~=tp
 end
-function c10100012.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c10100012.filter1(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c10100012.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	Duel.SelectTarget(tp,c10100012.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
-end
-function c10100012.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		Duel.Equip(tp,c,tc)
-	end
-end
---e2 - Equip Limit
-function c10100012.condition2(e,c)
-	return c:IsSetCard(0x155)
-end
---e3 - Immune to monsters
-function c10100012.value3(e,te)
-	return te:IsActiveType(TYPE_MONSTER)
-end
---e4 - Swap
-function c10100012.filter4(c,ec)
-	return c:IsSetCard(0x158) and c:CheckEquipTarget(ec)
-end
-function c10100012.cost4(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckLPCost(tp,800) end
-	Duel.PayLPCost(tp,800)
-end
-function c10100012.target4(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100012.filter4,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,nil,e:GetHandler():GetEquipTarget()) end
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
-end
-function c10100012.operation4(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local ec=c:GetEquipTarget()
-	if Duel.SendtoGrave(c,REASON_EFFECT) then
-		Duel.BreakEffect()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-		local g=Duel.SelectMatchingCard(tp,c10100012.filter4,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,ec)
-		if g:GetCount()>0 then
-			Duel.Equip(tp,g:GetFirst(),ec)
-		end
-	end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(ev)
 end
