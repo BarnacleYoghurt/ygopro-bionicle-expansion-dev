f4363c5bd0e548405e4737dc08a7cdd3a3fbf4c1
utso <cccygodev@gmail.com>
2021-10-06 20:48:43 +0200

    Matau: Implement changed conditions for SS from hand, delayed floating effect

--- cards-bcot/script/c10100022.lua
+++ cards-bcot/script/c10100022.lua
@@ -10,7 +10,6 @@ function s.initial_effect(c)
   e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
   e1:SetType(EFFECT_TYPE_IGNITION)
   e1:SetRange(LOCATION_MZONE)
-  e1:SetCondition(s.condition1)
   e1:SetTarget(s.target1)
   e1:SetOperation(s.operation1)
   e1:SetCountLimit(1,id)
@@ -22,10 +21,13 @@ function s.initial_effect(c)
   e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_GRAVE)
   e2:SetCondition(s.condition2)
-	e2:SetTarget(s.target2)
 	e2:SetOperation(s.operation2)
 	e2:SetCountLimit(1,id+1000000)
 	c:RegisterEffect(e2)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e2)
+	if not AshBlossomTable then AshBlossomTable={} end
+	table.insert(AshBlossomTable,e2)
 end
 function s.filter0(c)
   return c:IsAttribute(ATTRIBUTE_WIND) and c:IsRace(RACE_WARRIOR)
@@ -33,42 +35,60 @@ end
 function s.check0(g,lc)
   return g:IsExists(s.filter0,1,nil)
 end
-function s.filter1a(c)
-  return c:IsFaceup() and c:IsType(TYPE_EFFECT)
-end
-function s.filter1b(c,e,tp)
+function s.filter1(c,e,tp)
   return c:IsAttribute(ATTRIBUTE_WIND) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.condition1(e,tp,eg,ep,ev,re,r,rp)
-  return not Duel.IsExistingMatchingCard(s.filter1a,tp,LOCATION_MZONE,0,1,e:GetHandler())
-end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=Duel.SelectMatchingCard(tp,s.filter1b,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-    if g:GetCount()>0 then
-      Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    local tc=Duel.SelectMatchingCard(tp,s.filter1b,tp,LOCATION_HAND,0,1,1,nil,e,tp):GetFirst()
+    if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+      local e1=Effect.CreateEffect(e:GetHandler())
+      e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+      e1:SetType(EFFECT_TYPE_SINGLE)
+      e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+      e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+      e1:SetValue(0)
+      tc:RegisterEffect(e1)
+      Duel.SpecialSummonComplete()
     end
   end
-end
-function s.filter2(c)
-  return c:IsAttribute(ATTRIBUTE_WIND) and c:IsAbleToHand()
 end
 function s.condition2(e,tp,eg,ep,ev,re,r,rp)
   return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_GRAVE,0,1,nil) end
-  Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
-end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-  local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_GRAVE,0,1,1,nil)
-  if g:GetCount()>0 then
-    Duel.SendtoHand(g,nil,REASON_EFFECT)
+  local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetCountLimit(1)
+	e1:SetLabel(Duel.GetTurnCount())
+	e1:SetCondition(s.condition2_1)
+	e1:SetOperation(s.operation2_1)
+  if Duel.IsTurnPlayer(tp) then
+    e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
+  else
+    e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
   end
+	Duel.RegisterEffect(e1,tp)
+end
+function s.filter2_1(c)
+  return c:IsAttribute(ATTRIBUTE_WIND) and c:IsAbleToHand()
+end
+function s.condition2_1(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsTurnPlayer(tp) and Duel.GetTurnCount()~=e:GetLabel()
+end
+function s.operation2_1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter2_1),tp,LOCATION_GRAVE,0,1,1,nil)
+	if g:GetCount()>0 then
+		if Duel.SendtoHand(g,nil,REASON_EFFECT)>0 and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)<Duel.GetFieldGroupCount(1-tp,LOCATION_MZONE,0) then
+      Duel.BreakEffect()
+      Duel.Draw(tp,1,REASON_EFFECT)
+    end
+	end
 end
