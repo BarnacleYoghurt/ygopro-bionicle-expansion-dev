35db63533e89daebf5bf5dd671ea64a2802c2789
utso <cccygodev@gmail.com>
2021-12-02 23:19:36 +0100

    Implement Toa Mata Combination - Crystal

--- cards-bcot/script/c10100059.lua
+++ cards-bcot/script/c10100059.lua
@@ -1,88 +1,53 @@
+if not bcot then
+	dofile "expansions/util-bcot.lua"
+end
 --Toa Mata Combination - Crystal
-function c10100059.initial_effect(c)
-	aux.AddXyzProcedure(c,c10100059.filter0,6,2)
-	--Negate
+local s,id=GetID()
+function s.initial_effect(c)
+	--Xyz Summon
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0xb02),6,2)
+	c:EnableReviveLimit()
+	--Negate/Banish
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100059,0))
-	e1:SetCategory(CATEGORY_NEGATE)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DISABLE+CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_CHAINING)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(c10100059.condition1)
-	e1:SetCost(c10100059.cost1)
-	e1:SetTarget(c10100059.target1)
-	e1:SetOperation(c10100059.operation1)
+	e1:SetCondition(s.condition1)
+	e1:SetCost(s.cost1)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
+  e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
-	--Kaita Turbo
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(10100059,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e2:SetCondition(c10100059.condition2)
-	e2:SetCost(c10100059.cost2)
-	e2:SetTarget(c10100059.target2)
-	e2:SetOperation(c10100059.operation2)
-	c:RegisterEffect(e2)
+	--Tag out
+  local e2=bcot.toa_mata_combination_tagout(c,ATTRIBUTE_WATER,ATTRIBUTE_EARTH)
+  e2:SetCountLimit(1,id)
+  c:RegisterEffect(e2)
 end
---Summon Conditions
-function c10100059.filter0(c)
-	return c:IsSetCard(0x1155)
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+  return re:IsActiveType(TYPE_SPELL+TYPE_TRAP) and (Duel.IsChainDisablable(ev) or eg:IsExists(Card.IsAbleToRemove,1,nil,tp))
 end
---e1 - Negate
-function c10100059.condition1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsStatus(STATUS_BATTLE_DESTROYED) then return false end
-	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
-	local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-	return tg and tg:IsContains(c) and Duel.IsChainNegatable(ev)
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+  local min=1
+  if not eg:IsExists(Card.IsAbleToRemove,1,nil,tp) then min=2 end
+  local max=2
+  if not Duel.IsChainDisablable(ev) then max=1 end
+	if chk==0 then return max>min and e:GetHandler():CheckRemoveOverlayCard(tp,min,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,min,max,REASON_COST)
+  local ct=Duel.GetOperatedGroup():GetCount()
+	e:SetLabel(ct)
 end
-function c10100059.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
-end
-function c10100059.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-end
-function c10100059.operation1(e,tp,eg,ep,ev,re,r,rp)
-	Duel.NegateActivation(ev)
-end
---e2 - Kaita Turbo
-function c10100059.filter2(c,e,tp)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x1155) and c:GetLevel()==6 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function c10100059.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp and e:GetHandler():GetOverlayCount()>0
-end
-function c10100059.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsReleasable() end
-	Duel.Release(e:GetHandler(),REASON_COST)
-end
-function c10100059.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPECIAL_SUMMON)
-	Duel.SelectTarget(tp,c10100059.filter2,tp,LOCATION_GRAVE,0,1,3,nil,e,tp)
-end
-function c10100059.operation2(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
-	if g then
-		local tc=g:GetFirst()
-		while tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) do	
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_CANNOT_TRIGGER)
-			e1:SetReset(RESET_EVENT+0x1fe0000)
-			local e2=Effect.CreateEffect(e:GetHandler())
-			e2:SetType(EFFECT_TYPE_SINGLE)
-			e2:SetCode(EFFECT_CANNOT_ATTACK)
-			e2:SetReset(RESET_EVENT+0xff0000)
-			tc:RegisterEffect(e1)
-			tc:RegisterEffect(e2)
-			Duel.SpecialSummonComplete()
-			tc=g:GetNext()
-		end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,0)
+	if e:GetLabel()>1 then
+		Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	end
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+  if e:GetLabel()>1 then
+    Duel.NegateEffect(ev)
+  end
+  Duel.Remove(eg,POS_FACEUP,REASON_EFFECT)
 end
