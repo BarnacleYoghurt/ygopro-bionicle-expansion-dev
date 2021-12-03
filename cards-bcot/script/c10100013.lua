c23ec2d919d221f6c4255d71dd64aea44b006682
utso <cccygodev@gmail.com>
2021-12-03 18:01:57 +0100

    Implement Akamai

--- cards-bcot/script/c10100013.lua
+++ cards-bcot/script/c10100013.lua
@@ -1,93 +1,60 @@
 --Toa Kaita Mata Akamai
-function c10100013.initial_effect(c)
-	aux.AddXyzProcedure(c,c10100013.filter0,6,3)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Xyz Summon
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0xb02),6,3)
 	c:EnableReviveLimit()
-	--Equip
+  --No effects in BP
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100013,0))
-	e1:SetCategory(CATEGORY_EQUIP)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(c10100013.condition1)
-	e1:SetTarget(c10100013.target1)
-	e1:SetOperation(c10100013.operation1)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetTargetRange(0,1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.condition1)
+	e1:SetValue(1)
 	c:RegisterEffect(e1)
-	--Destroy face-down
+  --Negate effects while battling
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(10100013,1))
-	e2:SetCategory(CATEGORY_DESTROY)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCountLimit(1)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_DISABLE)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCost(c10100013.cost2)
-	e2:SetTarget(c10100013.target2)
-	e2:SetOperation(c10100013.operation2)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetTarget(s.target2)
 	c:RegisterEffect(e2)
-	--No effect activation
+  --Burn
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_BATTLED)
-	e3:SetOperation(c10100013.operation3)
+	e3:SetCategory(CATEGORY_DAMAGE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EVENT_BATTLE_DESTROYING)
+	e3:SetCondition(aux.bdocon)
+  e3:SetCost(s.cost3)
+	e3:SetTarget(s.target3)
+	e3:SetOperation(s.operation3)
+  e3:SetCountLimit(1)
 	c:RegisterEffect(e3)
 end
---Summon Conditions
-function c10100013.filter0(c)
-	return c:IsSetCard(0x1155)
+function s.condition1(e)
+  return Duel.IsBattlePhase()
 end
---e1 - Equip
-function c10100013.filter1(c,ec)
-	return c:IsType(TYPE_EQUIP)	and c:IsCode(10100048) and c:CheckEquipTarget(ec)
+function s.target2(e,c)
+	return c==e:GetHandler():GetBattleTarget()
 end
-function c10100013.condition1(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetSummonType()==SUMMON_TYPE_XYZ
-end
-function c10100013.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingMatchingCard(c10100013.filter1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e:GetHandler()) end
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_DECK)
-end
-function c10100013.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or c:IsFacedown() or not c:IsRelateToEffect(e) then return end
-	Duel.Hint(HINT_SELECTMSG,tp,nil)
-	local g=Duel.SelectMatchingCard(tp,c10100013.filter1,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,c)
-	if g:GetCount()>0 and g:GetFirst():CheckEquipTarget(c) then
-		Duel.Equip(tp,g:GetFirst(),c)
-	end
-end
---e2 - Destroy face-down
-function c10100013.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cost3(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
-function c10100013.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and c10100013.filter2(chkc,tp) end
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFacedown,tp,0,LOCATION_ONFIELD,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-	Duel.SetChainLimit(aux.FALSE)
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local tc=e:GetHandler():GetBattleTarget()
+	local atk=tc:GetBaseAttack()
+	if atk<0 then atk=0 end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(atk)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,atk)
 end
-function c10100013.operation2(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,Card.IsFacedown,tp,0,LOCATION_ONFIELD,1,1,nil)
-	if g:GetCount()>0 then
-		local tc=g:GetFirst()
-		if Duel.Destroy(tc,REASON_EFFECT) then
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_CANNOT_TRIGGER)
-			e1:SetReset(RESET_EVENT+0x17a0000)
-			tc:RegisterEffect(e1)
-		end
-	end
-end
---e3 - No effect activation
-function c10100013.operation3(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetAttackTarget()
-	if not tc or not tc:IsStatus(STATUS_BATTLE_DESTROYED) then return end
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CANNOT_TRIGGER)
-	e1:SetReset(RESET_EVENT+0x17a0000)
-	tc:RegisterEffect(e1)
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
 end
