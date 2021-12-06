5812d8c8a9aa8d7aa22d8a6557b9e8996084de78
utso <cccygodev@gmail.com>
2021-12-06 18:31:02 +0100

    Toa Mata Magma: Replace S/T search with stat reduction

--- cards-bcot/script/c10100057.lua
+++ cards-bcot/script/c10100057.lua
@@ -10,7 +10,7 @@ function s.initial_effect(c)
 	--Dump'n'search
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCost(s.cost1)
@@ -23,31 +23,38 @@ function s.initial_effect(c)
   e2:SetCountLimit(1,id)
   c:RegisterEffect(e2)
 end
-function s.filter1a(c)
+function s.filter1(c)
 	return c:GetLevel()==1 and c:IsRace(RACE_ROCK) and (c:GetAttack()==0 and c:IsDefenseBelow(0)) and c:IsAbleToGrave()
-end
-function s.filter1b(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP) and (c:IsSetCard(0x1b04) or c:IsSetCard(0xb02)) and c:IsAbleToHand()
 end
 function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,2,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,2,2,REASON_COST)
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1a,tp,LOCATION_DECK,0,1,nil) and Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
-  Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g1=Duel.SelectMatchingCard(tp,s.filter1a,tp,LOCATION_DECK,0,1,1,nil)
-	if g1:GetCount()>0 and Duel.SendtoGrave(g1,REASON_EFFECT)>0 then
-		Duel.BreakEffect()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g2=Duel.SelectMatchingCard(tp,s.filter1b,tp,LOCATION_DECK,0,1,1,nil)
+	local g1=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	if g1:GetCount()>0 and Duel.SendtoGrave(g1,REASON_EFFECT)>0 
+    and Duel.IsExistingMatchingCard(Card.IsFaceup,1-tp,LOCATION_MZONE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		local g2=Duel.SelectMatchingCard(tp,Card.IsFaceup,1-tp,LOCATION_MZONE,0,1,1,nil)
 		if g2:GetCount()>0 then
-			Duel.SendtoHand(g2,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,g2)
+      local tc=g2:GetFirst()
+			local e1=Effect.CreateEffect(e:GetHandler())
+      e1:SetType(EFFECT_TYPE_SINGLE)
+      e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+      e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+      e1:SetValue(tc:GetAttack()/2)
+      tc:RegisterEffect(e1)
+      local e2=Effect.CreateEffect(e:GetHandler())
+      e2:SetType(EFFECT_TYPE_SINGLE)
+      e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
+      e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+      e2:SetValue(tc:GetDefense()/2)
+      tc:RegisterEffect(e2)
 		end
 	end
 end
