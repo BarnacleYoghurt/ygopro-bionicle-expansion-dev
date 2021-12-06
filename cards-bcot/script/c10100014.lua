4a2299409d3b30e5c67853581fefc8113e681c0a
utso <cccygodev@gmail.com>
2021-12-06 18:15:42 +0100

    Wairuha: Split effect into two

--- cards-bcot/script/c10100014.lua
+++ cards-bcot/script/c10100014.lua
@@ -1,13 +1,14 @@
 --Wairuha, Toa Kaita of Wisdom
 local s,id=GetID()
 function s.initial_effect(c)
+	Duel.EnableGlobalFlag(GLOBALFLAG_DETACH_EVENT)
 	--Xyz Summon
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0xb02),6,3)
 	c:EnableReviveLimit()
-	--Wisdom (Negate)
+	--Negate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100014,1))
-	e1:SetCategory(CATEGORY_DISABLE+CATEGORY_REMOVE)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DISABLE)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
   e1:SetCode(EVENT_CHAINING)
@@ -18,22 +19,40 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation1)
 	e1:SetCountLimit(1)
 	c:RegisterEffect(e1)
+  --Wisdom
+  local e2=Effect.CreateEffect(c)
+  e2:SetDescription(aux.Stringid(id,1))
+  e2:SetCategory(CATEGORY_DRAW+CATEGORY_TOHAND+CATEGORY_REMOVE)
+  e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+  e2:SetProperty(EFFECT_FLAG_DELAY)
+  e2:SetCode(EVENT_DETACH_MATERIAL)
+  e2:SetRange(LOCATION_MZONE)
+  e2:SetTarget(s.target2)
+  e2:SetOperation(s.operation2)
+  e2:SetCountLimit(1,id)
+  c:RegisterEffect(e2)
 end
 function s.condition1(e,tp,eg,ep,ev,re,r,rp)
-  return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and ep==1-tp and Duel.IsChainNegatable(ev)
+  return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
 end
 function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetFieldGroupCount(1-tp,LOCATION_HAND,0)>0 and Duel.GetFieldGroupCount(1-tp,LOCATION_DECK,0)>0 end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
-  e:SetLabel(Duel.SelectOption(tp,70,71,72))
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-  if Duel.NegateActivation(ev) and Duel.GetFieldGroupCount(1-tp,LOCATION_HAND,0)>0 and Duel.GetFieldGroupCount(1-tp,LOCATION_DECK,0)>0 then
+  Duel.NegateActivation(ev)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.GetFieldGroupCount(1-tp,LOCATION_HAND,0)>0 and Duel.GetFieldGroupCount(1-tp,LOCATION_DECK,0)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
+  e:SetLabel(Duel.SelectOption(tp,70,71,72))
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+  if Duel.GetFieldGroupCount(1-tp,LOCATION_HAND,0)>0 and Duel.GetFieldGroupCount(1-tp,LOCATION_DECK,0)>0 then
     local t
     if e:GetLabel()==0 then t=TYPE_MONSTER end
     if e:GetLabel()==1 then t=TYPE_SPELL end
@@ -58,3 +77,4 @@ function s.operation1(e,tp,eg,ep,ev,re,r,rp)
     end
   end
 end
+  
