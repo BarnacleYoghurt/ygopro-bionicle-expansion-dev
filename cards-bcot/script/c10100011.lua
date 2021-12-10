0870c534457921de2d56ff35bf4895f4931596ac
utso <cccygodev@gmail.com>
2021-12-10 12:02:44 +0100

    Ákaku: Replace fancy hand sniping with continuous hand reveal and simpler sniping

--- cards-bcot/script/c10100011.lua
+++ cards-bcot/script/c10100011.lua
@@ -8,54 +8,81 @@ function s.initial_effect(c)
   --Destroy if replaced
   local e1=bcot.kanohi_selfdestruct(c)
   c:RegisterEffect(e1)
-	--Reveal drawn cards
+  --Hand Reveal
 	local e2=Effect.CreateEffect(c)
-  e2:SetDescription(aux.Stringid(id,0))
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_DRAW)
-  e2:SetRange(LOCATION_SZONE)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_PUBLIC)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(0,LOCATION_HAND)
   e2:SetCondition(s.condition2)
-	e2:SetOperation(s.operation2)
-  e2:SetCountLimit(1)
 	c:RegisterEffect(e2)
-  --Lock revealed cards
-  local e3a=Effect.CreateEffect(c)
-  e3a:SetType(EFFECT_TYPE_FIELD)
-  e3a:SetCode(EFFECT_CANNOT_TRIGGER)
-  e3a:SetRange(LOCATION_SZONE)
-  e3a:SetTargetRange(0,LOCATION_HAND)
-  e3a:SetTarget(s.target3)
-  e3a:SetValue(1)
-  c:RegisterEffect(e3a)
-  local e3b=e3a:Clone()
-  e3b:SetCode(EFFECT_CANNOT_SSET)
-  c:RegisterEffect(e3b)
-	--Search
-  local e3=bcot.kanohi_search(c,10100005)
-  e3:SetDescription(aux.Stringid(id,1))
-  e3:SetCountLimit(1,id)
+	--Reveal drawn cards
+	local e3=Effect.CreateEffect(c)
+  e3:SetDescription(aux.Stringid(id,0))
+  e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_TO_HAND)
+  e3:SetRange(LOCATION_SZONE)
+  e3:SetCondition(s.condition3)
+  e3:SetTarget(s.target3)
+	e3:SetOperation(s.operation3)
+  e3:SetCountLimit(1)
 	c:RegisterEffect(e3)
+	--Search
+  local e4=bcot.kanohi_search(c,10100005)
+  e4:SetDescription(aux.Stringid(id,1))
+  e4:SetCountLimit(1,id)
+	c:RegisterEffect(e4)
 end
-function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+function s.condition2(e)
   local ec=e:GetHandler():GetEquipTarget()
-	return ec and ec:IsControler(tp) and bcot.greatkanohi_con(e) and ep==1-tp
+	return bcot.greatkanohi_con(e) and ec:IsControler(e:GetOwnerPlayer())
 end
-function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-  local c=e:GetHandler()
-  for tc in aux.Next(eg) do
-    local rct=1
-		if Duel.GetTurnPlayer()~=tp then rct=2
-		elseif Duel.GetCurrentPhase()==PHASE_END then rct=3 end
-      
-    tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,EFFECT_FLAG_CLIENT_HINT,rct,c:GetFieldID(),aux.Stringid(id,2))
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_PUBLIC)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,rct)
-    tc:RegisterEffect(e1)
+function s.filter3(c,tp)
+  return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsControler(1-tp) and c:IsAbleToRemove()
+end
+function s.condition3(e,tp,eg,ep,ev,re,r,rp)
+  return s.condition2(e) and eg:IsExists(Card.IsControler,1,nil,1-tp)
+end
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return eg:IsExists(s.filter3,1,nil,tp) end
+  Duel.SetTargetCard(eg)
+  Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,eg:FilterCount(s.filter3,nil,tp),0,0)
+end
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
+  local rg=eg:Filter(s.filter3,nil,tp):Filter(Card.IsRelateToEffect,nil,e)
+  if rg:GetCount()>0 then
+    Duel.Remove(rg,POS_FACEUP,REASON_EFFECT+REASON_TEMPORARY)
+    local tc=rg:GetFirst()
+    for tc in aux.Next(rg) do
+      tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+    end
+    rg:KeepAlive()
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e1:SetCode(EVENT_PHASE+PHASE_END)
+    e1:SetCountLimit(1)
+    e1:SetLabelObject(rg)
+    e1:SetCondition(s.condition3_1)
+    e1:SetOperation(s.operation3_1)
+    e1:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e1,tp)
   end
 end
-function s.target3(e,c)
-  return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsPublic() and c:GetFlagEffectLabel(id)==e:GetHandler():GetFieldID()
+function s.filter3_1(c)
+  return c:GetFlagEffect(id)~=0
+end
+function s.condition3_1(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject():Filter(s.filter3_1,nil,id)
+	if g:GetCount()==0 then
+		e:Reset()
+		return false
+	else
+		return true
+	end
+end
+function s.operation3_1(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject():Filter(s.filter3_1,nil,id)
+	Duel.SendtoHand(g,1-tp,REASON_EFFECT)
 end
