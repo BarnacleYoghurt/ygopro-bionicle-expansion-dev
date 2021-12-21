9208159e31cadc19ad72011de6bff62bd561cb0e
utso <cccygodev@gmail.com>
2021-12-21 16:03:11 +0100

    Implement Diminished Matoran Nui

--- cards-bcot/script/c10100047.lua
+++ cards-bcot/script/c10100047.lua
@@ -1,5 +1,6 @@
 --Dimnished Matoran Nui
-function c10100047.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Cannot SS by other ways
 	local e0=Effect.CreateEffect(c)
@@ -8,81 +9,84 @@ function c10100047.initial_effect(c)
 	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e0:SetValue(aux.FALSE)
 	c:RegisterEffect(e0)
+  --To hand/grave
+  local e1=Effect.CreateEffect(c)
+  e1:SetDescription(aux.Stringid(id,0))
+  e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND+CATEGORY_TOGRAVE)
+  e1:SetType(EFFECT_TYPE_IGNITION)
+  e1:SetRange(LOCATION_HAND)
+  e1:SetCost(s.cost1)
+  e1:SetTarget(s.target1)
+  e1:SetOperation(s.operation1)
+  e1:SetCountLimit(1,id)
+  c:RegisterEffect(e1)
 	--Special Summon
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c10100047.condition1)
-	e1:SetValue(1)
-	c:RegisterEffect(e1)
-	--Destroy
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(10100047,0))
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetCost(c10100047.cost2)
-	e2:SetTarget(c10100047.target2)
-	e2:SetOperation(c10100047.operation2)
+  e2:SetDescription(aux.Stringid(id,1))
+  e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+  e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCondition(s.condition2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+  e2:SetCountLimit(1,id+1000000)
 	c:RegisterEffect(e2)
-	--When leave field
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(10100047,1))
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetCode(EVENT_LEAVE_FIELD)
-	e3:SetTarget(c10100047.target3)
-	e3:SetOperation(c10100047.operation3)
-	c:RegisterEffect(e3)
 end
---e1 - Special Summon
-function c10100047.filter1(c)
-	return c:IsSetCard(0x157) and c:GetLevel()==2
+function s.filter1(c)
+  return c:IsLevel(2) and c:IsSetCard(0xb01) and c:IsAbleToHand()
 end
-function c10100047.condition1(e,c)
-	if c==nil then return true end
-	local g=Duel.GetMatchingGroup(c10100047.filter1,c:GetControler(),LOCATION_GRAVE,0,nil)
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.GetMatchingGroup(s.filter1,tp,LOCATION_DECK,0,nil):GetClassCount(Card.GetAttribute)>=3 end
+  Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_DECK)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+  local g=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_DECK,0,nil)
+	if g:GetClassCount(Card.GetAttribute)>=3 then
+		local cg=Group.CreateGroup()
+		for i=1,3 do
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local sg=g:Select(tp,1,1,nil)
+			g:Remove(Card.IsAttribute,nil,sg:GetFirst():GetAttribute())
+			cg:Merge(sg)
+		end
+		Duel.ConfirmCards(1-tp,cg)
+		Duel.ShuffleDeck(tp)
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_ATOHAND)
+		local tg=cg:Select(1-tp,1,1,nil)
+		local tc=tg:GetFirst()
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		cg:RemoveCard(tc)
+		Duel.SendtoGrave(cg,REASON_EFFECT)
+	end
+end
+function s.filter2(c)
+	return c:IsSetCard(0xb01) and c:IsLevel(2)
+end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_GRAVE,0,nil)
 	return g:GetClassCount(Card.GetCode)>=6
 end
---e2 - Destroy
-function c10100047.filter2(c)
-	return c:GetLevel()==2
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+  if chk==0 then 
+      return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,true)
+        and Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) 
+  end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+  local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
+  Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c10100047.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroup(tp,c10100047.filter2,1,nil) and e:GetHandler():IsReleasable() end
-	local sg=Duel.SelectReleaseGroup(tp,c10100047.filter2,1,1,nil)
-	sg:AddCard(e:GetHandler())
-	Duel.Release(sg,REASON_COST)
-end
-function c10100047.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then
-		if chkc then return chkc:IsOnField() and chkc:IsDestructable() end
-		return Duel.IsExistingTarget(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,2,e:GetHandler())
-	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,2,2,e:GetHandler())
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
-end
-function c10100047.operation2(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
-	Duel.Destroy(sg,REASON_EFFECT)
-end
---e3 - When leave field
-function c10100047.filter3(c,e,tp)
-	return c:IsSetCard(0x157) and c:GetLevel()==2 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function c10100047.target3(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>2 
-		and Duel.IsExistingMatchingCard(c10100047.filter2,tp,LOCATION_GRAVE,0,3,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,3,tp,LOCATION_GRAVE)
-end
-function c10100047.operation3(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c10100047.filter3,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	if g:GetCount()>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-	end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+  local c=e:GetHandler()
+  if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,true,true,POS_FACEUP)>0 then
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+      Duel.Destroy(tc,REASON_EFFECT)
+    end
+  end
 end
