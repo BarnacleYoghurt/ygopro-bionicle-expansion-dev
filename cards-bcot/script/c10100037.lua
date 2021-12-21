c9a2a0bc3bfe0986162db0dc08874b60c0fc403b
utso <cccygodev@gmail.com>
2021-12-21 18:51:01 +0100

    Implement Takua

--- cards-bcot/script/c10100037.lua
+++ cards-bcot/script/c10100037.lua
@@ -1,79 +1,85 @@
 --Matoran Chronicler Takua
-function c10100037.initial_effect(c)
-	--Search
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100037,0))
-	e1:SetCategory(CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e1:SetTarget(c10100037.target1)
-	e1:SetOperation(c10100037.operation1)
-	c:RegisterEffect(e1)
-	--Look & Special Summon
+local s,id=GetID()
+function s.initial_effect(c)
+	--To deck
+	local e1a=Effect.CreateEffect(c)
+	e1a:SetDescription(aux.Stringid(id,0))
+	e1a:SetCategory(CATEGORY_TODECK)
+	e1a:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1a:SetCode(EVENT_SUMMON_SUCCESS)
+	e1a:SetProperty(EFFECT_FLAG_DELAY)
+	e1a:SetTarget(s.target1)
+	e1a:SetOperation(s.operation1)
+	c:RegisterEffect(e1a)
+  local e1b=e1a:Clone()
+  e1b:SetCode(EVENT_SPSUMMON_SUCCESS)
+  c:RegisterEffect(e1b)
+	--Excavate
 	local e2=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(10100037,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCost(c10100037.cost2)
-	e2:SetTarget(c10100037.target2)
-	e2:SetOperation(c10100037.operation2)
-	e2:SetCountLimit(1)
+	e2:SetCost(s.cost2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+	e2:SetCountLimit(1,id)
 	c:RegisterEffect(e2)
-	--Battle immune
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetTargetRange(LOCATION_MZONE,0)
-	e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x1157))
-	e3:SetValue(1)
-	c:RegisterEffect(e3)
 end
---e1 - Search
-function c10100037.filter1(c)
-	return c:IsSetCard(0x1157) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+function s.filter1(c)
+	return c:IsSetCard(0x1b01) and c:IsType(TYPE_MONSTER)
 end
-function c10100037.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100037.filter1,tp,LOCATION_DECK,0,1,nil) end
---	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+  Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,0,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
 end
-function c10100037.operation1(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(10100037,2))
-	local g=Duel.SelectMatchingCard(tp,c10100037.filter1,tp,LOCATION_DECK,0,1,1,nil)
-	local tc=g:GetFirst()
-	if tc then
-		Duel.ShuffleDeck(tp)
-		Duel.MoveSequence(tc,0)
-		Duel.ConfirmDecktop(tp,1)
-	end
-end
---e2 - Special Summon
-function c10100037.filter2a(c)
-	return c:IsSetCard(0x1157) and c:IsType(TYPE_MONSTER) and c:IsDiscardable()
-end
-function c10100037.filter2b(c,e,tp)
-	return c:IsSetCard(0x1157) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function c10100037.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100037.filter2a,tp,LOCATION_HAND,0,1,nil) end
-	Duel.DiscardHand(tp,c10100037.filter2a,1,1,REASON_COST+REASON_DISCARD)
-end
-function c10100037.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c10100037.filter2b,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPSUMMON,g,1,0,0)
-end
-function c10100037.operation2(e,tp,eg,ep,ev,re,r,rp,c)
-	Duel.ConfirmDecktop(tp,3)
-	local g=Duel.GetDecktopGroup(tp,3)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<g:FilterCount(c10100037.filter2b,nil,e,tp) then
- 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		g=g:FilterSelect(tp,c10100037.filter2b,1,Duel.GetLocationCount(tp,LOCATION_MZONE),nil,e,tp)
-	else
-		g=g:Filter(c10100037.filter2b,nil,e,tp)
-	end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter1),tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil)
 	if g:GetCount()>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    local tc=g:GetFirst()
+    if tc:IsLocation(LOCATION_DECK) then
+      Duel.ShuffleDeck(tp)
+      Duel.MoveToDeckTop(tc)
+    else
+      Duel.HintSelection(g,true)
+      Duel.SendtoDeck(tc,nil,SEQ_DECKTOP,REASON_EFFECT)
+    end
+    if not tc:IsLocation(LOCATION_EXTRA) then
+      Duel.ConfirmDecktop(tp,1)
+    end
+    
+    if tc:IsLocation(LOCATION_DECK) then
+      local e1=Effect.CreateEffect(e:GetHandler())
+      e1:SetType(EFFECT_TYPE_SINGLE)
+      e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+      e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+      e1:SetRange(LOCATION_MZONE)
+      e1:SetValue(tc:GetOriginalAttribute())
+      e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+      e:GetHandler():RegisterEffect(e1)
+    end
+  end
+end
+function s.filter2(c,e,tp)
+	return c:IsSetCard(0x1b01) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=6 end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp,c)
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)==0 then return end
+	Duel.ConfirmDecktop(tp,6)
+	local g=Duel.GetDecktopGroup(tp,6):Filter(s.filter2,nil,e,tp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 	end
+	Duel.ShuffleDeck(tp)
 end
