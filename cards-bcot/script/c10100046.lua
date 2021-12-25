353f258f08621c858a5b9b034eca82e8d5a66140
utso <cccygodev@gmail.com>
2021-12-25 13:18:09 +0100

    Diminished Matoran Kaita: Change to on-summon effect and detach on resolution (cf. Time Thief Xyzs)

--- cards-bcot/script/c10100046.lua
+++ cards-bcot/script/c10100046.lua
@@ -14,10 +14,12 @@ function s.initial_effect(c)
 	--Detach
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-  e2:SetCode(EVENT_FREE_CHAIN)
+  e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_DRAW+CATEGORY_SPECIAL_SUMMON+CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+  e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+  e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCost(aux.dxmcostgen(1,1,s.costproc2))
+	e2:SetCondition(s.condition2)
 	e2:SetTarget(s.target2)
 	e2:SetOperation(s.operation2)
 	e2:SetCountLimit(1)
@@ -32,39 +34,47 @@ end
 function s.filter2LIGHTDARK(c,e,tp)
 	return c:GetLevel()==2 and c:IsSetCard(0xb01) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.costproc2(e,og)
-	e:SetLabel(og:GetFirst():GetOriginalAttribute())
-end
-function s.target2WIND(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then return Duel.IsExistingMatchingCard(s.filter2WIND,tp,LOCATION_DECK,0,1,nil) end
-  Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-end
-function s.target2EARTH(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(1)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
-end
-function s.target2LIGHTDARK(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter2LIGHTDARK,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
-  Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+  return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
   if chk==0 then
-    return (e:GetHandler():GetOverlayGroup():IsExists(aux.NOT(Card.IsAttribute),1,nil,ATTRIBUTE_WIND) or s.target2WIND(e,tp,eg,ep,ev,re,r,rp,0))
-      and (e:GetHandler():GetOverlayGroup():IsExists(aux.NOT(Card.IsAttribute),1,nil,ATTRIBUTE_EARTH) or s.target2EARTH(e,tp,eg,ep,ev,re,r,rp,0))
-      and (e:GetHandler():GetOverlayGroup():IsExists(aux.NOT(Card.IsAttribute),1,nil,ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) or s.target2LIGHTDARK(e,tp,eg,ep,ev,re,r,rp,0))
+    local c=e:GetHandler()
+    local g=c:GetOverlayGroup()
+    local att=ATTRIBUTE_WATER | ATTRIBUTE_FIRE
+    if Duel.IsExistingMatchingCard(s.filter2WIND,tp,LOCATION_DECK,0,1,nil) then att=att | ATTRIBUTE_WIND end
+    if Duel.IsPlayerCanDraw(tp,1) then att=att | ATTRIBUTE_EARTH end
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter2LIGHTDARK,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) then 
+      att=att | ATTRIBUTE_LIGHT | ATTRIBUTE_DARK 
+    end
+    return att>0 and g:IsExists(Card.IsAttribute,1,nil,att) 
   end
-	local att=e:GetLabel()
-  if att==ATTRIBUTE_WIND then s.target2WIND(e,tp,eg,ep,ev,re,r,rp,1) end
-  if att==ATTRIBUTE_EARTH then s.target2EARTH(e,tp,eg,ep,ev,re,r,rp,1) end
-  if att==ATTRIBUTE_LIGHT or att==ATTRIBUTE_DARK then s.target2LIGHTDARK(e,tp,eg,ep,ev,re,r,rp,1) end
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-  local c=e:GetHandler()
-	local att=e:GetLabel()
-  if att==ATTRIBUTE_WIND then
+  --Detach up to 3 materials with different attributes
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local g=c:GetOverlayGroup()
+	local avAtt=0
+  if c:IsFaceup() then avAtt=avAtt | ATTRIBUTE_WATER | ATTRIBUTE_FIRE end
+  if Duel.IsExistingMatchingCard(s.filter2WIND,tp,LOCATION_DECK,0,1,nil) then avAtt=avAtt | ATTRIBUTE_WIND end
+  if Duel.IsPlayerCanDraw(tp,1) then avAtt=avAtt | ATTRIBUTE_EARTH end
+  if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter2LIGHTDARK,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) then 
+    avAtt=avAtt | ATTRIBUTE_LIGHT | ATTRIBUTE_DARK 
+  end
+	if avAtt==0 then return end
+	local sg=aux.SelectUnselectGroup(g:Filter(Card.IsAttribute,nil,avAtt),e,tp,1,3,
+    function(sg,e,tp,mg) return sg:GetClassCount(Card.GetAttribute)==sg:GetCount() end,1,tp,HINTMSG_REMOVEXYZ)
+	local att=0
+	for tc in aux.Next(sg) do
+		att=att | tc:GetAttribute()
+	end
+	Duel.SendtoGrave(sg,REASON_EFFECT)
+	Duel.RaiseSingleEvent(c,EVENT_DETACH_MATERIAL,e,0,0,0,0)
+	Duel.BreakEffect()
+  
+  --Apply the appropriate effect(s)
+	if att & ATTRIBUTE_WIND ~=0 then
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
     local g=Duel.SelectMatchingCard(tp,s.filter2WIND,tp,LOCATION_DECK,0,1,1,nil)
 		if g:GetCount()>0 then
@@ -72,35 +82,27 @@ function s.operation2(e,tp,eg,ep,ev,re,r,rp)
       Duel.ConfirmCards(1-tp,g)
     end
 	end
-  if att==ATTRIBUTE_WATER then
+  if att & ATTRIBUTE_WATER ~=0 then
 		local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetDescription(3001)
+    e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-    e1:SetTargetRange(LOCATION_MZONE,0)
-    e1:SetTarget(function (e,c) return c:IsFaceup() and c:IsSetCard(0xb01) end)
     e1:SetValue(1)
-    e1:SetReset(RESET_PHASE+PHASE_END)
-    Duel.RegisterEffect(e1,tp)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+    c:RegisterEffect(e1)
 	end
-	if att==ATTRIBUTE_FIRE then
-		if c:IsRelateToEffect(e) and c:IsFaceup() then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(600)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			c:RegisterEffect(e1)
-		end
+	if att & ATTRIBUTE_FIRE ~= 0 then
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_UPDATE_ATTACK)
+    e2:SetValue(1200)
+    e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+    c:RegisterEffect(e2)
 	end
-	if att==ATTRIBUTE_EARTH then
-		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-		if Duel.Draw(p,d,REASON_EFFECT)>0 then
-      Duel.ShuffleHand(tp)
-      Duel.BreakEffect()
-      Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT+REASON_DISCARD)
-    end
+	if att & ATTRIBUTE_EARTH ~= 0 then
+		Duel.Draw(tp,1,REASON_EFFECT)
 	end
-	if att==ATTRIBUTE_LIGHT or att==ATTRIBUTE_DARK then
+	if att & (ATTRIBUTE_LIGHT | ATTRIBUTE_DARK) ~= 0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
     local g=Duel.SelectMatchingCard(tp,s.filter2LIGHTDARK,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
     if g:GetCount()>0 then
