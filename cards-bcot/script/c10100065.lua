7655e3a4f5a7e50ba1aa0a801d0fde55c53077c6
utso <cccygodev@gmail.com>
2022-02-07 19:43:04 +0100

    Implement Jaa

---
+++ cards-bcot/script/c10100065.lua
@@ -0,0 +1,95 @@
+--Matoran Scribe Jaa
+local s,id=GetID()
+function s.initial_effect(c)
+  --Special Summon self
+  local e1=Effect.CreateEffect(c)
+  e1:SetDescription(aux.Stringid(id,0))
+  e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e1:SetType(EFFECT_TYPE_IGNITION)
+  e1:SetRange(LOCATION_HAND)
+  e1:SetCost(s.cost1)
+  e1:SetTarget(s.target1)
+  e1:SetOperation(s.operation1)
+  e1:SetCountLimit(1,id)
+  c:RegisterEffect(e1)
+  --Special Summon another
+  local e2=Effect.CreateEffect(c)
+  e2:SetDescription(aux.Stringid(id,1))
+  e2:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
+  e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+  e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+  e2:SetCode(EVENT_TO_GRAVE)
+  e2:SetTarget(s.target2)
+  e2:SetOperation(s.operation2)
+  e2:SetCountLimit(1,id+1000000)
+  c:RegisterEffect(e2)
+end
+function s.filter1(c)
+  return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToGraveAsCost()
+end
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil) end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+  local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil)
+  Duel.SendtoGrave(g,REASON_COST)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+  local c=e:GetHandler()
+  if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,tp,false,false) end
+  Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+  local c=e:GetHandler()
+  if c:IsRelateToEffect(e) then
+    Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+  end
+end
+function s.filter2a(c)
+  return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToDeck()
+end
+function s.filter2b(c,e,tp)
+  return c:IsAttribute(ATTRIBUTE_WATER) and c:IsSetCard(0xb01) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,tp,false,false)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingTarget(s.filter2a,tp,LOCATION_GRAVE,0,1,nil) end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+  local g=Duel.SelectTarget(tp,s.filter2a,tp,LOCATION_GRAVE,0,1,1,nil)
+  Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+  Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+  local tc=Duel.GetFirstTarget()
+  if tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,SEQ_DECKTOP,REASON_EFFECT)>0 then
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter2b,tp,LOCATION_GRAVE,0,1,nil,e,tp) then
+      if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.filter2b,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+        if Duel.SpecialSummonStep(g:GetFirst(),0,tp,tp,false,false,POS_FACEUP) then
+          local e1=Effect.CreateEffect(e:GetHandler())
+          e1:SetType(EFFECT_TYPE_SINGLE)
+          e1:SetCode(EFFECT_DISABLE)
+          e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+          g:GetFirst():RegisterEffect(e1)
+          local e2=Effect.CreateEffect(e:GetHandler())
+          e2:SetType(EFFECT_TYPE_SINGLE)
+          e2:SetCode(EFFECT_DISABLE_EFFECT)
+          e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+          g:GetFirst():RegisterEffect(e2)
+          local e3=Effect.CreateEffect(e:GetHandler())
+          e3:SetType(EFFECT_TYPE_FIELD)
+          e3:SetRange(LOCATION_MZONE)
+          e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+          e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
+          e3:SetTargetRange(1,0)
+          e3:SetCondition(s.condition2_3)
+          e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+          g:GetFirst():RegisterEffect(e3)
+        end
+        Duel.SpecialSummonComplete()
+      end
+    end
+  end
+end
+function s.condition2_3(e)
+  return e:GetHandler():IsControler(e:GetOwnerPlayer())
+end
