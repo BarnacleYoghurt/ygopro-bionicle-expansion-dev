cfcdaabf1366dec45ed8189750ad927fe5b88555
utso <cccygodev@gmail.com>
2022-02-07 20:02:09 +0100

    Implement Makani

---
+++ cards-bcot/script/c10100066.lua
@@ -0,0 +1,62 @@
+--Matoran Musician Makani
+local s,id=GetID()
+function s.initial_effect(c)
+  --Special Summon
+  local e1=Effect.CreateEffect(c)
+  e1:SetDescription(aux.Stringid(id,0))
+  e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+  e1:SetProperty(EFFECT_FLAG_DELAY)
+  e1:SetCode(EVENT_SUMMON_SUCCESS)
+  e1:SetTarget(s.target1)
+  e1:SetOperation(s.operation1)
+  e1:SetCountLimit(1,id)
+  c:RegisterEffect(e1)
+  local e1b=e1:Clone()
+  e1b:SetCode(EVENT_SPSUMMON_SUCCESS)
+  c:RegisterEffect(e1b)
+  --To hand
+  local e2=Effect.CreateEffect(c)
+  e2:SetDescription(aux.Stringid(id,1))
+  e2:SetCategory(CATEGORY_TOHAND)
+  e2:SetType(EFFECT_TYPE_IGNITION)
+  e2:SetRange(LOCATION_GRAVE)
+  e2:SetCondition(aux.exccon)
+  e2:SetCost(s.cost2)
+  e2:SetTarget(s.target2)
+  e2:SetOperation(s.operation2)
+  e2:SetCountLimit(1,id+1000000)
+  c:RegisterEffect(e2)
+end
+function s.filter1(c,e,tp)
+  return c:IsLevelBelow(4) and c:IsSetCard(0xb01) and c:IsCanBeSpecialSummoned(e,tp,tp,false,false)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_HAND,0,1,nil,e,tp) end
+  Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+  if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+  local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+  Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+end
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+  local c=e:GetHandler()
+  if chk==0 then return c:IsAbleToRemoveAsCost() end
+  Duel.Remove(c,POS_FACEUP,REASON_COST)
+end
+function s.filter2(c)
+  return c:IsAttribute(ATTRIBUTE_WIND) and c:IsRace(RACE_WARRIOR) and not c:IsCode(id) and c:IsAbleToHand()
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingTarget(s.filter2,tp,LOCATION_GRAVE,0,1,nil) end
+  local g=Duel.SelectTarget(tp,s.filter2,tp,LOCATION_GRAVE,0,1,1,nil)
+  Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+  local tc=Duel.GetFirstTarget()
+  if tc:IsRelateToEffect(e) then
+    Duel.SendtoHand(tc,nil,REASON_EFFECT)
+  end
+end
