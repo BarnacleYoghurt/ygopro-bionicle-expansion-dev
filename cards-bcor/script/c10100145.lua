505892ae9967ab95218b911771796b62fc84eb47
utso <cccygodev@gmail.com>
2022-02-11 11:41:23 +0100

    Jala: Buff target is now Level 4 or lower FIRE Warriors, extra NS can also be used on a FIRE Toa

--- cards-bcor/script/c10100145.lua
+++ cards-bcor/script/c10100145.lua
@@ -8,7 +8,7 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(LOCATION_MZONE,0)
-	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xb01))
+	e1:SetTarget(s.target1)
 	e1:SetCondition(s.condition1)
 	e1:SetValue(s.value1)
 	c:RegisterEffect(e1)	
@@ -18,15 +18,21 @@ function s.initial_effect(c)
 	e2:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
 	e2:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xb01))
+	e2:SetTarget(s.target2)
 	c:RegisterEffect(e2)
 end
 function s.filter1(c)
 	return c:IsFaceup() and c:IsSetCard(0xb01)
+end
+function s.target1(e,c)
+  return c:IsLevelBelow(4) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_WARRIOR)
 end
 function s.value1(e,c)
 	return Duel.GetMatchingGroupCount(s.filter1,e:GetHandlerPlayer(),LOCATION_MZONE,0,nil)*400
 end
 function s.condition1(e)
 	return Duel.IsExistingMatchingCard(Card.IsFaceup,e:GetHandlerPlayer(),0,LOCATION_MZONE,1,nil)
+end
+function s.target2(e,c)
+  return c:IsSetCard(0xb01) or (c:IsAttribute(ATTRIBUTE_FIRE) and c:IsSetCard(0xb02))
 end
