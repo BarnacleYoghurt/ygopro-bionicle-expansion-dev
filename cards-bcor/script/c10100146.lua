7af600153af83fefb13ad9d145782eccc9b31000
utso <cccygodev@gmail.com>
2022-02-17 20:14:55 +0100

    Hahli: Instead of special summoning from hand for a search (repeatably), special summon from GY and burn as a quickie for easy chain links

--- cards-bcor/script/c10100146.lua
+++ cards-bcor/script/c10100146.lua
@@ -1,64 +1,91 @@
 --Matoran Assistant Hahli
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon
+  --Protect
 	local e1=Effect.CreateEffect(c)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(s.condition1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(s.target1)
+	e1:SetValue(1)
 	c:RegisterEffect(e1)
-	--Protect
+	--Burn
 	local e2=Effect.CreateEffect(c)
-	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e2:SetType(EFFECT_TYPE_FIELD)
+  e2:SetDescription(aux.Stringid(id,0))
+  e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(s.target2)
-	e2:SetValue(1)
+	e2:SetCondition(s.condition2)
+  e2:SetTarget(s.target2)
+  e2:SetOperation(s.operation2)
+  e2:SetCountLimit(1,id)
 	c:RegisterEffect(e2)
-	--Search
+	--Special Summon
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_MZONE)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+  e3:SetProperty(EFFECT_FLAG_DELAY)
+  e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetRange(LOCATION_GRAVE)
 	e3:SetCondition(s.condition3)
 	e3:SetTarget(s.target3)
 	e3:SetOperation(s.operation3)
-	e3:SetCountLimit(1)
+	e3:SetCountLimit(1,id+1000000)
 	c:RegisterEffect(e3)
 end
-function s.filter1(c)
-	return c:IsFaceup() and c:IsSetCard(0xb01) and c:GetCode()~=id
-end
-function s.condition1(e,c)
-	if c==nil then return true end
-	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter1,c:GetControler(),LOCATION_MZONE,0,1,nil)
-end
-function s.target2(e,c)
+function s.target1(e,c)
 	return c~=e:GetHandler() and c:IsSetCard(0xb01)
 end
-function s.filter3a(c)
-	return c:IsSetCard(0xb01) and c:IsFaceup()
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+  return Duel.IsMainPhase()
 end
-function s.filter3b(c)
-	return c:IsSetCard(0xb01) and not c:IsCode(id) and c:IsAbleToHand()
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return true end
+  Duel.SetTargetPlayer(1-tp)
+  Duel.SetTargetParam(400)
+  Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,400)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+  local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+  Duel.Damage(p,d,REASON_EFFECT)
+end
+function s.filter3(c,tp)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsSetCard(0xb01) and not c:IsCode(id) and c:IsControler(tp)
 end
 function s.condition3(e,tp,eg,ep,ev,re,r,rp)		
-	return Duel.IsExistingMatchingCard(s.filter3a,tp,LOCATION_MZONE,0,1,e:GetHandler())
+	return eg:IsExists(s.filter3,1,nil,tp)
 end
 function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter3b,tp,LOCATION_DECK,0,1,nil) end
-	local g=Duel.GetMatchingGroup(s.filter3b,tp,LOCATION_DECK,0,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,tp,LOCATION_DECK)
+  local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function s.operation3(e,tp,eg,ep,ev,re,r,rp)		
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter3b,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+    local fid=c:GetFieldID()
+		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,fid)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e1:SetCode(EVENT_PHASE+PHASE_END)
+    e1:SetCondition(s.condition3_1)
+    e1:SetOperation(s.operation3_1)
+    e1:SetLabel(fid)
+    e1:SetLabelObject(c)
+    e1:SetCountLimit(1)
+    e1:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e1,tp)
 	end
+end
+function s.condition3_1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	return tc:GetFlagEffectLabel(id)==e:GetLabel()
+end
+function s.operation3_1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
 end
