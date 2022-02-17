8b6befc7654e74654ebc8d046cf2fbe5366530cc
utso <cccygodev@gmail.com>
2022-02-17 15:55:11 +0100

    Takua: Only stacks on NS now, excavates without cost but for lower max payoff

--- cards-bcot/script/c10100037.lua
+++ cards-bcot/script/c10100037.lua
@@ -2,25 +2,21 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--To deck
-	local e1a=Effect.CreateEffect(c)
-	e1a:SetDescription(aux.Stringid(id,0))
-	e1a:SetCategory(CATEGORY_TODECK)
-	e1a:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1a:SetCode(EVENT_SUMMON_SUCCESS)
-	e1a:SetProperty(EFFECT_FLAG_DELAY)
-	e1a:SetTarget(s.target1)
-	e1a:SetOperation(s.operation1)
-	c:RegisterEffect(e1a)
-  local e1b=e1a:Clone()
-  e1b:SetCode(EVENT_SPSUMMON_SUCCESS)
-  c:RegisterEffect(e1b)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
+	c:RegisterEffect(e1)
 	--Excavate
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCost(s.cost2)
 	e2:SetTarget(s.target2)
 	e2:SetOperation(s.operation2)
 	e2:SetCountLimit(1,id)
@@ -61,38 +57,28 @@ function s.operation1(e,tp,eg,ep,ev,re,r,rp)
     end
   end
 end
-function s.filter2a(c,e,tp)
-	return c:IsSetCard(0x1b01) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.filter2b(c)
-  return c:IsCode(10100038) and c:IsAbleToHand()
-end
-function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
-	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+function s.filter2(c,e,tp)
+	return (c:IsSetCard(0x1b01) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)) 
+    or (c:IsCode(10100038) and c:IsAbleToHand())
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=6 end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp,c)
 	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)==0 then return end
 	Duel.ConfirmDecktop(tp,6)
   local g=Duel.GetDecktopGroup(tp,6)
-	local g1=g:Filter(s.filter2a,nil,e,tp)
+	local fg=g:Filter(s.filter2,nil,e,tp)
   --Special Summon 1 excavated "C.C. Matoran" monster
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and g1:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=g1:Select(tp,1,1,nil)
-		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	if fg:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
+		local sg=fg:Select(tp,1,1,nil)
+    if sg:GetFirst():IsCode(10100038) then --Add "The Chronicler's Company"
+      Duel.SendtoHand(sg,nil,REASON_EFFECT)
+      Duel.ConfirmCards(1-tp,sg)
+    else --Special Summon a "C.C. Matoran" monster
+      Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+    end
 	end
-  --Add 1 excavated "The Chronicler's Company" to hand
-  local g2=g:Filter(s.filter2b,nil)
-  if g2:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local ag=g2:Select(tp,1,1,nil)
-    Duel.SendtoHand(ag,nil,REASON_EFFECT)
-    Duel.ConfirmCards(1-tp,ag)
-  end
 	Duel.ShuffleDeck(tp)
 end
