90faf43eba37fd09c47950457b663c288d89aa06
utso <cccygodev@gmail.com>
2022-02-27 00:11:15 +0100

    Kotu: Bounce effect now triggers when sent from hand or field to GY (also, add chkc handling)

--- cards-bbts/script/c10100246.lua
+++ cards-bbts/script/c10100246.lua
@@ -17,7 +17,8 @@ function s.initial_effect(c)
   e2:SetDescription(aux.Stringid(id,0))
   e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
   e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-  e2:SetCode(EVENT_SUMMON_SUCCESS)
+  e2:SetCode(EVENT_TO_GRAVE)
+  e2:SetCondition(s.condition2)
   e2:SetTarget(s.target2)
   e2:SetOperation(s.operation2)
   e2:SetCountLimit(1,id+1000000)
@@ -43,7 +44,11 @@ end
 function s.filter2(c)
   return Duel.IsPlayerCanDraw(c:GetOwner(),1) and c:IsAbleToHand()
 end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+  return e:GetHandler():IsPreviousLocation(LOCATION_HAND+LOCATION_ONFIELD)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter2(chkc) end
   if chk==0 then
     return Duel.IsExistingTarget(s.filter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
   end
