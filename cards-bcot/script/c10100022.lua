dbc39fdc1527c275d4e5c54124b30d6d708ad052
utso <cccygodev@gmail.com>
2022-02-27 00:05:32 +0100

    Matau: Monster is recycled from GY without targeting, bonus draw is now bonus stat reduction

--- cards-bcot/script/c10100022.lua
+++ cards-bcot/script/c10100022.lua
@@ -21,14 +21,11 @@ function s.initial_effect(c)
   e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_GRAVE)
   e2:SetCondition(s.condition2)
-  e2:SetTarget(s.target2)
 	e2:SetOperation(s.operation2)
 	e2:SetCountLimit(1,id+1000000)
 	c:RegisterEffect(e2)
-	--if not GhostBelleTable then GhostBelleTable={} end
-	--table.insert(GhostBelleTable,e2)
-	if not AshBlossomTable then AshBlossomTable={} end
-	table.insert(AshBlossomTable,e2)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e2)
 end
 function s.filter0(c)
   return c:IsAttribute(ATTRIBUTE_WIND) and c:IsRace(RACE_WARRIOR)
@@ -65,24 +62,13 @@ end
 function s.condition2(e,tp,eg,ep,ev,re,r,rp)
   return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-  local c=e:GetHandler()
-  if chkc then return chkc~=c and chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and aux.NecroValleyFilter(s.filter2)(chkc) end 
-  if chk==0 then return Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.filter2),tp,LOCATION_GRAVE,0,1,c) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-  local g=Duel.SelectTarget(tp,s.filter2,tp,LOCATION_GRAVE,0,1,1,c)
-  Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-  local tc=Duel.GetFirstTarget()
-  if tc:IsRelateToEffect(e) then
     local e1=Effect.CreateEffect(e:GetHandler())
     e1:SetDescription(aux.Stringid(id,2))
     e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
     e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
     e1:SetCountLimit(1)
     e1:SetLabel(Duel.GetTurnCount())
-    e1:SetLabelObject(tc)
     e1:SetCondition(s.condition2_1)
     e1:SetOperation(s.operation2_1)
     if Duel.IsTurnPlayer(tp) then
@@ -91,19 +77,30 @@ function s.operation2(e,tp,eg,ep,ev,re,r,rp)
       e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
     end
     Duel.RegisterEffect(e1,tp)
-    tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
-    e:GetHandler():SetCardTarget(tc) --Just to get a visual hint in case of multiple copies, at least while Matau is in GY
-  end
 end
 function s.condition2_1(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsTurnPlayer(tp) and Duel.GetTurnCount()~=e:GetLabel()
 end
 function s.operation2_1(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	if e:GetLabelObject():GetFlagEffect(id)~=0 and aux.NecroValleyFilter(s.filter2)(tc) then
-		if Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)<Duel.GetFieldGroupCount(1-tp,LOCATION_MZONE,0) then
+  local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter2),tp,LOCATION_GRAVE,0,1,1,c)
+	if g:GetCount()>0 then
+		if Duel.SendtoHand(g,nil,REASON_EFFECT)>0 and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)<Duel.GetFieldGroupCount(1-tp,LOCATION_MZONE,0)
+      and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
       Duel.BreakEffect()
-      Duel.Draw(tp,1,REASON_EFFECT)
+      local og=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+      for tc in aux.Next(og) do
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(-700)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e1)
+        local e2=e1:Clone()
+        e2:SetCode(EFFECT_UPDATE_DEFENSE)
+        tc:RegisterEffect(e2)
+      end
     end
 	end
 end
