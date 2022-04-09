83035e05fa3848faa09e6782417841179cfb7750
utso <cccygodev@gmail.com>
2022-04-09 20:03:23 +0200

    Implement Makoki Stone

--- cards-bcot/script/c10100054.lua
+++ cards-bcot/script/c10100054.lua
@@ -1,61 +1,87 @@
 --Makoki Stone
-function c10100054.initial_effect(c)
-	c:SetUniqueOnField(1,0,10100054)
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
 	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
+	local e0=Effect.CreateEffect(c)
+  e0:SetCategory(CATEGORY_REMOVE)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+  e0:SetTarget(s.target0)
+  e0:SetOperation(s.operation0)
+	c:RegisterEffect(e0)
 	--Add counter
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e1:SetCode(EVENT_CHAIN_SOLVING)
+	e1:SetRange(LOCATION_SZONE) 
+  e1:SetCondition(s.condition1)
+	e1:SetOperation(s.operation1)
+	c:RegisterEffect(e1)
+	--To hand
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_COUNTER)
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e2:SetCode(EVENT_CHAIN_SOLVED)
-	e2:SetRange(LOCATION_SZONE) 
-	e2:SetOperation(c10100054.operation2)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_SZONE)
+  e2:SetLabelObject(e0)
+	e2:SetCost(s.cost2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
 	c:RegisterEffect(e2)
-	--Search
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(10100054,0))
-	e3:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCondition(c10100054.condition3)
-	e3:SetCost(c10100054.cost3)
-	e3:SetTarget(c10100054.target3)
-	e3:SetOperation(c10100054.operation3)
-	c:RegisterEffect(e3)
 end
---e2 - Add counter
-function c10100054.filter2(c)
-	return c:IsSetCard(0x155) or c:IsSetCard(0x156) or c:IsSetCard(0x157)
+function s.target0(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,LOCATION_DECK,0,1,nil,tp,POS_FACEDOWN) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
 end
-function c10100054.operation2(e,tp,eg,ep,ev,re,r,rp)
-	if re and re:GetHandler() and c10100054.filter2(re:GetHandler()) then
-		e:GetHandler():AddCounter(0x10b1,1)
+function s.operation0(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local tc=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_DECK,0,1,1,nil,tp,POS_FACEDOWN):GetFirst()
+	if tc and Duel.Remove(tc,POS_FACEDOWN,REASON_EFFECT)>0 then
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,0)
+		e:SetLabelObject(tc)
 	end
 end
---e3 - Search
-function c10100054.filter3(c)
-	return (c:IsType(TYPE_SPELL) or c:IsType(TYPE_TRAP)) and c:IsAbleToHand()
+function s.filter1(c)
+	return c:IsSetCard(0xb01) or c:IsSetCard(0xb02) or c:IsSetCard(0xb03)
 end
-function c10100054.condition3(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetCounter(0x10b1)>=6
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+	return re and s.filter1(re:GetHandler()) and not re:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
-function c10100054.cost3(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
-	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+  e:GetHandler():AddCounter(0x10b1,1)
 end
-function c10100054.target3(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c = e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(c10100054.filter3,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+  local c=e:GetHandler()
+	if chk==0 then return c:IsStatus(STATUS_EFFECT_ENABLED) and c:IsAbleToGraveAsCost() and c:GetCounter(0x10b1)>=6 end
+	Duel.SendtoGrave(c,REASON_COST)
 end
-function c10100054.operation3(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.SelectMatchingCard(tp,c10100054.filter3,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=e:GetLabelObject():GetLabelObject()
+	if chk==0 then return tc and tc:GetFlagEffect(id)>0 and tc:IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,tc,1,0,0)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject():GetLabelObject()
+	if tc and tc:GetFlagEffect(id)~=0 then
+    Duel.SendtoHand(tc,nil,REASON_EFFECT)
+    if tc:IsLocation(LOCATION_HAND) then
+      local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+			e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+			e1:SetTargetRange(1,0)
+			e1:SetValue(s.value2_1)
+			e1:SetLabelObject(tc)
+			e1:SetReset(RESET_PHASE+PHASE_END)
+			Duel.RegisterEffect(e1,tp)
+    end
+  end
+end
+function s.value2_1(e,re,tp)
+	local tc=e:GetLabelObject()
+	return re:GetHandler():IsCode(tc:GetCode())
 end
