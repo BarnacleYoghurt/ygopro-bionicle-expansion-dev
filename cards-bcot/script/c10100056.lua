7d74f481a1d987f6f8277f9a7a52f2289e2f55ee
utso <cccygodev@gmail.com>
2022-04-11 18:22:50 +0200

    Copper Kanohi: Add floating effect

--- cards-bcot/script/c10100056.lua
+++ cards-bcot/script/c10100056.lua
@@ -19,6 +19,18 @@ function s.initial_effect(c)
   e2:SetTarget(s.target2)
   e2:SetOperation(s.operation2)
   c:RegisterEffect(e2)
+  --Equip to highest ATK
+  local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_EQUIP)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCountLimit(1,id)
+	e3:SetCondition(s.condition3)
+	e3:SetTarget(s.target3)
+	e3:SetOperation(s.operation3)
+	c:RegisterEffect(e3)
 end
 function s.condition2(e,tp,eg,ep,ev,re,r,rp)
 	local ec=e:GetHandler():GetEquipTarget()
@@ -36,4 +48,36 @@ function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	if c:IsRelateToEffect(e) and c:GetEquipTarget():GetBattleTarget():IsRelateToBattle() then
     Duel.Destroy(Group.FromCards(c:GetEquipTarget():GetBattleTarget(),c),REASON_EFFECT)
   end
+end
+function s.condition3(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_DESTROY) and c:CheckUniqueOnField(tp)
+end
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+  local c=e:GetHandler()
+	if chk==0 then return c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,c,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,0,0)
+end
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+  tg=tg:GetMaxGroup(Card.GetAttack)
+	if c:IsRelateToEffect(e) and c:CheckUniqueOnField(tp) then
+    if tg:GetCount()>1 then
+      Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+			tg=tg:Select(tp,1,1,nil)
+			Duel.HintSelection(tg)
+    end
+    if tg:GetCount()>0 and Duel.Equip(tp,c,tg:GetFirst()) then
+      local e1=Effect.CreateEffect(c)
+      e1:SetDescription(3300)
+      e1:SetType(EFFECT_TYPE_SINGLE)
+      e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+      e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+      e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+      e1:SetValue(LOCATION_REMOVED)
+      c:RegisterEffect(e1,true)
+    end
+	end
 end
