7276d7dbc479daa6ab1735ff09764ed089c80695
utso <cccygodev@gmail.com>
2022-04-11 17:55:18 +0200

    Gift of the Shrine: Implement modular effect

--- cards-bcot/script/c10100051.lua
+++ cards-bcot/script/c10100051.lua
@@ -10,38 +10,69 @@ function c10100051.initial_effect(c)
 	e1:SetTarget(s.target1)
 	e1:SetOperation(s.operation1)
 	c:RegisterEffect(e1)
-  if not GhostBelleTable then GhostBelleTable={} end
-	table.insert(GhostBelleTable,e1)
 end
 function s.filter1a(c,tp)
-  return c:IsFaceup() and Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,c)
+  return c:IsFaceup() and (
+    Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,c) or
+    (Duel.IsExistingMatchingCard(s.filter1c,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) and Duel.IsExistingMatchingCard(s.filter1d,tp,LOCATION_DECK,0,1,nil,c))
+  )
 end
 function s.filter1b(c,ec)
 	return c:IsSetCard(0xb04) and c:IsType(TYPE_EQUIP) and c:CheckEquipTarget(ec)
 end
-function s.filter1c(c,e,tp)
-  return c:IsLevel(1) and c:IsRace(RACE_ROCK) and (c:GetAttack()==0 and c:IsDefenseBelow(0)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.filter1c(c)
+  return c:IsLevel(1) and c:IsRace(RACE_ROCK) and (c:GetAttack()==0 and c:IsDefenseBelow(0)) and c:IsAbleToRemoveAsCost()
+end
+function s.filter1d(c,ec)
+	return (c:IsSetCard(0x1b04) or c:IsSetCard(0x2b04)) and c:IsType(TYPE_EQUIP) and c:CheckEquipTarget(ec)
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingTarget(s.filter1a,tp,LOCATION_MZONE,0,1,nil,tp) end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
   local tg=Duel.SelectTarget(tp,s.filter1a,tp,LOCATION_MZONE,0,1,1,nil,tp)
-  local eqg=Duel.GetMatchingGroup(s.filter1b,tp,LOCATION_GRAVE+LOCATION_HAND,0,nil,tg:GetFirst())
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,eqg,1,0,0)
+  local tc=tg:GetFirst()
+  local b1=(Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,tc))
+  local b2=(Duel.IsExistingMatchingCard(s.filter1c,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) 
+    and Duel.IsExistingMatchingCard(s.filter1d,tp,LOCATION_DECK,0,1,nil,tc))
+  
+  local sel=-1
+  if b1 and b2 then
+    sel=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
+  elseif b1 then
+    sel=Duel.SelectOption(tp,aux.Stringid(id,0))
+  elseif b2 then
+    sel=Duel.SelectOption(tp,aux.Stringid(id,1))+1
+  end
+  e:SetLabel(sel)
+  
+  if sel==0 then
+    local eqg=Duel.GetMatchingGroup(s.filter1b,tp,LOCATION_GRAVE+LOCATION_HAND,0,nil,tc)
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,eqg,1,0,0)
+  elseif sel==1 then
+    local eqg=Duel.GetMatchingGroup(s.filter1d,tp,LOCATION_DECK,0,nil,tc)
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_MZONE+LOCATION_GRAVE)
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,eqg,1,0,0)
+  end
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-  if tc:IsRelateToEffect(e) and tc:IsFaceup() and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
+  
+  local sg=Group.CreateGroup()
+  if e:GetLabel()==0 then
+    sg=Duel.GetMatchingGroup(s.filter1b,tp,LOCATION_HAND+LOCATION_GRAVE,0,nil,tc)
+  elseif e:GetLabel()==1 then
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local rg=Duel.SelectMatchingCard(tp,s.filter1c,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
+    if Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)>0 then
+      sg=Duel.GetMatchingGroup(s.filter1d,tp,LOCATION_DECK,0,nil,tc)
+    end
+  end
+      
+  if sg:GetCount()>0 and tc:IsRelateToEffect(e) and tc:IsFaceup() and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-    local g=Duel.SelectMatchingCard(tp,s.filter1b,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,tc)
+    local g=sg:Select(tp,1,1,nil)
     if g:GetCount()>0 then
-      if Duel.Equip(tp,g:GetFirst(),tc) and tc:IsSetCard(0xb02) 
-        and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter1c,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) 
-        and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local sg=Duel.SelectMatchingCard(tp,s.filter1c,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
-        Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-      end
+      Duel.Equip(tp,g:GetFirst(),tc)
     end
   end
 end
