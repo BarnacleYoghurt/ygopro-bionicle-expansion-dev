b8202f3f85e95f9e219c4bf26f27e75805a58f78
utso <cccygodev@gmail.com>
2022-04-17 00:22:35 +0200

    Copper Kanohi: Replace destruction trigger with attack declaration trigger from GY

--- cards-bcot/script/c10100056.lua
+++ cards-bcot/script/c10100056.lua
@@ -23,13 +23,12 @@ function s.initial_effect(c)
   local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_EQUIP)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_TO_GRAVE)
-	e3:SetCountLimit(1,id)
-	e3:SetCondition(s.condition3)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+  e3:SetRange(LOCATION_GRAVE)
 	e3:SetTarget(s.target3)
 	e3:SetOperation(s.operation3)
+	e3:SetCountLimit(1,id)
 	c:RegisterEffect(e3)
 end
 function s.condition2(e,tp,eg,ep,ev,re,r,rp)
@@ -49,15 +48,10 @@ function s.operation2(e,tp,eg,ep,ev,re,r,rp)
     Duel.Destroy(Group.FromCards(c:GetEquipTarget():GetBattleTarget(),c),REASON_EFFECT)
   end
 end
-function s.condition3(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return c:IsReason(REASON_DESTROY) and c:CheckUniqueOnField(tp)
-end
 function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
-  local c=e:GetHandler()
-	if chk==0 then return c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,c,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,0,0)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
 end
 function s.operation3(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
