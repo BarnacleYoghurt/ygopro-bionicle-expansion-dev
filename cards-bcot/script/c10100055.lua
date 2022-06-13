d451a6132c72c0afadcb25027e80b371f07854c6
utso <cccygodev@gmail.com>
2022-06-14 00:15:14 +0200

    Suva: Change LP cost to 600; make compatible with target redirection, do not equip if target changes control before resolution

--- cards-bcot/script/c10100055.lua
+++ cards-bcot/script/c10100055.lua
@@ -5,9 +5,9 @@ function s.initial_effect(c)
   local e1a=Effect.CreateEffect(c)
 	e1a:SetType(EFFECT_TYPE_SINGLE)
 	e1a:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1a:SetCode(EFFECT_ADD_ATTRIBUTE)
 	e1a:SetRange(LOCATION_HAND)
-	e1a:SetValue(ATTRIBUTE_WIND+ATTRIBUTE_WATER+ATTRIBUTE_FIRE+ATTRIBUTE_EARTH)
+	e1a:SetCode(EFFECT_ADD_ATTRIBUTE)
+	e1a:SetValue(ATTRIBUTE_EARTH+ATTRIBUTE_WATER+ATTRIBUTE_FIRE+ATTRIBUTE_WIND)
 	c:RegisterEffect(e1a)
   local e1b=e1a:Clone()
   e1b:SetRange(LOCATION_MZONE)
@@ -18,6 +18,7 @@ function s.initial_effect(c)
   e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_EQUIP)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
+  e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EVENT_FREE_CHAIN)
   e2:SetCost(s.cost2)
@@ -36,6 +37,7 @@ function s.initial_effect(c)
   e3:SetCountLimit(1,id)
   c:RegisterEffect(e3)
 end
+s.listed_series={0xb05,0xb02,0xb04}
 function s.filter1(c)
   return c:IsFaceup() and c:IsSetCard(0xb05)
 end
@@ -51,12 +53,13 @@ function s.filter2b(c,ec)
     and not (c:IsLocation(LOCATION_GRAVE) and c:IsReason(REASON_DESTROY) and c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousPosition(POS_FACEUP) and c:GetTurnID()==Duel.GetTurnCount())
 end
 function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then return Duel.CheckLPCost(tp,500) end
-  Duel.PayLPCost(tp,500)
+  if chk==0 then return Duel.CheckLPCost(tp,600) end
+  Duel.PayLPCost(tp,600)
 end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
   local c=e:GetHandler()
-  if chk==0 then return Duel.IsExistingMatchingCard(s.filter2a,tp,LOCATION_MZONE,0,1,nil,tp) and c:GetFlagEffect(id)==0 end
+  if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter2a(chkc,tp) end
+  if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingTarget(s.filter2a,tp,LOCATION_MZONE,0,1,nil,tp) and c:GetFlagEffect(id)==0 end
   c:RegisterFlagEffect(id,RESET_CHAIN,0,1)
   local tg=Duel.SelectTarget(tp,s.filter2a,tp,LOCATION_MZONE,0,1,1,nil,tp)
   local eqg=Duel.GetMatchingGroup(s.filter2b,tp,LOCATION_GRAVE+LOCATION_HAND,0,nil,tg:GetFirst())
@@ -64,7 +67,7 @@ function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
   local tc=Duel.GetFirstTarget()
-  if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+  if Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsControler(tp) then
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
     local g=Duel.SelectMatchingCard(tp,s.filter2b,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,tc)
     if g:GetCount()>0 then
