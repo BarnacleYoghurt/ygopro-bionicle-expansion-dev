a1d2b4dfec32d98e98f12a27891aedd477f615c9
utso <cccygodev@gmail.com>
2022-06-16 00:21:05 +0200

    Gali: Add chkc handling, only boost ATK if negation is actually applying.

--- cards-bcot/script/c10100002.lua
+++ cards-bcot/script/c10100002.lua
@@ -1,5 +1,5 @@
 if not bcot then
-	dofile "expansions/util-bcot.lua"
+	Duel.LoadScript("../util-bcot.lua")
 end
 --Toa Mata Gali
 local s,id=GetID()
@@ -12,23 +12,26 @@ function s.initial_effect(c)
   local e2=Effect.CreateEffect(c)
   e2:SetDescription(aux.Stringid(id,1))
   e2:SetCategory(CATEGORY_DISABLE+CATEGORY_ATKCHANGE)
-  e2:SetRange(LOCATION_MZONE)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_CHAINING)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+  e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_CHAINING)
   e2:SetCondition(s.condition2)
   e2:SetTarget(s.target2)
   e2:SetOperation(s.operation2)
   e2:SetCountLimit(1)
   c:RegisterEffect(e2)
 end
+s.listed_series={0x1b02}
 function s.condition2(e,tp,eg,ep,ev,re,r,rp)
 	local rc=re:GetHandler()
 	return re:IsActiveType(TYPE_MONSTER) and not rc:IsCode(id) and rp==1-Duel.GetTurnPlayer()
     and (Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()) --Can be activated up to damage calc because ATK Change, like AD Thunder Lv10
 end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsLocation(LOCATION_MZONE) and aux.disfilter1(chkc) and chkc~=e:GetHandler() end
 	if chk==0 then return Duel.IsExistingTarget(aux.disfilter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
   local g=Duel.SelectTarget(tp,aux.disfilter1,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,e:GetHandler())
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
 end
@@ -50,12 +53,13 @@ function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetValue(RESET_TURN_SET)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e2)
-    
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetCode(EFFECT_UPDATE_ATTACK)
-    e3:SetValue(400)
-		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
-    c:RegisterEffect(e3)
+    if not tc:IsImmuneToEffect(e1) and not tc:IsImmuneToEffect(e2) then
+      local e3=Effect.CreateEffect(c)
+      e3:SetType(EFFECT_TYPE_SINGLE)
+      e3:SetCode(EFFECT_UPDATE_ATTACK)
+      e3:SetValue(400)
+      e3:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+      c:RegisterEffect(e3)
+    end
 	end
 end
