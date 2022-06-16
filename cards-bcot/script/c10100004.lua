fe0dbdadb2d0f2d98ee07ca20b22b12e524c6845
utso <cccygodev@gmail.com>
2022-06-16 14:29:48 +0200

    Pohatu: Additional card to destroy must be different from first card if that is still on the field, for consistency with 'additional target' (e.g. Mega Raiza)

--- cards-bcot/script/c10100004.lua
+++ cards-bcot/script/c10100004.lua
@@ -1,5 +1,5 @@
 if not bcot then
-	dofile "expansions/util-bcot.lua"
+	Duel.LoadScript("../util-bcot.lua")
 end
 --Toa Mata Pohatu
 local s,id=GetID()
@@ -12,10 +12,10 @@ function s.initial_effect(c)
 	local e2a=Effect.CreateEffect(c)
 	e2a:SetDescription(aux.Stringid(id,1))
 	e2a:SetCategory(CATEGORY_DESTROY)
-	e2a:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e2a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2a:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2a:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e2a:SetRange(LOCATION_MZONE)
+	e2a:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2a:SetCondition(s.condition2a)
 	e2a:SetTarget(s.target2)
 	e2a:SetOperation(s.operation2)
@@ -26,6 +26,7 @@ function s.initial_effect(c)
   e2b:SetCondition(s.condition2b)
   c:RegisterEffect(e2b)
 end
+s.listed_series={0x1b02}
 function s.filter2a(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP)
 end
@@ -39,7 +40,7 @@ function s.condition2b(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetFlagEffect(id)==0 and re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsSummonLocation(LOCATION_EXTRA) 
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and s.filter2(chkc) end
+	if chkc then return chkc:IsOnField() and s.filter2a(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.filter2a,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,s.filter2a,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
@@ -51,9 +52,9 @@ function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
-    if Duel.IsExistingMatchingCard(s.filter2a,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) and Duel.IsExistingMatchingCard(s.filter2b,tp,LOCATION_MZONE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+    if Duel.IsExistingMatchingCard(s.filter2a,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil,tc) and Duel.IsExistingMatchingCard(s.filter2b,tp,LOCATION_MZONE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
       Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-      local g=Duel.SelectMatchingCard(tp,s.filter2a,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+      local g=Duel.SelectMatchingCard(tp,s.filter2a,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil,tc)
       if g:GetCount()>0 then
         Duel.Destroy(g,REASON_EFFECT)
       end
