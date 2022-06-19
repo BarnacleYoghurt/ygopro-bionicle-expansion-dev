b685ef9f4609f14068aa0a715c948fd2de9766e6
utso <cccygodev@gmail.com>
2022-06-19 14:54:43 +0200

    Ruru: Do not reveal card if it is no longer Set

--- cards-bcot/script/c10100042.lua
+++ cards-bcot/script/c10100042.lua
@@ -23,13 +23,13 @@ function s.initial_effect(c)
   --Recycle
   local e3=bcot.kanohi_revive(c,10100019)
   e3:SetDescription(aux.Stringid(id,1))
-  e3:SetCountLimit(1,id+1000000)
+  e3:SetCountLimit(1,{id,1})
   c:RegisterEffect(e3)
 end
 s.listed_names={10100019}
 s.listed_series={0xb04,0xb03,0xb02,0xb07}
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-  if chkc then return chkc:IsCanBeEffectTarget(e) and chkc:IsFacedown() and chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(1-tp) end 
+  if chkc then return chkc:IsFacedown() and chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(1-tp) end 
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFacedown,tp,0,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEDOWN)
   local g=Duel.SelectTarget(tp,Card.IsFacedown,tp,0,LOCATION_ONFIELD,1,1,nil)
@@ -37,7 +37,7 @@ end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
   local ec=e:GetHandler():GetEquipTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) and tc:IsFacedown() then
     Duel.ConfirmCards(tp,tc)
     if tc:IsType(TYPE_SPELL+TYPE_TRAP) then
       Duel.Damage(1-tp,500,REASON_EFFECT)
