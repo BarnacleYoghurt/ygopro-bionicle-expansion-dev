13308b82d18cc8fcf3abd20c95ea2b39b9303ee2
utso <cccygodev@gmail.com>
2022-06-20 12:37:18 +0200

    Makoki Stone: Only count monster effects and ignore resolution of effects whose activation was not seen

--- cards-bcot/script/c10100054.lua
+++ cards-bcot/script/c10100054.lua
@@ -1,8 +1,9 @@
 --Makoki Stone
 local s,id=GetID()
+local COUNTER_KEY=0x10b1
 function s.initial_effect(c)
 	c:SetUniqueOnField(1,0,id)
-  c:SetCounterLimit(0x10b1,6)
+  c:SetCounterLimit(COUNTER_KEY,6)
 	--Activate
 	local e0=Effect.CreateEffect(c)
   e0:SetCategory(CATEGORY_REMOVE)
@@ -12,27 +13,36 @@ function s.initial_effect(c)
   e0:SetOperation(s.operation0)
 	c:RegisterEffect(e0)
 	--Add counter
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_COUNTER)
-	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e1:SetCode(EVENT_CHAIN_SOLVING)
-	e1:SetRange(LOCATION_SZONE) 
-  e1:SetCondition(s.condition1)
-	e1:SetOperation(s.operation1)
-	c:RegisterEffect(e1)
+  local e1a=Effect.CreateEffect(c)
+  e1a:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e1a:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1a:SetRange(LOCATION_SZONE)
+	e1a:SetCode(EVENT_CHAINING)
+	e1a:SetOperation(aux.chainreg)
+	c:RegisterEffect(e1a)
+	local e1b=Effect.CreateEffect(c)
+	e1b:SetCategory(CATEGORY_COUNTER)
+	e1b:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e1b:SetRange(LOCATION_SZONE) 
+	e1b:SetCode(EVENT_CHAIN_SOLVING)
+  e1b:SetCondition(s.condition1)
+	e1b:SetOperation(s.operation1)
+	c:RegisterEffect(e1b)
 	--To hand
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_TOHAND)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_SZONE)
-  e2:SetLabelObject(e0)
+	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetCost(s.cost2)
 	e2:SetTarget(s.target2)
 	e2:SetOperation(s.operation2)
+  e2:SetLabelObject(e0)
 	c:RegisterEffect(e2)
 end
+s.listed_series={0xb01,0xb03,0xb02}
+s.counter_place_list={COUNTER_KEY}
 function s.target0(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,LOCATION_DECK,0,1,nil,tp,POS_FACEDOWN) end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
@@ -50,14 +60,14 @@ function s.filter1(c)
 	return c:IsSetCard(0xb01) or c:IsSetCard(0xb02) or c:IsSetCard(0xb03)
 end
 function s.condition1(e,tp,eg,ep,ev,re,r,rp)
-	return re and s.filter1(re:GetHandler()) and not re:IsHasType(EFFECT_TYPE_ACTIVATE)
+	return re and s.filter1(re:GetHandler()) and re:IsActiveType(TYPE_MONSTER) and e:GetHandler():GetFlagEffect(1)>0
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-  e:GetHandler():AddCounter(0x10b1,1)
+  e:GetHandler():AddCounter(COUNTER_KEY,1)
 end
 function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
   local c=e:GetHandler()
-	if chk==0 then return c:IsStatus(STATUS_EFFECT_ENABLED) and c:IsAbleToGraveAsCost() and c:GetCounter(0x10b1)==6 end
+	if chk==0 then return c:IsStatus(STATUS_EFFECT_ENABLED) and c:IsAbleToGraveAsCost() and c:GetCounter(COUNTER_KEY)==6 end
 	Duel.SendtoGrave(c,REASON_COST)
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
