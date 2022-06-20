6a4fb8307d20d08aa2cb8805a500a76af98124ef
utso <cccygodev@gmail.com>
2022-06-20 09:50:33 +0200

    Rua: Get Xyz effect from util

--- cards-bcot/script/c10100049.lua
+++ cards-bcot/script/c10100049.lua
@@ -24,14 +24,9 @@ function s.initial_effect(c)
   e3:SetCondition(s.condition)
 	c:RegisterEffect(e3)
 	--Xyz
-	local e4=Effect.CreateEffect(c)
+	local e4=bcot.kanohi_xyz(c)
 	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e4:SetType(EFFECT_TYPE_IGNITION)
-	e4:SetRange(LOCATION_SZONE)
-	e4:SetCondition(s.condition4)
-	e4:SetTarget(s.target4)
-	e4:SetOperation(s.operation4)
+  e4:SetCountLimit(1)
 	c:RegisterEffect(e4)
 end
 s.listed_series={0xb04,0x1b02,0x2b02,0xb02}
@@ -40,37 +35,4 @@ function s.condition(e)
 end
 function s.value2(e,re)
 	return e:GetHandlerPlayer()~=re:GetOwnerPlayer()
-end
-function s.filter4a(c,e,tp,lv)
-	return c:IsSetCard(0x1b02) and c:IsLevel(lv) and c:IsCanBeSpecialSummoned(e,0,tp,tp,false,false) 
-    and Duel.IsExistingMatchingCard(s.filter4b,tp,LOCATION_EXTRA,0,1,nil,tp,Group.FromCards(c))
-end
-function s.filter4b(c,tp,sg)
-  local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
-	return c:IsSetCard(0xb02) and c:IsXyzSummonable(sg:GetFirst(),Group.Merge(g,sg)) and Duel.GetLocationCountFromEx(tp,tp,Group.Merge(g,sg),c)>0
-end
-function s.condition4(e,tp,eg,ep,ev,re,r,rp)
-  return bcot.kanohi_con(e,{0x1b02}) and e:GetHandler():GetEquipTarget():IsControler(tp)
-end
-function s.target4(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-  local ec=e:GetHandler():GetEquipTarget()
-	if chk==0 then
-    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and ec and ec:HasLevel()
-      and Duel.IsExistingMatchingCard(s.filter4a,tp,LOCATION_HAND,0,1,nil,e,tp,ec:GetLevel())
-  end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
-end
-function s.operation4(e,tp,eg,ep,ev,re,r,rp)
-  local ec=e:GetHandler():GetEquipTarget()
-  if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 or not (ec and ec:HasLevel() and e:GetHandler():IsRelateToEffect(e)) then return end
-  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-  local g=Duel.SelectMatchingCard(tp,s.filter4a,tp,LOCATION_HAND,0,1,1,nil,e,tp,ec:GetLevel())
-	if g:GetCount()>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
-    Duel.BreakEffect()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local xyzg=Duel.SelectMatchingCard(tp,s.filter4b,tp,LOCATION_EXTRA,0,1,1,nil,tp,g)
-		if xyzg:GetCount()>0 then
-			Duel.XyzSummon(tp,xyzg:GetFirst(),g:GetFirst())
-		end
-  end
 end
