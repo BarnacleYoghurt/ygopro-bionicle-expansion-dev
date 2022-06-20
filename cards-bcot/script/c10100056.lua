8ffd03145bcc6e54050d0c78cc00d69c1b675d22
utso <cccygodev@gmail.com>
2022-06-20 12:54:44 +0200

    Copper Kanohi: Add listed series, add simpler detection of ongoing battle, remove pointless UniqueOnField check.

--- cards-bcot/script/c10100056.lua
+++ cards-bcot/script/c10100056.lua
@@ -1,5 +1,5 @@
 if not bcot then
-	dofile "expansions/util-bcot.lua"
+	Duel.LoadScript("../util-bcot.lua")
 end
 --Copper Kanohi of Victory
 local s,id=GetID()
@@ -13,8 +13,8 @@ function s.initial_effect(c)
   e2:SetDescription(aux.Stringid(id,0))
   e2:SetCategory(CATEGORY_DESTROY)
   e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-  e2:SetCode(EVENT_BATTLE_START)
   e2:SetRange(LOCATION_SZONE)
+  e2:SetCode(EVENT_BATTLE_START)
   e2:SetCondition(s.condition2)
   e2:SetTarget(s.target2)
   e2:SetOperation(s.operation2)
@@ -24,16 +24,17 @@ function s.initial_effect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_EQUIP)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
   e3:SetRange(LOCATION_GRAVE)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e3:SetTarget(s.target3)
 	e3:SetOperation(s.operation3)
 	e3:SetCountLimit(1,id)
 	c:RegisterEffect(e3)
 end
+s.listed_series={0xb04,0xb01,0xb03,0xb02}
 function s.condition2(e,tp,eg,ep,ev,re,r,rp)
 	local ec=e:GetHandler():GetEquipTarget()
-	return ec and (ec:IsSetCard(0xb01) or ec:IsSetCard(0xb02) or ec:IsSetCard(0xb03)) and (Duel.GetAttacker()==ec or Duel.GetAttackTarget()==ec)
+	return ec and (ec:IsSetCard(0xb01) or ec:IsSetCard(0xb02) or ec:IsSetCard(0xb03)) and ec:GetBattleTarget()
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
@@ -44,7 +45,8 @@ function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:GetEquipTarget():GetBattleTarget():IsRelateToBattle() then
+  local tc=c:GetEquipTarget():GetBattleTarget()
+	if c:IsRelateToEffect(e) and tc and tc:IsRelateToBattle() then
     Duel.Destroy(Group.FromCards(c:GetEquipTarget():GetBattleTarget(),c),REASON_EFFECT)
   end
 end
@@ -57,7 +59,7 @@ function s.operation3(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
   tg=tg:GetMaxGroup(Card.GetAttack)
-	if c:IsRelateToEffect(e) and c:CheckUniqueOnField(tp) then
+	if c:IsRelateToEffect(e) then
     if tg:GetCount()>1 then
       Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 			tg=tg:Select(tp,1,1,nil)
@@ -67,10 +69,10 @@ function s.operation3(e,tp,eg,ep,ev,re,r,rp)
       local e1=Effect.CreateEffect(c)
       e1:SetDescription(3300)
       e1:SetType(EFFECT_TYPE_SINGLE)
-      e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
       e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
-      e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+      e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
       e1:SetValue(LOCATION_REMOVED)
+      e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
       c:RegisterEffect(e1,true)
     end
 	end
