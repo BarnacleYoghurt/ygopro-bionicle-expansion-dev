e156219d735f8abfd81822cfb2552b6c29c0ee9c
utso <cccygodev@gmail.com>
2022-06-20 19:06:25 +0200

    Akamai: Do not allow activating burn effect if it would inflict 0 damage (i.e., do nothing)

--- cards-bcot/script/c10100013.lua
+++ cards-bcot/script/c10100013.lua
@@ -2,24 +2,24 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Xyz Summon
-	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0xb02),6,3)
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x1b02),6,3)
 	c:EnableReviveLimit()
   --No effects in BP
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
-	e1:SetTargetRange(0,1)
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,1)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e1:SetCondition(s.condition1)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
   --Negate effects while battling
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_DISABLE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetCode(EFFECT_DISABLE)
 	e2:SetTarget(s.target2)
 	c:RegisterEffect(e2)
   --Burn
@@ -36,6 +36,7 @@ function s.initial_effect(c)
   e3:SetCountLimit(1)
 	c:RegisterEffect(e3)
 end
+s.listed_series={0x1b02}
 function s.condition1(e)
   return Duel.IsBattlePhase()
 end
@@ -47,10 +48,10 @@ function s.cost3(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
 function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
 	local tc=e:GetHandler():GetBattleTarget()
 	local atk=tc:GetBaseAttack()
 	if atk<0 then atk=0 end
+	if chk==0 then return atk>0 end
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(atk)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,atk)
