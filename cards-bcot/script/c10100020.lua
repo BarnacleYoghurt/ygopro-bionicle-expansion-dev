bc64ec507af951a858326bb29e3fc615e82b2596
utso <cccygodev@gmail.com>
2022-06-23 15:28:31 +0200

    Onewa: Also allow adding from GY if you do not yet have any cards to banish in hand (but you must be able to banish in general)

--- cards-bcot/script/c10100020.lua
+++ cards-bcot/script/c10100020.lua
@@ -6,6 +6,7 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
   --Draw & banish
   local e1=Effect.CreateEffect(c)
+  e1:SetDescription(aux.Stringid(id,0))
   e1:SetCategory(CATEGORY_DRAW+CATEGORY_REMOVE)
   e1:SetType(EFFECT_TYPE_IGNITION)
   e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -16,6 +17,7 @@ function s.initial_effect(c)
   c:RegisterEffect(e1)
 	--Special Summon
 	local e2=Effect.CreateEffect(c)
+  e1:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
   e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
@@ -24,7 +26,7 @@ function s.initial_effect(c)
   e2:SetCondition(aux.zptcon(s.filter2a))
   e2:SetTarget(s.target2)
   e2:SetOperation(s.operation2)
-  e2:SetCountLimit(1,id+1000000)
+  e2:SetCountLimit(1,{id,1})
   c:RegisterEffect(e2)
 end
 function s.filter0(c)
@@ -38,12 +40,12 @@ function s.filter1(c)
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
   if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter1(chkc) end
-  if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_GRAVE,0,1,nil) and Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,LOCATION_HAND,0,1,nil) end
+  if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_GRAVE,0,1,nil) and Duel.IsPlayerCanRemove(tp) end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local tg=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_GRAVE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,tg,tg:GetCount(),0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,tg,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_ALL,1)
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_HAND)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
   local pct=Duel.GetFieldGroupCount(tp,LOCATION_HAND+LOCATION_ONFIELD,0)
@@ -56,11 +58,13 @@ function s.operation1(e,tp,eg,ep,ev,re,r,rp)
   end
   
   local tc=Duel.GetFirstTarget()
-  if tc:IsRelateToEffect(e) then
+  if tc:IsRelateToEffect(e) and s.filter1(tc) then
     if Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
       Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
       local g1=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_HAND,0,1,1,nil)
-      Duel.Remove(g1,POS_FACEUP,REASON_EFFECT)
+      if g1:GetCount()>0 then
+        Duel.Remove(g1,POS_FACEUP,REASON_EFFECT)
+      end
     end
   end
 end
@@ -78,7 +82,6 @@ function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
   Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tg,1,0,0)
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-  local c=e:GetHandler()
   local tc=Duel.GetFirstTarget()
   if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and tc:IsRelateToEffect(e) then
     Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
