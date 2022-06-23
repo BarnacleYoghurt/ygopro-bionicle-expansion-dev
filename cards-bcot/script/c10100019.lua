3a453af013bc6f16cc4ddab068e463a4d4b728e2
utso <cccygodev@gmail.com>
2022-06-23 14:41:58 +0200

    Whenua: Remove redundant check for TYPE_MONSTER, make LP gain behave more like a continuous effect by removing target function

--- cards-bcot/script/c10100019.lua
+++ cards-bcot/script/c10100019.lua
@@ -7,20 +7,19 @@ function s.initial_effect(c)
 	--LP gain
   local e1=Effect.CreateEffect(c)
   e1:SetCategory(CATEGORY_RECOVER)
-  e1:SetRange(LOCATION_MZONE)
   e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+  e1:SetRange(LOCATION_MZONE)
   e1:SetCode(EVENT_TO_GRAVE)
   e1:SetCondition(s.condition1)
-  e1:SetTarget(s.target1)
   e1:SetOperation(s.operation1)
   c:RegisterEffect(e1)
   --Search
   local e2=Effect.CreateEffect(c)
-  e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
   e2:SetDescription(aux.Stringid(id,0))
-  e2:SetRange(LOCATION_MZONE)
+  e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
   e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+  e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
   e2:SetCondition(s.condition2)
   e2:SetCost(s.cost2)
@@ -36,24 +35,17 @@ function s.check0(g,lc)
   return g:IsExists(s.filter0,1,nil)
 end
 function s.filter1(c,tp)
-  return c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsPreviousLocation(LOCATION_HAND+LOCATION_ONFIELD) and c:GetPreviousControler()==tp
+  return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsPreviousLocation(LOCATION_HAND+LOCATION_ONFIELD) and c:GetPreviousControler()==tp
 end
 function s.condition1(e,tp,eg,ep,ev,re,r,rp)
   return eg:IsExists(s.filter1,1,nil,tp)
 end
-function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then return true end
-  local val=400*eg:FilterCount(s.filter1,nil,tp)
-  Duel.SetTargetPlayer(tp)
-  Duel.SetTargetParam(val)
-  Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,val)
-end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-  local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-  Duel.Recover(p,d,REASON_EFFECT)
+  Duel.Hint(HINT_CARD,0,id)
+  Duel.Recover(tp,400*eg:FilterCount(s.filter1,nil,tp),REASON_EFFECT)
 end
 function s.filter2(c,tp)
-  return c:IsType(TYPE_MONSTER) and c:IsLevelBelow(4) and c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAbleToHand() 
+  return c:IsLevelBelow(4) and c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAbleToHand() 
     and not Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,c:GetCode())
 end
 function s.condition2(e,tp,eg,ep,ev,re,r,rp)
@@ -65,8 +57,7 @@ function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
   if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil,tp) end
-  local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_DECK,0,nil,tp)
-  Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,tp,LOCATION_DECK)
+  Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
