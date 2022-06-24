8c163d7b46689790a1aad51a305da6e30c25fc9a
utso <cccygodev@gmail.com>
2022-06-24 23:35:30 +0200

    Ta-Koro: Clean up outdated/redundant code in ATK boosting effect

--- cards-bcot/script/c10100025.lua
+++ cards-bcot/script/c10100025.lua
@@ -20,8 +20,7 @@ function s.initial_effect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_ATKCHANGE)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetRange(LOCATION_SZONE)
+	e2:SetRange(LOCATION_FZONE)
 	e2:SetCode(EVENT_BATTLE_CONFIRM)
 	e2:SetHintTiming(TIMING_DAMAGE_STEP)
 	e2:SetCondition(s.condition2)
@@ -37,44 +36,36 @@ end
 function s.condition1(e)
   local tp=e:GetHandlerPlayer()
   local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-  return g:GetCount()>=2 and g:GetCount()==g:FilterCount(s.filter1,nil)
+  return g:GetCount()>=2 and g:GetCount()==g:FilterCount(Card.IsAttribute,nil,ATTRIBUTE_FIRE)
 end
 function s.condition2(e,tp,eg,ep,ev,re,r,rp)
-	local phase=Duel.GetCurrentPhase()
-	if phase~=PHASE_DAMAGE or Duel.IsDamageCalculated() then return false end
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
-  if a:GetControler()~=tp then
-    local s=a
-    a=d
-    d=s
+  if not d then return false end  
+  if not a:IsControler(tp) then 
+    a,d=d,a 
   end
+  e:SetLabelObject(a)
   
-	return a and d 
-    and a:GetControler()==tp and d:GetControler()~=tp 
-    and a:IsFaceup() and d:IsFaceup()
-    and a:IsAttribute(ATTRIBUTE_FIRE) and a:IsRelateToBattle() and a:GetBaseAttack()<d:GetBaseAttack()
+	return a:IsControler(tp) and d:IsControler(1-tp) and a:IsFaceup() and d:IsFaceup()
+    and a:IsAttribute(ATTRIBUTE_FIRE) and a:GetBaseAttack()<d:GetBaseAttack()
 end
-function c10100025.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
-	Duel.DiscardHand(tp,Card.IsAbleToGraveAsCost,1,1,REASON_COST,nil)
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_DISCARD+REASON_COST,nil)
 end
-function c10100025.operation2(e,tp,eg,ep,ev,re,r,rp,chk)
-	local a=Duel.GetAttacker()
-	local d=Duel.GetAttackTarget()
-  if a:GetControler()~=tp then
-    local s=a
-    a=d
-    d=s
+function s.operation2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if e:GetHandler():IsRelateToEffect(e) then
+    local a=e:GetLabelObject()
+    if a:IsRelateToBattle() and a:IsFaceup() and a:IsControler(tp) then
+      local _,atk=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil):GetMaxGroup(Card.GetBaseAttack)
+      
+      local e1=Effect.CreateEffect(e:GetHandler())
+      e1:SetType(EFFECT_TYPE_SINGLE)
+      e1:SetCode(EFFECT_UPDATE_ATTACK)
+      e1:SetValue(atk)
+      e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+      a:RegisterEffect(e1)
+    end
   end
-	if not a:IsRelateToBattle() or not d:IsRelateToBattle() then return end
-  
-  local _,atk=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil):GetMaxGroup(Card.GetBaseAttack)
-  
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(atk)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-  a:RegisterEffect(e1)
 end
