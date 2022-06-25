ec1e97cfae5a4131514c475615f3b894d776e851
utso <cccygodev@gmail.com>
2022-06-25 20:56:52 +0200

    Taipu: Properly check effect relation and faceup status before applying stat boost to target

--- cards-bcot/script/c10100033.lua
+++ cards-bcot/script/c10100033.lua
@@ -5,24 +5,25 @@ function s.initial_effect(c)
   local e1=Effect.CreateEffect(c)
   e1:SetType(EFFECT_TYPE_FIELD)
   e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-  e1:SetCode(EFFECT_SPSUMMON_PROC)
   e1:SetRange(LOCATION_HAND)
+  e1:SetCode(EFFECT_SPSUMMON_PROC)
   e1:SetCondition(s.condition1)
   e1:SetOperation(s.operation1)
   e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
   c:RegisterEffect(e1)
   --Raise ATK
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_ATKCHANGE)
   e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_TO_GRAVE)
   e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_TO_GRAVE)
   e2:SetCondition(s.condition2)
 	e2:SetTarget(s.target2)
 	e2:SetOperation(s.operation2)
 	c:RegisterEffect(e2)
 end
+s.listed_series={0x1b01}
 function s.target1_1(e,c)
   return not c:IsSetCard(0x1b01)
 end
@@ -33,8 +34,8 @@ end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp,c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
   e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
 	e1:SetTargetRange(LOCATION_MZONE,0)
   e1:SetTarget(s.target1_1)
 	e1:SetReset(RESET_PHASE+PHASE_END)
@@ -48,20 +49,22 @@ function s.condition2(e,tp,eg,ep,ev,re,r,rp)
   return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-  if chkc then return s.filter2(chkc) and chkc:IsLocation(LOCATION_MZONE) end
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE,0,1,nil) end
+  if chkc then return s.filter2(chkc) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter2,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,s.filter2,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(1000)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
-	tc:RegisterEffect(e1)
-  local e1b=e1:Clone()
-  e1b:SetCode(EFFECT_UPDATE_DEFENSE)
-  tc:RegisterEffect(e1b)
+  if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetValue(1000)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+    tc:RegisterEffect(e1)
+    local e1b=e1:Clone()
+    e1b:SetCode(EFFECT_UPDATE_DEFENSE)
+    tc:RegisterEffect(e1b)
+  end
 end
