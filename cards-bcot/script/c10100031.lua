cf15b0c593b9e12e481732d7c844400882e87f7a
utso <cccygodev@gmail.com>
2022-06-25 18:36:12 +0200

    Kapura: Effect block during battle now correctly affects the player, not the cards

--- cards-bcot/script/c10100031.lua
+++ cards-bcot/script/c10100031.lua
@@ -4,20 +4,20 @@ function s.initial_effect(c)
 	--Kapura'd
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
-	e1:SetRange(LOCATION_MZONE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(0,1)
-	e1:SetValue(s.value1)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e1:SetCondition(s.condition1)
+  e1:SetValue(1)
 	c:RegisterEffect(e1)
 	--Double attack
 	local e2=Effect.CreateEffect(c)
   e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
-  e2:SetRange(LOCATION_GRAVE)
   e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+  e2:SetRange(LOCATION_GRAVE)
+	e2:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
   e2:SetCondition(s.condition2)
   e2:SetCost(s.cost2)
   e2:SetTarget(s.target2)
@@ -25,19 +25,16 @@ function s.initial_effect(c)
   e2:SetCountLimit(1,id)
 	c:RegisterEffect(e2)
 end
-
-function s.value1(e,re,tp)
-	return not re:GetHandler():IsImmuneToEffect(e)
-end
+s.listed_series={0x1b01}
 function s.condition1(e)
   local a=Duel.GetAttacker()
 	return a and a:IsControler(e:GetHandlerPlayer()) and a:IsSetCard(0x1b01)
 end
-function s.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp
-end
 function s.filter2(c)
   return c:IsFaceup() and not c:IsStatus(STATUS_SUMMON_TURN+STATUS_SPSUMMON_TURN+STATUS_FLIP_SUMMON_TURN)
+end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
 end
 function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
 		local c=e:GetHandler()
