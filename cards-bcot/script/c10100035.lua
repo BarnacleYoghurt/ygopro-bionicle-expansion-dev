baff69d0e3e9687c34389dd5109c79f1016575b0
utso <cccygodev@gmail.com>
2022-06-25 23:01:42 +0200

    Kopeke: Add missing HOPT

--- cards-bcot/script/c10100035.lua
+++ cards-bcot/script/c10100035.lua
@@ -5,12 +5,13 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TODECK)
-  e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
+  e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetTarget(s.target1)
 	e1:SetOperation(s.operation1)
+  e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
   local e1b=e1:Clone()
   e1b:SetCode(EVENT_FLIP)
@@ -18,35 +19,22 @@ function s.initial_effect(c)
 	--To Defense
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_DESTROY_REPLACE)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
 	e2:SetTarget(s.target2)
   e2:SetValue(s.value2)
 	e2:SetOperation(s.operation2)
 	c:RegisterEffect(e2)
 end
+s.listed_series={0x1b01}
 function s.filter1(c)
-	return c:IsLevel(2) and c:IsRace(RACE_WARRIOR) and not c:IsCode(id) and (c:IsAbleToHand() or c:IsAbleToDeck())
+	return c:IsLevel(2) and c:IsRace(RACE_WARRIOR) and not c:IsCode(id) and c:IsAbleToHand()
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-  local function OpToDecktop(c)
-    if c:IsLocation(LOCATION_DECK) then
-      Duel.ShuffleDeck(tp)
-      Duel.MoveToDeckTop(c)
-    else
-      Duel.HintSelection(Group.FromCards(c),true)
-      Duel.SendtoDeck(c,nil,SEQ_DECKTOP,REASON_EFFECT)
-    end
-    if not c:IsLocation(LOCATION_EXTRA) then
-      Duel.ConfirmDecktop(tp,1)
-    end
-  end
-  
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter1),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
   if g:GetCount()>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
@@ -67,7 +55,7 @@ end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
   local c=e:GetHandler()
 	if chk==0 then return c:IsFaceup() and c:IsCanTurnSet() and eg:IsExists(s.filter2,1,nil,tp) and not eg:IsContains(c) end
-	return Duel.SelectYesNo(tp,aux.Stringid(id,2))
+	return Duel.SelectYesNo(tp,aux.Stringid(id,1))
 end
 function s.value2(e,c)
 	return s.filter2(c,e:GetHandlerPlayer())
