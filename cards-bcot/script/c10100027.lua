ef0421eeaf5622b04870a0166aff4a4ae6f2b950
utso <cccygodev@gmail.com>
2022-06-25 09:36:44 +0200

    Onu-Koro: Less convoluted target check for draw effect

--- cards-bcot/script/c10100027.lua
+++ cards-bcot/script/c10100027.lua
@@ -27,41 +27,47 @@ function s.initial_effect(c)
   e2:SetCost(s.cost2)
   e2:SetTarget(s.target2)
   e2:SetOperation(s.operation2)
-  e2:SetCountLimit(1,id+1000000)
+  e2:SetCountLimit(1,{id,1})
   c:RegisterEffect(e2)
 	Duel.AddCustomActivityCounter(id,ACTIVITY_SUMMON,s.counterfilter)
 	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,s.counterfilter)
 end
 s.listed_attributes={ATTRIBUTE_EARTH}
+function s.counterfilter(c)
+  return c:IsAttribute(ATTRIBUTE_EARTH)
+end
+
 function s.filter1(c)
-  return c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAbleToDeck()
+  return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAbleToDeck()
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-  if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.filter1(chkc) end
+  if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter1(chkc) end
   if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_GRAVE,0,1,nil) end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
   local tg=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_GRAVE,0,1,5,nil)
   Duel.SetOperationInfo(0,CATEGORY_TODECK,tg,tg:GetCount(),0,0)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	local tg=Duel.GetTargetCards(e)
   if Duel.SendtoDeck(tg,tp,2,REASON_EFFECT)>0 then
     local sc=tg:FilterCount(Card.IsLocation,nil,LOCATION_DECK)
-    Duel.Recover(tp,sc*600,REASON_EFFECT)
+    if sc>0 then
+      Duel.BreakEffect()
+      Duel.Recover(tp,sc*600,REASON_EFFECT)
+    end
   end
 end
 function s.filter2(c)
-  return c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAbleToGraveAsCost()
+  return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAbleToGraveAsCost()
 end
 function s.condition2(e,tp,eg,ep,ev,re,r,rp)
-  return Duel.GetLP(tp) > Duel.GetLP(1-tp)
+  return Duel.GetLP(tp)>Duel.GetLP(1-tp)
 end
 function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
   if chk==0 then
-    e:SetLabel(1) --If cost chk returns true, target chk will run next and thanks to this check if we can draw at least 1 card
     return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SUMMON)+Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 
       and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil)
-      and Duel.CheckLPCost(tp, 1000)
+      and Duel.CheckLPCost(tp,1000)
   end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
   local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
@@ -69,8 +75,8 @@ function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
   
   local options={}
   for dc=1,3 do
-    if Duel.CheckLPCost(tp, dc*1000) and Duel.IsPlayerCanDraw(tp,dc) then
-      table.insert(options, dc*1000)
+    if Duel.CheckLPCost(tp,dc*1000) and Duel.IsPlayerCanDraw(tp,dc) then
+      table.insert(options,dc*1000)
     end
   end
   Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
@@ -92,8 +98,8 @@ function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,3),nil)
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
   local dc=e:GetLabel()
-  if chk==0 then return Duel.IsPlayerCanDraw(tp,dc) end
   Duel.SetTargetPlayer(tp)
   Duel.SetTargetParam(dc)
   Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,dc)
@@ -101,14 +107,11 @@ end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
-  if Duel.GetLP(tp) < Duel.GetLP(1-tp) then
+  if Duel.GetLP(tp)<Duel.GetLP(1-tp) then
     Duel.BreakEffect()
     Duel.DiscardHand(tp,nil,d,d,REASON_EFFECT)
   end
 end
 function s.target2_1(e,c)
   return not c:IsAttribute(ATTRIBUTE_EARTH)
-end
-function s.counterfilter(c)
-  return c:IsAttribute(ATTRIBUTE_EARTH)
 end
