8c46f0bae430b1b81cd925a18ed332d4c730c0d5
utso <cccygodev@gmail.com>
2022-06-26 11:43:25 +0200

    Midak: Fix interaction of mill/heal effect with Macro Cosmos

--- cards-bcot/script/c10100063.lua
+++ cards-bcot/script/c10100063.lua
@@ -5,8 +5,8 @@ function s.initial_effect(c)
   local e1=Effect.CreateEffect(c)
   e1:SetDescription(aux.Stringid(id,0))
   e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_RECOVER)
-  e1:SetType(EFFECT_TYPE_IGNITION)
   e1:SetRange(LOCATION_HAND)
+  e1:SetType(EFFECT_TYPE_IGNITION)
   e1:SetCondition(s.condition1)
   e1:SetCost(s.cost1)
   e1:SetTarget(s.target1)
@@ -19,9 +19,10 @@ function s.initial_effect(c)
   e2:SetCode(EFFECT_SEND_REPLACE)
 	e2:SetTarget(s.target2)
 	e2:SetValue(s.value2)
-  e2:SetCountLimit(1,id+1000000)
+  e2:SetCountLimit(1,{id,1})
   c:RegisterEffect(e2)
 end
+s.listed_series={0xb01}
 function s.filter1a(c)
   return c:IsSetCard(0xb01) and c:IsFaceup() and not c:IsCode(id)
 end
@@ -39,17 +40,14 @@ end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
   if chk==0 then return Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_DECK,0,1,nil) end
   Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+  Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,400)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
   local g=Duel.SelectMatchingCard(tp,s.filter1b,tp,LOCATION_DECK,0,1,1,nil)
-  if Duel.SendtoGrave(g,REASON_EFFECT)>0 then
+  if Duel.SendtoGrave(g,REASON_EFFECT)>0 and g:GetFirst():IsLocation(LOCATION_GRAVE) then
     Duel.Recover(tp,400,REASON_EFFECT)
   end
-end
-function s.repfilter(c,tp)
-	return c:IsControler(tp) and c:IsLocation(LOCATION_GRAVE) and c:GetDestination()==LOCATION_DECK and c:IsType(TYPE_MONSTER)
-		and c:IsAbleToHand()
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
   local c=e:GetHandler()
