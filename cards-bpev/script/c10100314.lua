cea0ba8a348caffed8e5e9f357e4b9a342b804ee
utso <cccygodev@gmail.com>
2022-08-05 23:18:38 +0200

    Implement Pakari Nuva

--- cards-bpev/script/c10100314.lua
+++ cards-bpev/script/c10100314.lua
@@ -1,4 +1,77 @@
---Nuva Kanohi Pakari
-function c10100314.initial_effect(c)
+if not bcot then
+	Duel.LoadScript("../util-bcot.lua")
 end
-
+--Nuva Kanohi Pakari
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c)
+  --Destroy if replaced
+  local e1=bcot.kanohi_selfdestruct(c)
+  c:RegisterEffect(e1)
+  --Search EP monster
+  local e2=Effect.CreateEffect(c)
+  e2:SetCode(aux.Stringid(id,0))
+  e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+  e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+  e2:SetCode(EVENT_TO_GRAVE)
+  e2:SetCost(s.cost2)
+  e2:SetTarget(s.target2)
+  e2:SetOperation(s.operation2)
+  e2:SetCountLimit(1,id)
+  c:RegisterEffect(e2)
+  --ATK gain
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_EQUIP)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+  e3:SetCondition(function(e) return bcot.kanohi_con(e,{0xb0c}) end)
+	e3:SetValue(1000)
+	c:RegisterEffect(e3)
+  --Piercing
+  local e4=Effect.CreateEffect(c)
+  e4:SetType(EFFECT_TYPE_EQUIP)
+  e4:SetCode(EFFECT_PIERCE)
+  e4:SetCondition(function(e) return bcot.kanohi_con(e,{0xb0c}) end)
+  c:RegisterEffect(e4)
+  --AOE
+  local e5=Effect.CreateEffect(c)
+  e5:SetDescription(aux.Stringid(id,2))
+  e5:SetType(EFFECT_TYPE_IGNITION)
+  e5:SetRange(LOCATION_SZONE)
+  e5:SetCondition(function(e) return bcot.kanohi_con(e,{0xb0c}) end)
+  e5:SetOperation(s.operation5)
+  e5:SetCountLimit(1)
+  c:RegisterEffect(e5)
+end
+function s.filter2(c)
+  return c:IsSetCard(0xb0b) and c:IsAbleToHand()
+end
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+  Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil) end
+  Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+  local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil)
+  if #g>0 then
+    Duel.SendtoHand(g,nil,REASON_EFFECT)
+    Duel.ConfirmCards(1-tp,g)
+  end
+end
+function s.operation5(e,tp,eg,ep,ev,re,r,rp)
+  if e:GetHandler():IsRelateToEffect(e) then
+    local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+    for tc in aux.Next(g) do
+      local e1=Effect.CreateEffect(e:GetHandler())
+      e1:SetType(EFFECT_TYPE_SINGLE)
+      e1:SetCode(EFFECT_UPDATE_ATTACK)
+      e1:SetValue(1000)
+      e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+      tc:RegisterEffect(e1)
+    end
+  end
+end
