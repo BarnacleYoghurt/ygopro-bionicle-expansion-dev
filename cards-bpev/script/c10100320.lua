451ed00ea7b6b217ec6324437a4747c4a0c8e784
utso <cccygodev@gmail.com>
2022-08-14 19:38:42 +0200

    Deep Wisdom: Update benefit and drawback

--- cards-bpev/script/c10100320.lua
+++ cards-bpev/script/c10100320.lua
@@ -18,14 +18,16 @@ function s.initial_effect(c)
   e1:SetOperation(s.operation1)
   e1:SetCountLimit(1,id)
   c:RegisterEffect(e1)
-  --Gain LP
+  --Draw
   local e2=Effect.CreateEffect(c)
   e2:SetDescription(aux.Stringid(id,1))
-  e2:SetCategory(CATEGORY_RECOVER)
-  e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+  e2:SetCategory(CATEGORY_DRAW)
+  e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+  e2:SetProperty(EFFECT_FLAG_DELAY)
   e2:SetRange(LOCATION_SZONE)
   e2:SetCode(EVENT_CHAINING)
   e2:SetCondition(s.condition2)
+  e2:SetCost(s.cost2)
   e2:SetTarget(s.target2)
   e2:SetOperation(s.operation2)
   e2:SetCountLimit(1)
@@ -33,7 +35,7 @@ function s.initial_effect(c)
   --Leave field
   local e3=Effect.CreateEffect(c)
   e3:SetDescription(aux.Stringid(id,2))
-  e3:SetCategory(CATEGORY_DAMAGE)
+  e3:SetCategory(CATEGORY_REMOVE)
   e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
   e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
   e3:SetCode(EVENT_LEAVE_FIELD)
@@ -66,14 +68,20 @@ function s.condition2(e,tp,eg,ep,ev,re,r,rp)
   return re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsSetCard(0xb0c) 
     and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,10100320),tp,LOCATION_ONFIELD,0,1,nil)
 end
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then return true end
-  Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,re:GetHandler():GetAttack())
-  re:GetHandler():CreateEffectRelation(e)
+  if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-  if e:GetHandler():IsRelateToEffect(e) and re:GetHandler():IsRelateToEffect(e) and re:GetHandler():IsFaceup() then
-    Duel.Recover(tp,re:GetHandler():GetAttack(),REASON_EFFECT)
+  if e:GetHandler():IsRelateToEffect(e) then
+    local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+    Duel.Draw(p,d,REASON_EFFECT)
   end
 end
 function s.condition3(e,tp,eg,ep,ev,re,r,rp)
@@ -85,12 +93,12 @@ function s.target3(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
   local g=Duel.SelectTarget(tp,aux.FilterFaceupFunction(Card.IsCode,10100308),tp,LOCATION_ONFIELD,0,1,1,nil)
   if #g>0 then
-    Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,g:GetFirst():GetAttack())
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_HAND)
   end
 end
 function s.operation3(e,tp,eg,ep,ev,re,r,rp)
   local tc=Duel:GetFirstTarget()
-  if e:GetHandler():IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+  if tc:IsRelateToEffect(e) and tc:IsFaceup() then
     Duel.NegateRelatedChain(tc,RESET_TURN_SET)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
@@ -104,7 +112,11 @@ function s.operation3(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e2)
     if (not tc:IsImmuneToEffect(e1)) and (not tc:IsImmuneToEffect(e2)) then
-      Duel.Damage(tp,tc:GetAttack(),REASON_EFFECT)
+      Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
+      local rg=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_HAND,0,1,1,nil)
+      if #rg>0 then
+        Duel.Remove(rg,POS_FACEDOWN,REASON_EFFECT)
+      end
     end
   end
 end
