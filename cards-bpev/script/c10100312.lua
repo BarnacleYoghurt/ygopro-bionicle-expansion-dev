078ada99eb1bc22e350e1b5c0695ea6b08d8ed4d
utso <cccygodev@gmail.com>
2022-12-07 11:07:33 +0100

    Hau Nuva, Pakari Nuva: Reorder and adjust effects for new structure

--- cards-bpev/script/c10100312.lua
+++ cards-bpev/script/c10100312.lua
@@ -11,52 +11,36 @@ function s.initial_effect(c)
   --Destroy if replaced
   local e1=bcot.kanohi_selfdestruct(c)
   c:RegisterEffect(e1)
-  --Place Nuva Symbol
-  local e2=bpev.kanohi_nuva_search(c)
-  e2:SetDescription(aux.Stringid(id,0))
-  e2:SetCountLimit(1,id)
-  c:RegisterEffect(e2)
   --Indestructible by battle
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+  e2:SetCondition(function(e) return bcot.kanohi_con(e,{0xb0c}) end)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--No damage
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_EQUIP)
-	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
   e3:SetCondition(function(e) return bcot.kanohi_con(e,{0xb0c}) end)
 	e3:SetValue(1)
 	c:RegisterEffect(e3)
-	--No damage
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_EQUIP)
-	e4:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-  e4:SetCondition(function(e) return bcot.kanohi_con(e,{0xb0c}) end)
-	e4:SetValue(1)
-	c:RegisterEffect(e4)
-  --AOE
-  local e5=Effect.CreateEffect(c)
-  e5:SetDescription(aux.Stringid(id,2))
-  e5:SetType(EFFECT_TYPE_IGNITION)
-  e5:SetRange(LOCATION_SZONE)
-  e5:SetCondition(function(e) return bcot.kanohi_con(e,{0xb0c}) end)
-  e5:SetCost(s.cost5)
-  e5:SetOperation(s.operation5)
-  e5:SetCountLimit(1)
-  c:RegisterEffect(e5)
+  --Place Nuva Symbol & protect field
+  local e4=bpev.kanohi_nuva_search(c,s.operation4)
+  e4:SetDescription(aux.Stringid(id,0))
+  e4:SetCountLimit(1,id)
+  c:RegisterEffect(e4)
 end
-function s.cost5(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
-	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
-end
-function s.operation5(e,tp,eg,ep,ev,re,r,rp)
-  if e:GetHandler():IsRelateToEffect(e) then
-    local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-    for tc in aux.Next(g) do
-      local e1=Effect.CreateEffect(e:GetHandler())
-      e1:SetDescription(3000)
-      e1:SetType(EFFECT_TYPE_SINGLE)
-      e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-      e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-      e1:SetValue(1)
-      e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
-      tc:RegisterEffect(e1)
-    end
+function s.operation4(e,tp,eg,ep,ev,re,r,rp)
+  local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+  for tc in aux.Next(g) do
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetDescription(3000)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e1:SetValue(1)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+    tc:RegisterEffect(e1)
   end
 end
--- cards-bpev/script/c10100314.lua
+++ cards-bpev/script/c10100314.lua
@@ -11,50 +11,34 @@ function s.initial_effect(c)
   --Destroy if replaced
   local e1=bcot.kanohi_selfdestruct(c)
   c:RegisterEffect(e1)
-  --Place Nuva Symbol
-  local e2=bpev.kanohi_nuva_search(c)
-  e2:SetDescription(aux.Stringid(id,0))
-  e2:SetCountLimit(1,id)
-  c:RegisterEffect(e2)
   --ATK gain
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_EQUIP)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetCode(EFFECT_UPDATE_ATTACK)
-  e3:SetCondition(function(e) return bcot.kanohi_con(e,{0xb0c}) end)
-	e3:SetValue(1000)
-	c:RegisterEffect(e3)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+  e2:SetCondition(function(e) return bcot.kanohi_con(e,{0xb0c}) end)
+	e2:SetValue(1000)
+	c:RegisterEffect(e2)
   --Piercing
-  local e4=Effect.CreateEffect(c)
-  e4:SetType(EFFECT_TYPE_EQUIP)
-  e4:SetCode(EFFECT_PIERCE)
-  e4:SetCondition(function(e) return bcot.kanohi_con(e,{0xb0c}) end)
+  local e3=Effect.CreateEffect(c)
+  e3:SetType(EFFECT_TYPE_EQUIP)
+  e3:SetCode(EFFECT_PIERCE)
+  e3:SetCondition(function(e) return bcot.kanohi_con(e,{0xb0c}) end)
+  c:RegisterEffect(e3)
+  --Place Nuva Symbol & boost field
+  local e4=bpev.kanohi_nuva_search(c,s.operation4)
+  e4:SetDescription(aux.Stringid(id,0))
+  e4:SetCountLimit(1,id)
   c:RegisterEffect(e4)
-  --AOE
-  local e5=Effect.CreateEffect(c)
-  e5:SetDescription(aux.Stringid(id,2))
-  e5:SetType(EFFECT_TYPE_IGNITION)
-  e5:SetRange(LOCATION_SZONE)
-  e5:SetCondition(function(e) return bcot.kanohi_con(e,{0xb0c}) end)
-  e5:SetCost(s.cost5)
-  e5:SetOperation(s.operation5)
-  e5:SetCountLimit(1)
-  c:RegisterEffect(e5)
 end
-function s.cost5(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
-	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
-end
-function s.operation5(e,tp,eg,ep,ev,re,r,rp)
-  if e:GetHandler():IsRelateToEffect(e) then
-    local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-    for tc in aux.Next(g) do
-      local e1=Effect.CreateEffect(e:GetHandler())
-      e1:SetType(EFFECT_TYPE_SINGLE)
-      e1:SetCode(EFFECT_UPDATE_ATTACK)
-      e1:SetValue(500)
-      e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
-      tc:RegisterEffect(e1)
-    end
+function s.operation4(e,tp,eg,ep,ev,re,r,rp)
+  local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+  for tc in aux.Next(g) do
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetValue(500)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+    tc:RegisterEffect(e1)
   end
 end
