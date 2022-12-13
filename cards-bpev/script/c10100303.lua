8375608fc2760a1276e73ef61744c5413ca87e67
utso <cccygodev@gmail.com>
2022-12-13 23:19:58 +0100

    EP Flow: Trigger even if sent to GY by something else than card effect

--- cards-bpev/script/c10100303.lua
+++ cards-bpev/script/c10100303.lua
@@ -29,7 +29,7 @@ end
 s.material_setcode={0xb0b}
 function s.condition1(e,tp,eg,ep,ev,re,r,rp)
   local c=e:GetHandler()
-  return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_FUSION) and c:IsReason(REASON_EFFECT)
+  return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_FUSION)
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
