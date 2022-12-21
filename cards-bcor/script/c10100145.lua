d47e8fe66cc132add4f18456467a89fbffa95ff6
utso <cccygodev@gmail.com>
2022-12-21 21:03:21 +0100

    Jala: Move extra normal summon to damage step

--- cards-bcor/script/c10100145.lua
+++ cards-bcor/script/c10100145.lua
@@ -8,21 +8,28 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetCondition(s.condition1)
 	e1:SetTarget(s.target1)
 	e1:SetCondition(s.condition1)
 	e1:SetValue(s.value1)
 	c:RegisterEffect(e1)	
 	--Extra Summon
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
-	e2:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+  e2:SetDescription(aux.Stringid(id,0))
+  e2:SetCategory(CATEGORY_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_START)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTarget(s.target2)
+	e2:SetCondition(s.condition2)
+  e2:SetTarget(s.target2)
+  e2:SetOperation(s.operation2)
 	c:RegisterEffect(e2)
 end
 function s.filter1(c)
 	return c:IsFaceup() and c:IsSetCard(0xb01)
+end
+function s.condition1(e)
+	return Duel.IsExistingMatchingCard(Card.IsFaceup,e:GetHandlerPlayer(),0,LOCATION_MZONE,1,nil)
 end
 function s.target1(e,c)
   return c:IsLevelBelow(4) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_WARRIOR)
@@ -30,9 +37,20 @@ end
 function s.value1(e,c)
 	return Duel.GetMatchingGroupCount(s.filter1,e:GetHandlerPlayer(),LOCATION_MZONE,0,nil)*400
 end
-function s.condition1(e)
-	return Duel.IsExistingMatchingCard(Card.IsFaceup,e:GetHandlerPlayer(),0,LOCATION_MZONE,1,nil)
+function s.filter2(c)
+  return c:IsRace(RACE_WARRIOR) and c:IsSummonable(true,nil)
 end
-function s.target2(e,c)
-  return c:IsSetCard(0xb01) or (c:IsAttribute(ATTRIBUTE_FIRE) and c:IsSetCard(0xb02))
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+  return Duel.GetAttacker()==e:GetHandler()
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_HAND,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 then
+		Duel.Summon(tp,g:GetFirst(),true,nil)
+	end
 end
