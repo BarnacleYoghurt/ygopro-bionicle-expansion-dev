7c6f69ad59d14eccc542332357536b3a26018641
utso <cccygodev@gmail.com>
2023-04-11 00:19:45 +0200

    Za-Kal: Change bugged destruction replacement to destruction negate

--- cards-bpev/script/c10100338.lua
+++ cards-bpev/script/c10100338.lua
@@ -18,29 +18,42 @@ function s.initial_effect(c)
   local e2a,e2b=bpev.krana_kal_debuff(c)
   c:RegisterEffect(e2a)
   c:RegisterEffect(e2b)
-  --Destroy replace
-  --Warning: Detaching Za-Kal for this will cause a crash!
+  --Negate destruction
   local e3=Effect.CreateEffect(c)
-  e3:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_CONTINUOUS)
-  e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-  e3:SetCode(EFFECT_DESTROY_REPLACE)
+  e3:SetDescription(aux.Stringid(id,0))
+  e3:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+  e3:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_QUICK_O)
+  e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+  e3:SetCode(EVENT_CHAINING)
   e3:SetCondition(s.condition3)
+  e3:SetCost(aux.dxmcostgen(1,1,nil))
   e3:SetTarget(s.target3)
+  e3:SetOperation(s.operation3)
+  e3:SetCountLimit(1)
   c:RegisterEffect(e3)
 end
 function s.filter0(c)
   return c:IsSetCard(0xb08) or c:IsSetCard(0xb09)
 end
+function s.filter3(c,tp)
+  return c:IsFaceup() and c:IsSetCard(0xb08) and c:IsOnField() and c:IsControler(tp)
+end
 function s.condition3(e,tp,eg,ep,ev,re,r,rp)
-  return e:GetHandler():IsSetCard(0xb08)
+  if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) or not Duel.IsChainNegatable(ev) then return false end
+	if re:IsHasCategory(CATEGORY_NEGATE)
+		and Duel.GetChainInfo(ev-1,CHAININFO_TRIGGERING_EFFECT):IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	return ex and tg~=nil and tc>(#tg-tg:FilterCount(s.filter3,nil,tp)) --Can activate if number of cards to destroy is greater than number of non-relevant options
 end
 function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return not c:IsReason(REASON_REPLACE) and c:CheckRemoveOverlayCard(tp,1,REASON_EFFECT) end
-	if Duel.SelectEffectYesNo(tp,c,96) then
-		c:RemoveOverlayCard(tp,1,1,REASON_EFFECT)
-		return true
-	else 
-    return false 
-  end
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
+  if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
 end
