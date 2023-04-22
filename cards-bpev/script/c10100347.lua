d44f85f11aba88ded10c74198a17d741682c1f3d
utso <cccygodev@gmail.com>
2023-04-22 19:20:56 +0200

    Bohrok-Kal Strategy: Use SelectEffect function and correct strings, remove outdated checks

--- cards-bpev/script/c10100347.lua
+++ cards-bpev/script/c10100347.lua
@@ -27,8 +27,8 @@ function s.filter1(c,e,tp)
   return c:IsSetCard(0xb08) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-  if e:GetHandler():IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-    if Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+  if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+    if Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
       Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
       local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_HAND,0,1,1,nil,e,tp)
       if #g>0 then
@@ -54,48 +54,39 @@ function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
   if chkc then return e:GetLabel()~=1 and chkc:IsType(TYPE_SPELL+TYPE_TRAP) and chkc~=c end
   local b1=Duel.IsExistingTarget(Card.IsType,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c,TYPE_SPELL+TYPE_TRAP)
   local b2=Duel.IsExistingMatchingCard(s.filter2b,tp,LOCATION_DECK,0,1,nil)
+  local bkal=Duel.IsExistingMatchingCard(s.filter2c,tp,LOCATION_MZONE,0,1,nil)
   if chk==0 then return b1 or b2 end
-  local op=0
-  if b1 and b2 then
-    if Duel.IsExistingMatchingCard(s.filter2c,tp,LOCATION_MZONE,0,1,nil) then
-      op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2),aux.Stringid(id,3))
-    else
-      op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
-    end
-  elseif b1 then
-    op=Duel.SelectOption(tp,aux.Stringid(id,1))
-  else
-    op=Duel.SelectOption(tp,aux.Stringid(id,2))+1
-  end
+  local op=Duel.SelectEffect(tp,
+    {b1,aux.Stringid(id,2)},
+    {b2,aux.Stringid(id,3)},
+    {b1 and b2 and bkal,aux.Stringid(id,4)})
   e:SetLabel(op)
   
-  if op~=1 then
+  if op~=2 then
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
     local g=Duel.SelectTarget(tp,Card.IsType,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,c,TYPE_SPELL+TYPE_TRAP)
     Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
   end
-  if op~=0 then
+  if op~=1 then
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
   end
-  e:SetCategory((op~=1 and CATEGORY_DESTROY or 0)+(op~=0 and CATEGORY_SEARCH+CATEGORY_TOHAND or 0))
-  e:SetProperty(EFFECT_FLAG_DELAY+(op~=1 and EFFECT_FLAG_CARD_TARGET or 0))
+  e:SetCategory((op~=2 and CATEGORY_DESTROY or 0)+(op~=1 and CATEGORY_SEARCH+CATEGORY_TOHAND or 0))
+  e:SetProperty(EFFECT_FLAG_DELAY+(op~=2 and EFFECT_FLAG_CARD_TARGET or 0))
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-  if e:GetHandler():IsRelateToEffect(e) then
-    local op=e:GetLabel()
-    if op~=1 then
-      local tc=Duel.GetFirstTarget()
-      if tc:IsRelateToEffect(e) then
-        Duel.Destroy(tc,REASON_EFFECT)
-      end
+  local op=e:GetLabel()
+  if op~=2 then
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+      Duel.Destroy(tc,REASON_EFFECT)
     end
-    if op~=0 then
-      if op==2 then Duel.BreakEffect() end --in sequence
-      local g=Duel.SelectMatchingCard(tp,s.filter2b,tp,LOCATION_DECK,0,1,1,nil)
-      if #g>0 then
-        Duel.SendtoHand(g,tp,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,g)
-      end
+  end
+  if op~=1 then
+    if op==3 then Duel.BreakEffect() end --in sequence
+    local g=Duel.SelectMatchingCard(tp,s.filter2b,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+      Duel.SendtoHand(g,tp,REASON_EFFECT)
+      Duel.ConfirmCards(1-tp,g)
     end
   end
 end
