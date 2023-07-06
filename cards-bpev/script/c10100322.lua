13cbcf5908e617a417a185c52c2098c7c310aa80
utso <cccygodev@gmail.com>
2023-07-06 19:01:00 +0200

    Implement Frigid Serenity

---
+++ cards-bpev/script/c10100322.lua
@@ -0,0 +1,60 @@
+if not bpev then
+	Duel.LoadScript("../util-bpev.lua")
+end
+--Nuva Symbol of Frigid Serenity
+local s,id=GetID()
+function s.initial_effect(c)
+    --Activate
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_ACTIVATE)
+    e0:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e0)
+    --Search EP or Kopaka
+    local e1=bpev.nuva_symbol_search(c,10100005,aux.Stringid(id,3))
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCountLimit(1,id)
+    c:RegisterEffect(e1)
+    --Banish
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_REMOVE)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+    e2:SetCode(EVENT_LEAVE_FIELD)
+    e2:SetCondition(s.condition2)
+    e2:SetTarget(s.target2)
+    e2:SetOperation(s.operation2)
+    e2:SetCountLimit(1,{id,1})
+    c:RegisterEffect(e2)
+    --Leave field
+    local e3=bpev.nuva_symbol_punish(c,s.operation3)
+    e3:SetDescription(aux.Stringid(id,2))
+    c:RegisterEffect(e3)
+end
+function s.filter2(c,tp)
+    return c:IsPreviousSetCard(0xb0c) and c:IsType(TYPE_FUSION) and c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousControler(tp)
+       and c:GetReasonPlayer()==1-tp
+end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(s.filter2,1,nil,tp)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_ONFIELD)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,1,nil)
+    if #g>0 then
+        Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+    end
+end
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,0,LOCATION_REMOVED,1,nil) and Duel.SelectYesNo(1-tp,aux.Stringid(id,4)) then
+        local g=Duel.SelectMatchingCard(1-tp,Card.IsAbleToHand,tp,0,LOCATION_REMOVED,1,1,nil)
+        if #g>0 then
+            Duel.SendtoHand(g,nil,REASON_EFFECT)
+        end
+    end
+end
