0764227110f18da945e47c4b3b8c8870aa9d5c04
utso <cccygodev@gmail.com>
2023-07-08 17:43:20 +0200

    Nuva Emergence: Do not allow materials from field

--- cards-bpev/tests/c10100330.lua
+++ cards-bpev/tests/c10100330.lua
@@ -1,6 +1,6 @@
 --Nuva Emergence
 Debug.SetAIName("Unit Test")
-Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN,4)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN,5)
 Debug.SetPlayerInfo(0,8000,0,0)
 Debug.SetPlayerInfo(1,8000,0,0)
 Debug.AddCard(10100330,0,0,LOCATION_HAND,2,0)
