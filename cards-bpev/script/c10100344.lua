15c3f042c1797ca09e8a128279b0cdb2fcc8a0e2
utso <cccygodev@gmail.com>
2023-11-09 17:46:16 +0100

    Implement Bo-Kal

---
+++ cards-bpev/script/c10100344.lua
@@ -0,0 +1,55 @@
+if not bpev then
+	Duel.LoadScript("util-bpev.lua")
+end
+--Krana Bo-Kal, Visionary
+local s,id=GetID()
+function s.initial_effect(c)
+    --Link Summon
+    Link.AddProcedure(c,s.filter0,1,1)
+    c:EnableReviveLimit()
+    --No Link Material
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
+    --Special Summon
+    local e2=bpev.krana_kal_ssummon(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    c:RegisterEffect(e2)
+    --Look
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_IGNITION)
+    e3:SetCondition(s.condition3)
+    e3:SetTarget(s.target3)
+    e3:SetOperation(s.operation3)
+    e3:SetCountLimit(1)
+    c:RegisterEffect(e3)
+end
+function s.condition3(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsSetCard(0xb08)
+end
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFacedown,tp,0,LOCATION_ONFIELD,1,nil) or Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0 end
+end
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
+    --Set Cards
+    local g1=Duel.GetMatchingGroup(Card.IsFacedown,tp,0,LOCATION_ONFIELD,nil)
+    if #g1>0 then
+        Duel.ConfirmCards(tp,g1)
+    end
+    --Hand
+    local g2=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+    local max=math.min(#g2, Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsSetCard,0xb08),tp,LOCATION_MZONE,0,nil))
+    if max>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+        local ct=max
+        if max>1 then
+            ct=Duel.AnnounceNumber(tp,1,max)
+        end
+        g2=g2:RandomSelect(tp,ct)
+	    Duel.ConfirmCards(tp,g2)
+    end
+end
+
