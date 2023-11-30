bc6967e579f0856c5d66d6d471facbfa572eef7a
utso <cccygodev@gmail.com>
2023-11-30 11:15:32 +0100

    Pahrak-Kal: Make effect target, banish face-up

--- cards-bpev/script/c10100334.lua
+++ cards-bpev/script/c10100334.lua
@@ -21,6 +21,7 @@ function s.initial_effect(c)
     e3:SetDescription(aux.Stringid(id,1))
     e3:SetCategory(CATEGORY_REMOVE)
     e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e3:SetCode(EVENT_BATTLE_START)
     e3:SetCost(aux.dxmcostgen(1,1))
     e3:SetTarget(s.target3)
@@ -29,15 +30,16 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_MZONE)
+    if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,1,nil) end
+    local tc=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+    local tg=tc+tc:GetColumnGroup():Filter(Card.IsAbleToRemove,nil):Filter(Card.IsControler,nil,1-tp)
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,tg,#tg,0,0)
 end
 function s.operation3(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_MZONE,1,1,nil)
-    if #g>0 then
-        g=g:GetFirst()+g:GetFirst():GetColumnGroup()
-        g=g:Filter(Card.IsControler,nil,1-tp)
-        if Duel.Remove(g,POS_FACEDOWN,REASON_EFFECT)==1 then
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        local tg=tc+tc:GetColumnGroup():Filter(Card.IsAbleToRemove,nil):Filter(Card.IsControler,nil,1-tp)
+        if Duel.Remove(tg,POS_FACEUP,REASON_EFFECT)==1 then
             Duel.BreakEffect()
             Duel.Damage(1-tp,1200,REASON_EFFECT)
         end
