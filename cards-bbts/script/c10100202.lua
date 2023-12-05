2dc6b68c1fb8e08f3e519546c4700464e7b4be0e
utso <cccygodev@gmail.com>
2023-12-05 20:45:31 +0100

    Gahlok: Spell effect now only negates, banish effect only lasts until End Phase

--- cards-bbts/script/c10100202.lua
+++ cards-bbts/script/c10100202.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
   c:RegisterEffect(e1)
 	--be unpredictable
 	local e2=Effect.CreateEffect(c)
-  e2:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTarget(s.target2)
@@ -43,8 +43,8 @@ function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 		local tg=Duel.SelectTarget(tp,Card.IsDestructable,tp,0,LOCATION_ONFIELD,1,1,nil)
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,tg,1,0,0)
 	elseif top:IsType(TYPE_SPELL) then
-		local tg=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
-		Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE+CATEGORY_DISABLE,tg,1,0,0)
+		local tg=Duel.GetMatchingGroup(Card.IsNegatableMonster,tp,0,LOCATION_MZONE,nil)
+		Duel.SetOperationInfo(0,CATEGORY_DISABLE,tg,1,0,0)
 	elseif top:IsType(TYPE_TRAP) then
 		local tg=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_HAND,nil)
 		Duel.SetOperationInfo(0,CATEGORY_REMOVE,tg,1,0,0)
@@ -60,43 +60,61 @@ function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 		tc=g:GetNext()
 	end
   
-  local c=e:GetHandler()
+	local c=e:GetHandler()
 	if top:IsType(TYPE_MONSTER) then
 		local tc=Duel.GetFirstTarget()
 		if tc and tc:IsRelateToEffect(e) then 
 			Duel.Destroy(tc,REASON_EFFECT)
 		end
 	elseif top:IsType(TYPE_SPELL) then
-		local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,Card.IsNegatableMonster,tp,0,LOCATION_MZONE,1,1,nil)
 		if g:GetCount()>0 then
 			local tc=g:GetFirst()
-      if tc:IsType(TYPE_EFFECT) then
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_DISABLE)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-        tc:RegisterEffect(e1)
-        local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetCode(EFFECT_DISABLE_EFFECT)
-        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-        tc:RegisterEffect(e2)
-      end
-			local e3=Effect.CreateEffect(c)
-			e3:SetType(EFFECT_TYPE_SINGLE)
-			e3:SetCode(EFFECT_SET_ATTACK_FINAL)
-      e3:SetValue(0)
-			e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			tc:RegisterEffect(e3)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e2)
 		end
 	elseif top:IsType(TYPE_TRAP) then
 		local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_HAND,nil)
 		if g:GetCount()>0 then
-			local sg=g:RandomSelect(tp,1)
-			Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
+			local sc=g:RandomSelect(tp,1):GetFirst()
+			Duel.Remove(sc,POS_FACEUP,REASON_EFFECT)
+
+			sc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+			local e1=Effect.CreateEffect(c)
+			e1:SetDescription(aux.Stringid(id,3))
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_PHASE+PHASE_END)
+			e1:SetCondition(s.condition2_1)
+			e1:SetOperation(s.operation2_1)
+			e1:SetLabelObject(sc)
+			e1:SetReset(RESET_PHASE+PHASE_END)
+			e1:SetCountLimit(1)
+			Duel.RegisterEffect(e1,tp)
 		end
 	end
-  --To deck
-  local e1=bbts.bohrok_shuffledelayed(c)
-  Duel.RegisterEffect(e1,tp)
+	--To deck
+	local e1=bbts.bohrok_shuffledelayed(c)
+	e1:SetDescription(aux.Stringid(id,2))
+	Duel.RegisterEffect(e1,tp)
+end
+function s.condition2_1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffect(id)==0 then
+		e:Reset()
+		return false
+	else
+		return true
+	end
+end
+function s.operation2_1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.SendtoHand(tc,1-tp,REASON_EFFECT)
 end
