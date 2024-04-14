0abfc99846eb5b6a63b8022db2ceb8f09e2a9152
utso <cccygodev@gmail.com>
2024-04-14 23:14:12 +0200

    Wairuha Nuva: No looking at deck, instead you have to guess for a draw, like in old times

--- cards-bpev/script/c10100326.lua
+++ cards-bpev/script/c10100326.lua
@@ -7,69 +7,65 @@ function s.initial_effect(c)
     c:EnableReviveLimit()
     --Xyz Material
     Xyz.AddProcedure(c,nil,8,3)
-    --Look
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetProperty(EFFECT_FLAG_DELAY)
-    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetTarget(s.target1)
-    e1:SetOperation(s.operation1)
-    e1:SetCountLimit(1,id)
-    c:RegisterEffect(e1)
     --Negate
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_REMOVE)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e2:SetRange(LOCATION_MZONE)
-    e2:SetCode(EVENT_CHAINING)
-	e2:SetCondition(s.condition2)
-	e2:SetCost(aux.dxmcostgen(1,1))
-	e2:SetTarget(s.target2)
-	e2:SetOperation(s.operation2)
-	e2:SetCountLimit(1,{id,1})
-	c:RegisterEffect(e2)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_DISABLE+CATEGORY_REMOVE+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.condition1)
+	e1:SetCost(aux.dxmcostgen(1,1))
+	e1:SetTarget(s.target1)
+	e1:SetOperation(s.operation1)
+	e1:SetCountLimit(1,id)
+	c:RegisterEffect(e1)
     --Search
-    local e3=bpev.toa_nuva_kaita_search(c)
-    e3:SetDescription(aux.Stringid(id,2))
-    e3:SetCountLimit(1,{id,2})
-    c:RegisterEffect(e3)
+    local e2=bpev.toa_nuva_kaita_search(c)
+    e2:SetDescription(aux.Stringid(id,2))
+    e2:SetCountLimit(1,id)
+    c:RegisterEffect(e2)
 end
-function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 or Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)>0 end
-end
-function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-    s.operation1_look(tp,tp)
-    s.operation1_look(tp,1-tp)
-end
-function s.operation1_look(tp,p)
-    local gc=math.min(5,Duel.GetFieldGroupCount(p,LOCATION_DECK,0))
-	if gc>0 then
-		local ac=gc==1 and gc or Duel.AnnounceNumberRange(tp,1,gc)
-		Duel.ConfirmCards(tp,Duel.GetDecktopGroup(p,ac))
-	end
-end
-function s.filter2(c)
+function s.filter1(c)
     return c:IsLevel(8) and c:IsSetCard(0xb02)
 end
-function s.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetOverlayGroup():IsExists(s.filter2,1,nil)
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetOverlayGroup():IsExists(s.filter1,1,nil)
 end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
     local rc=re:GetHandler()
     local relation=rc:IsRelateToEffect(re)
     if chk==0 then return rc:IsAbleToRemove(tp) or (not relation and Duel.IsPlayerCanRemove(tp)) end
     Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-    if relation then
-        Duel.SetOperationInfo(0,CATEGORY_REMOVE,rc,1,rc:GetControler(),rc:GetLocation())
+    if rc:IsRelateToEffect(re) then
+        Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,rc,1,rc:GetControler(),rc:GetLocation())
     else
-        Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,0,rc:GetPreviousLocation())
+        Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,nil,0,0,rc:GetPreviousLocation())
     end
+    Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
+    Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_DECK)
+    Duel.SetPossibleOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
-function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
-        Duel.Remove(eg,POS_FACEUP,REASON_EFFECT)
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+    local rc=re:GetHandler()
+    local odc=Duel.GetMatchingGroupCount(Card.IsAbleToRemove,1-tp,LOCATION_DECK,0,nil,tp)
+    local pdc=Duel.GetMatchingGroupCount(Card.IsAbleToRemove,tp,LOCATION_DECK,0,nil,tp)
+    if Duel.NegateActivation(ev) and rc:IsRelateToEffect(re) and rc:IsAbleToRemove(tp) and odc+pdc>0 then
+        local op=Duel.SelectEffect(tp,{odc>0,aux.Stringid(id,2)},{pdc>0,aux.Stringid(id,3)},{true,aux.Stringid(id,4)})
+        local rg
+        if op==1 then
+            rg=eg+Duel.GetDecktopGroup(1-tp,1)
+        elseif op==2 then
+            rg=eg+Duel.GetDecktopGroup(tp,1)
+        end
+        if rg then
+            Duel.DisableShuffleCheck()
+            if Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)==2
+                and rg:GetClassCount(function (c) return c:GetType()&(TYPE_MONSTER|TYPE_SPELL|TYPE_TRAP) end)==2 then
+                Duel.BreakEffect()
+                Duel.Draw(tp,1,REASON_EFFECT)
+            end
+        end
     end
 end
