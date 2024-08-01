5e74a3226abf532e26f8a8e4c602ad28cb58b8fa
utso <cccygodev@gmail.com>
2024-08-01 14:08:25 +0200

    BCOR: Rahi Update: Manas

--- cards-bcor/script/c10100106.lua
+++ cards-bcor/script/c10100106.lua
@@ -1,62 +1,88 @@
 --Manas, Monstrous Crab Rahi
 local s,id=GetID()
 function s.initial_effect(c)
-	--No Targeting or Destruction
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e1:SetValue(aux.tgoval)
-	c:RegisterEffect(e1)
-	local e1a=e1:Clone()
-	e1a:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e1a:SetValue(s.value1a)
+	--Gain ATK/DEF
+	local e1a=Effect.CreateEffect(c)
+	e1a:SetType(EFFECT_TYPE_SINGLE)
+	e1a:SetCode(EFFECT_UPDATE_ATTACK)
+	e1a:SetValue(s.value1)
 	c:RegisterEffect(e1a)
-	--Gain ATK
+	local e1b=e1a:Clone()
+	e1b:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e1b)
+	--Add to hand
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_ATKCHANGE)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_CHAIN_SOLVED)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(s.condition2)
+	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND+LOCATION_MZONE)
+	e2:SetCost(s.cost2)
+	e2:SetTarget(s.target2)
 	e2:SetOperation(s.operation2)
-	e2:SetCountLimit(1)
+	e2:SetCountLimit(1,id)
 	c:RegisterEffect(e2)
-	--To Hand
+	--Special Summon
 	local e3=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_TOHAND)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e3:SetRange(LOCATION_MZONE)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetCondition(s.condition3)
 	e3:SetTarget(s.target3)
 	e3:SetOperation(s.operation3)
+	e3:SetCountLimit(1,{id,1})
 	c:RegisterEffect(e3)
 end
-function s.value1a(e,re,rp)
-	return rp~=e:GetHandlerPlayer()
+function s.value1(e)
+	return 400*Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_SZONE,0)
 end
-function s.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and (re:IsActiveType(TYPE_SPELL) or re:IsActiveType(TYPE_TRAP)) 
+function s.filter2(c)
+	return c:IsSetCard(0xb06) and c:IsType(TYPE_NORMAL) and c:IsAbleToHand()
+end
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsReleasable() end
+	Duel.Release(c,REASON_COST)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(800)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
-	e:GetHandler():RegisterEffect(e1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.filter3(c)
+	return c:IsSetCard(0xb06) and c:IsOriginalType(TYPE_NORMAL) and c:IsMonsterCard()
 end
 function s.condition3(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp and bit.band(e:GetHandler():GetSummonType(),SUMMON_TYPE_SPECIAL)==SUMMON_TYPE_SPECIAL
+	return Duel.IsTurnPlayer(1-tp) and Duel.IsExistingMatchingCard(s.filter3,tp,LOCATION_ONFIELD,0,1,nil)
 end
 function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToHand() end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function s.operation3(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsRelateToEffect(e) then
-		Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3101)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_IMMUNE_EFFECT)
+		e1:SetValue(s.value3_1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1,true)
 	end
+	Duel.SpecialSummonComplete()
+end
+function s.value3_1(e,te)
+	return te:IsMonsterEffect() and te:GetOwner()~=e:GetOwner()
 end
