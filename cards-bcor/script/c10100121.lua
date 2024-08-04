00b3432fa51165f7471fb5fa2e9de85baae1839b
utso <cccygodev@gmail.com>
2024-08-05 00:17:26 +0200

    BCOR: Rahi Update: Hikaki

--- cards-bcor/script/c10100121.lua
+++ cards-bcor/script/c10100121.lua
@@ -1,120 +1,70 @@
 --Hikaki, Dragon Rahi
 local s,id=GetID()
 function s.initial_effect(c)
-	--pendulum summon
-	Pendulum.AddProcedure(c)
-	--Special Summon
+	--Xyz Summon
+	Xyz.AddProcedure(c,nil,3,2,nil,nil,99)
+	c:EnableReviveLimit()
+	--To hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_PZONE)
-	e1:SetCost(s.condition1)
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.condition1)
+	e1:SetCost(aux.dxmcostgen(1,1))
 	e1:SetTarget(s.target1)
 	e1:SetOperation(s.operation1)
-	c:RegisterEffect(e1)	
-	--Search
+	e1:SetCountLimit(1,id)
+	c:RegisterEffect(e1)
+	--Destroy
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetCategory(CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetRange(LOCATION_PZONE)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetCondition(s.condition2)
+	e2:SetCost(aux.dxmcostgen(1,1))
 	e2:SetTarget(s.target2)
 	e2:SetOperation(s.operation2)
-	e2:SetCountLimit(1,id)
+	e2:SetCountLimit(1,{id,1})
 	c:RegisterEffect(e2)
-	--Negate Trap
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_BE_MATERIAL)
-	e3:SetCondition(s.condition3)
-	e3:SetOperation(s.operation3)
-	c:RegisterEffect(e3)
 end
-function s.condition1(e,tp,eg,ep,ev,re,r,rp,chk)
-	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+function s.filter1(c)
+	return c:IsSetCard(0xb06) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,LOCATION_SZONE)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function s.filter2(c)
-	return c:IsSetCard(0xb06) and c:IsType(TYPE_TUNER) and c:IsAbleToHand()
+function s.filter2(c,tp)
+	return c:IsSetCard(0xb06) and c:IsMonster() and c:IsControler(tp)
 end
 function s.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return eg and eg:GetCount()>0 and eg:IsExists(Card.IsSetCard,1,nil,0xb06)
+	return eg:IsExists(s.filter2,1,nil,tp)
 end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil) end
-	local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_DECK,0,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsFaceup() and chkc:IsLocation(LOCATION_ONFIELD) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsRelateToEffect(e) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil)
-		if g:GetCount()>0 then
-			Duel.SendtoHand(g,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,g)	
-		end
-	end
-end
-function s.condition3(e,tp,eg,ep,ev,re,r,rp)
-	return r==REASON_SYNCHRO
-end
-function s.operation3(e,tp,eg,ep,ev,re,r,rp)
-	local c=re:GetHandler()
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,2))
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e1:SetCode(EVENT_CHAINING)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	e1:SetCondition(s.condition3_1)
-	e1:SetCost(s.cost3_1)
-	e1:SetTarget(s.target3_1)
-	e1:SetOperation(s.operation3_1)
-	c:RegisterEffect(e1)
-	if not c:IsType(TYPE_EFFECT) then
-		local e2=Effect.CreateEffect(e:GetHandler())
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_ADD_TYPE)
-		e2:SetValue(TYPE_EFFECT)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		c:RegisterEffect(e2,true)
-	end
-end
-function s.filter3_1(c)
-	return c:IsSetCard(0xb06) and c:IsAbleToDeck() and c:IsType(TYPE_PENDULUM) and c:IsFaceup()
-end
-function s.condition3_1(e,tp,eg,ep,ev,re,r,rp)
-	return re:IsActiveType(TYPE_TRAP) and re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev)
-end
-function s.cost3_1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter3_1,tp,LOCATION_EXTRA,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectMatchingCard(tp,s.filter3_1,tp,LOCATION_EXTRA,0,1,1,nil)
-	Duel.SendtoDeck(g,nil,2,REASON_COST)
-	Duel.ShuffleDeck(tp)
-end
-function s.target3_1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
-	end
-end
-function s.operation3_1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.NegateActivation(ev) then
-		Duel.Destroy(eg,REASON_EFFECT)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
 	end
 end
