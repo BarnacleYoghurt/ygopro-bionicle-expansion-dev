569d98b36d170f26f7f42de968be59424477df59
utso <cccygodev@gmail.com>
2024-08-11 20:02:13 +0200

    Implement Subterranean Worm

---
+++ cards-bpev/script/c10100353.lua
@@ -0,0 +1,50 @@
+--Subterranean Worm Rahi
+local s,id=GetID()
+function s.initial_effect(c)
+    --synchro summon
+    Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xb06),1,1,Synchro.NonTuner(nil),1,99)
+    c:EnableReviveLimit()
+    --Piercing
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_PIERCE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTargetRange(LOCATION_MZONE,0)
+    e1:SetTarget(function(_,c2) return c2:IsSetCard(0xb06) end)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
+    --Special Summon
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetCondition(s.condition2)
+    e2:SetTarget(s.target2)
+    e2:SetOperation(s.operation2)
+    e2:SetCountLimit(1,id)
+    c:RegisterEffect(e2)
+end
+function s.filter2(c,e,tp)
+    return c:IsSetCard(0xb06) and c:IsType(TYPE_TUNER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return s.filter2(chkc,e,tp) and chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) end
+    if chk==0 then
+        return Duel.IsExistingTarget(s.filter2,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+            and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+    end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectTarget(tp,s.filter2,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+        Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+    end
+end
