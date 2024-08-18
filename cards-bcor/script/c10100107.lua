9ad84110e776cf07bb30ff676c631a63e4f0711d
utso <cccygodev@gmail.com>
2024-08-18 14:47:29 +0200

    Fikou: now regular HOPT, but does not banish as cost

--- cards-bcor/script/c10100107.lua
+++ cards-bcor/script/c10100107.lua
@@ -4,14 +4,13 @@ function s.initial_effect(c)
 	--Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_LVCHANGE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_LVCHANGE+CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
-	e1:SetCost(s.cost1)
 	e1:SetTarget(s.target1)
 	e1:SetOperation(s.operation1)
-	e1:SetCountLimit(2,id)
+	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
 end
 function s.filter1a(c)
@@ -25,20 +24,23 @@ function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
 	if chk==0 then
-		return Duel.IsExistingTarget(s.filter1a,tp,LOCATION_MZONE,0,1,nil)
+		return Duel.IsExistingTarget(s.filter1a,tp,LOCATION_MZONE,0,1,nil) and c:IsAbleToRemove()
 			and Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_HAND+LOCATION_DECK,0,1,e:GetHandler(),e,tp)
 			and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-	end	
+	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	local g=Duel.SelectTarget(tp,s.filter1a,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_LVCHANGE,g,1,0,0)	
+	Duel.SetOperationInfo(0,CATEGORY_LVCHANGE,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,c,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local lvc=Duel.GetFirstTarget()
-	if lvc:IsRelateToEffect(e) and s.filter1a(lvc) and lvc:UpdateLevel(-1,RESET_EVENT+RESETS_STANDARD,e:GetHandler())==-1 then
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+	if lvc:IsRelateToEffect(e) and s.filter1a(lvc) and lvc:UpdateLevel(-1,RESET_EVENT+RESETS_STANDARD,c)==-1 then
+		if Duel.Remove(c,POS_FACEUP,REASON_EFFECT)>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 			local g=Duel.SelectMatchingCard(tp,s.filter1b,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
 			if #g>0 then
