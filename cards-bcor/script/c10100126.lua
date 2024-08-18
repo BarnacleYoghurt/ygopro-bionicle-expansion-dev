4ae77b97521e85be125c5add56ac7d9d5d6346ae
utso <cccygodev@gmail.com>
2024-08-18 16:02:07 +0200

    Ruki: always banish 2, but search on summon

--- cards-bcor/script/c10100126.lua
+++ cards-bcor/script/c10100126.lua
@@ -1,39 +1,75 @@
 --Ruki, Fish Rahi
 local s,id=GetID()
 function s.initial_effect(c)
-	--Destroy monster
+	--To hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(s.cost1)
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND+CATEGORY_TOEXTRA)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetTarget(s.target1)
 	e1:SetOperation(s.operation1)
+	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
+	local e1b=e1:Clone()
+	e1b:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e1b)
+	--Destroy monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_HAND+LOCATION_MZONE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCost(s.cost2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.operation2)
+	c:RegisterEffect(e2)
 end
-function s.filter1(c)
-	return (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE)) and c:IsType(TYPE_MONSTER) and c:IsSetCard(0xb06) and c:IsAbleToRemoveAsCost()
+function s.filter1a(c)
+	return c:IsRace(RACE_FISH|RACE_SEASERPENT|RACE_AQUA) and c:IsSetCard(0xb06) and c:IsType(TYPE_PENDULUM) and c:IsAbleToHand()
 end
-function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then
-		return c:IsAbleToRemoveAsCost()
-			and (c:IsLocation(LOCATION_MZONE) or Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,nil))
-	end
-	local g=Group.FromCards(c)
-	if not c:IsLocation(LOCATION_MZONE) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-		g:Merge(Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,1,nil))
-	end
-	Duel.Remove(g,POS_FACEUP,REASON_COST)
+function s.filter1b(c)
+	return c:IsType(TYPE_PENDULUM) and c:IsAbleToExtra()
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1a,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOEXTRA,nil,1,tp,LOCATION_HAND)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp,chk)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g1=Duel.SelectMatchingCard(tp,s.filter1a,tp,LOCATION_DECK,0,1,1,nil)
+	if #g1>0 and Duel.SendtoHand(g1,nil,REASON_EFFECT)>0 and g1:FilterCount(Card.IsLocation,nil,LOCATION_HAND)>0 then
+		Duel.ConfirmCards(1-tp,g1)
+		if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			local g2=Duel.SelectMatchingCard(tp,s.filter1b,tp,LOCATION_HAND,0,1,1,nil)
+			if #g2>0 then
+				Duel.BreakEffect()
+				Duel.SendtoExtraP(g2,nil,REASON_EFFECT)
+			end
+		end
+	end
+end
+function s.filter2(c)
+	return (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE)) and c:IsType(TYPE_MONSTER) and c:IsSetCard(0xb06) and c:IsAbleToRemoveAsCost()
+end
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		return c:IsAbleToRemoveAsCost() and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,nil)
+	end
+	local g=Group.FromCards(c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	g:Merge(Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,1,nil))
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,1-tp,LOCATION_MZONE)
 end
-function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
 	if #g>0 then
