224ac3edc5e2f96d75ff8eb0b0585aab0dc5bceb
utso <cccygodev@gmail.com>
2024-08-18 15:26:05 +0200

    Manas: only search from hand, only buff for face-up S/T, return to hand in End Phase

--- cards-bcor/script/c10100106.lua
+++ cards-bcor/script/c10100106.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
 	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_HAND+LOCATION_MZONE)
+	e2:SetRange(LOCATION_HAND)
 	e2:SetCost(s.cost2)
 	e2:SetTarget(s.target2)
 	e2:SetOperation(s.operation2)
@@ -35,15 +35,15 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function s.value1(e)
-	return 400*Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_SZONE,0)
+	return 400*Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsSpellTrap),e:GetHandlerPlayer(),LOCATION_ONFIELD,0,nil)
 end
 function s.filter2(c)
 	return c:IsSetCard(0xb06) and c:IsType(TYPE_NORMAL) and c:IsAbleToHand()
 end
 function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return c:IsReleasable() end
-	Duel.Release(c,REASON_COST)
+	if chk==0 then return c:IsDiscardable() end
+	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil) end
@@ -71,6 +71,8 @@ end
 function s.operation3(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+		--Unaffected by other monsters' effects
 		local e1=Effect.CreateEffect(c)
 		e1:SetDescription(3101)
 		e1:SetType(EFFECT_TYPE_SINGLE)
@@ -80,9 +82,31 @@ function s.operation3(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(s.value3_1)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e1,true)
+		--Return to hand during End Phase
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e2:SetRange(LOCATION_MZONE)
+		e2:SetCode(EVENT_PHASE+PHASE_END)
+		e2:SetCondition(s.condition3_2)
+		e2:SetOperation(s.operation3_2)
+		e2:SetLabelObject(c)
+		e2:SetCountLimit(1)
+		Duel.RegisterEffect(e2,tp)
 	end
 	Duel.SpecialSummonComplete()
 end
 function s.value3_1(e,te)
 	return te:IsMonsterEffect() and te:GetOwner()~=e:GetOwner()
+end
+function s.condition3_2(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabelObject():GetFlagEffect(id)>0 then
+		return true
+	else
+		e:Reset()
+		return false
+	end
+end
+function s.operation3_2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SendtoHand(e:GetLabelObject(),nil,REASON_EFFECT)
 end
