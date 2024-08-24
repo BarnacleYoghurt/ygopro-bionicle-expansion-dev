38a73736fd3ea2f1c93610c4ca4c7b5f911124d6
utso <cccygodev@gmail.com>
2024-08-24 18:58:39 +0200

    Husi: Implement alternate Pendulum Effect

--- cards-bcor/script/c10100123.lua
+++ cards-bcor/script/c10100123.lua
@@ -6,14 +6,14 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--pendulum summon
 	Pendulum.AddProcedure(c)
-	--Special Summon
+	--Pend swap
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_TOEXTRA)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetCode(EVENT_DESTROYED)
+	e1:SetCode(EVENT_CHAINING)
 	e1:SetCondition(s.condition1)
 	e1:SetTarget(s.target1)
 	e1:SetOperation(s.operation1)
@@ -33,22 +33,27 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2_grant)
 end
 function s.filter1(c,tp)
-	local rchk=c:IsRace(RACE_BEAST|RACE_WINGEDBEAST)
-	if c:IsPreviousLocation(LOCATION_MZONE) then rchk=c:IsPreviousRaceOnField(RACE_BEAST|RACE_WINGEDBEAST) end
-	return rchk and c:IsPreviousSetCard(0xb06) and c:IsMonsterCard() and c:IsPreviousLocation(LOCATION_ONFIELD)
-		and c:IsPreviousPosition(POS_FACEUP) and c:IsReason(REASON_EFFECT)
+	return c:IsRace(RACE_BEAST|RACE_WINGEDBEAST) and c:IsSetCard(0xb06) and c:IsType(TYPE_PENDULUM)
+		and c:CheckUniqueOnField(tp) and not c:IsForbidden()
 end
 function s.condition1(e,tp,eg,ep,ev,re,r,rp,chk)
-	return eg:IsExists(s.filter1,1,nil,tp)
+	return rp==tp and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_PENDULUM) and re:GetHandler():IsSetCard(0xb06)
 end
-function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return s.filter1(chkc,tp) and chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) end
 	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,tp) and c:IsAbleToExtra() end
+	local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOEXTRA,c,1,0,0)
+	if g:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE) then
+		Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
+	end
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+	local tc=Duel.GetFirstTarget()
+	if Duel.SendtoExtraP(e:GetHandler(),tp,REASON_EFFECT)>0 and tc:IsRelateToEffect(e) then
+		Duel.BreakEffect()
+		Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 	end
 end
 function s.filter2(c,tp)
