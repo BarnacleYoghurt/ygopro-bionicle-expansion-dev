0c9c09a79daa091a1c141cfaf7d0d2388132a832
utso <cccygodev@gmail.com>
2024-08-24 18:20:39 +0200

    Bog Snake: Move Synchro effect to pend and damage to monster

--- cards-bcor/script/c10100120.lua
+++ cards-bcor/script/c10100120.lua
@@ -3,29 +3,27 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--pendulum summon
 	Pendulum.AddProcedure(c)
-	--Damage
+	--Synchro
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DAMAGE+CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(s.condition1)
 	e1:SetTarget(s.target1)
 	e1:SetOperation(s.operation1)
-	e1:SetCountLimit(1,id)
+	e1:SetCountLimit(1)
 	c:RegisterEffect(e1)
-	--Synchro
+	--Damage
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e2:SetTarget(s.target2)
 	e2:SetOperation(s.operation2)
-	e2:SetCountLimit(1,{id,1})
+	e2:SetCountLimit(1,id)
 	c:RegisterEffect(e2)
 	--To S/T
 	local e3=Effect.CreateEffect(c)
@@ -35,55 +33,34 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_DESTROYED)
 	e3:SetTarget(s.target3)
 	e3:SetOperation(s.operation3)
-	e3:SetCountLimit(1,{id,2})
+	e3:SetCountLimit(1,{id,1})
 	c:RegisterEffect(e3)
 end
-function s.filter1(c,tp)
-	return c:IsSetCard(0xb06) and c:IsControler(tp)
-end
-function s.condition1(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.filter1,1,nil,tp)
-end
-function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetTargetPlayer(1-tp)
-	Duel.SetTargetParam(800)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,800)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
-end
-function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	if Duel.Damage(p,d,REASON_EFFECT)>0 then
-		Duel.Destroy(e:GetHandler(),REASON_EFFECT)
-	end
-end
-function s.filter2a(c)
+function s.filter1a(c)
 	return c:IsFaceup() and c:IsSetCard(0xb06) and c:IsMonsterCard() and c:HasLevel()
 end
-function s.filter2b(c,e,tp,lv)
+function s.filter1b(c,e,tp,lv)
 	return c:IsRace(RACE_REPTILE) and c:IsSetCard(0xb06) and c:IsType(TYPE_SYNCHRO) and c:IsLevelBelow(lv)
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false)
 end
-function s.rescon2(sg,e,tp)
-	return Duel.IsExistingMatchingCard(s.filter2b,tp,LOCATION_EXTRA,0,1,nil,e,tp,sg:GetSum(Card.GetLevel))
+function s.rescon1(sg,e,tp)
+	return Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_EXTRA,0,1,nil,e,tp,sg:GetSum(Card.GetLevel))
 		and Duel.GetMZoneCount(tp,sg)>0
 		and Duel.GetLocationCount(tp,LOCATION_SZONE)+sg:FilterCount(Card.IsLocation,nil,LOCATION_SZONE)>0
 end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return s.filter2a(chkc) and chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(tp) end
-	local g=Duel.GetMatchingGroup(s.filter2a,tp,LOCATION_ONFIELD,0,nil)
-	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon2,0,tp,0,s.rescon2) end
-	g=aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon2,1,tp,HINTMSG_DESTROY,s.rescon2)
-	Duel.SetTargetCard(g)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.filter1a,tp,LOCATION_ONFIELD,0,nil)
+	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon1,0,tp,0,s.rescon1) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_ONFIELD)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-	local dg=Duel.GetTargetCards(e)
-	if Duel.Destroy(dg,REASON_EFFECT)>0 then
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.filter1a,tp,LOCATION_ONFIELD,0,nil)
+	local dg=aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon1,1,tp,HINTMSG_DESTROY,s.rescon1)
+	if #dg>0 and Duel.Destroy(dg,REASON_EFFECT)>0 then
 		local lv=Duel.GetOperatedGroup():GetSum(Card.GetLevel)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sc=Duel.SelectMatchingCard(tp,s.filter2b,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,lv):GetFirst()
+		local sc=Duel.SelectMatchingCard(tp,s.filter1b,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,lv):GetFirst()
 		if sc and Duel.SpecialSummon(sc,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)>0 then
 			sc:CompleteProcedure()
 			Duel.BreakEffect()
@@ -101,6 +78,16 @@ function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 			end
 		end
 	end
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsMonsterCard),tp,LOCATION_SZONE,0,1,nil) end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,300)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local d=Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsMonsterCard),tp,LOCATION_SZONE,0,nil)*300
+	Duel.Damage(p,d,REASON_EFFECT)
 end
 function s.filter3(c,tp)
 	return (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE)) and c:IsRace(RACE_REPTILE) and c:IsSetCard(0xb06)
