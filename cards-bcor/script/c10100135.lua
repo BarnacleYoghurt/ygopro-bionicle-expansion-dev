fd12d3b728b1814a9acfa8816cedc8627e2807b1
utso <cccygodev@gmail.com>
2024-08-24 19:37:17 +0200

    Moa: Implement alternate Pendulum Effect; move previous Pendulum Effect to GY; do not recycle self

--- cards-bcor/script/c10100135.lua
+++ cards-bcor/script/c10100135.lua
@@ -2,25 +2,26 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	Pendulum.AddProcedure(c)
-	--Banish
+	--Tuner
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_DESTROY)
+	e1:SetCategory(CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_PZONE)
 	e1:SetTarget(s.target1)
 	e1:SetOperation(s.operation1)
+	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
-	--Special Summon
+	--Banish
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCategory(CATEGORY_REMOVE)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetTarget(s.target2)
 	e2:SetOperation(s.operation2)
-	e2:SetCountLimit(1,id)
+	e2:SetCountLimit(1,{id,1})
 	c:RegisterEffect(e2)
 	--To Deck
 	local e3=Effect.CreateEffect(c)
@@ -31,44 +32,48 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_REMOVE)
 	e3:SetTarget(s.target3)
 	e3:SetOperation(s.operation3)
-	e3:SetCountLimit(1,id)
+	e3:SetCountLimit(1,{id,1})
 	c:RegisterEffect(e3)
 end
 function s.filter1(c)
-	return c:IsSetCard(0xb06) and c:IsRace(RACE_BEAST|RACE_WINGEDBEAST) and c:IsAbleToRemove()
+	return c:IsFaceup() and c:IsLevelBelow(4) and not c:IsType(TYPE_TUNER)
 end
-function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return s.filter1(chkc) and chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) end
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil) and c:IsAbleToRemove() end
+	Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,c,1,0,0)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 and Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 then
-		Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_ADD_TYPE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(TYPE_TUNER)
+		tc:RegisterEffect(e1)
 	end
+	Duel.Remove(c,POS_FACEUP,REASON_EFFECT)
 end
-function s.filter2(c,e,tp)
-	return c:IsSetCard(0xb06) and c:IsLevelBelow(4) and c:IsRace(RACE_BEAST|RACE_WINGEDBEAST)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.filter2(c)
+	return c:IsSetCard(0xb06) and c:IsRace(RACE_BEAST|RACE_WINGEDBEAST) and c:IsAbleToRemove()
 end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then
-		return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-		if #g>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
 	end
 end
 function s.filter3(c)
-	return c:IsFaceup() and c:IsSetCard(0xb06) and c:IsAbleToDeck()
+	return c:IsFaceup() and c:IsSetCard(0xb06) and c:IsAbleToDeck() and not c:IsCode(id)
 end
 function s.target3(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return s.filter3(chkc) and chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) end
