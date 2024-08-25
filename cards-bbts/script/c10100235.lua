277546467474647558af48d12d1b8aa6182b7898
utso <cccygodev@gmail.com>
2024-08-25 10:50:29 +0200

    Infernavika: Implement burn Pendulum Effect

--- cards-bbts/script/c10100235.lua
+++ cards-bbts/script/c10100235.lua
@@ -2,85 +2,95 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	Pendulum.AddProcedure(c)
-	--Fiyah
+	--Fiyah (battle)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+	e1:SetCode(EVENT_BATTLED)
+	e1:SetCondition(s.condition1)
 	e1:SetTarget(s.target1)
 	e1:SetOperation(s.operation1)
 	e1:SetCountLimit(1)
 	c:RegisterEffect(e1)
-	--Destroy
+	--Fiyah (target)
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_DESTROY)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
-	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetTarget(s.target2)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCondition(s.condition2)
 	e2:SetOperation(s.operation2)
-	e2:SetCountLimit(1,id)
 	c:RegisterEffect(e2)
-	--To ED
+	--Destroy
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_REMOVE)
-	e3:SetCost(s.cost3)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_TO_GRAVE)
 	e3:SetTarget(s.target3)
 	e3:SetOperation(s.operation3)
 	e3:SetCountLimit(1,id)
 	c:RegisterEffect(e3)
+	--To ED
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_REMOVE)
+	e4:SetCost(s.cost4)
+	e4:SetTarget(s.target4)
+	e4:SetOperation(s.operation4)
+	e4:SetCountLimit(1,id)
+	c:RegisterEffect(e4)
+end
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+	local bc=Duel.GetBattleMonster(tp)
+	return bc and bc:IsFaceup() and bc:IsRace(RACE_BEAST|RACE_WINGEDBEAST) and bc:IsSetCard(0xb06)
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
-  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(600)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,600)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-  local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) then
-		local e1 = Effect.CreateEffect(c)
-		e1:SetCategory(CATEGORY_DESTROY)
-		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_BATTLE_START)
-		e1:SetTarget(s.target1_1)
-		e1:SetOperation(s.operation1_1)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
-	end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
 end
-function s.filter1_1(c)
-	return c:IsDestructable() and not c:IsAttribute(ATTRIBUTE_FIRE)
+function s.filter2(c,tp)
+	return c:IsFaceup() and c:IsRace(RACE_BEAST|RACE_WINGEDBEAST) and c:IsSetCard(0xb06)
+		and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
 end
-function s.target1_1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+	if not re or not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return g and g:IsExists(s.filter2,1,nil,tp) and rp==1-tp
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local tc=Duel.GetAttacker()
-	if tc==c then tc=Duel.GetAttackTarget() end
-	local g = Group.FromCards(c,tc)
-	local dg=g:Filter(s.filter1_1,nil)
-	if chk==0 then return tc and dg:GetCount()>0 end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,dg:GetCount(),0,0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCode(EVENT_CHAIN_SOLVING)
+	e1:SetLabelObject(re)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_CHAIN)
+	e1:SetCondition(s.condition2_1)
+	e1:SetOperation(s.operation2_1)
+	c:RegisterEffect(e1)
 end
-function s.operation1_1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetAttacker()
-	if tc==c then tc=Duel.GetAttackTarget() end
-	local g = Group.FromCards(c,tc)
-	local dg=g:Filter(s.filter1_1,nil):Filter(Card.IsRelateToBattle,nil)
-	if dg:GetCount()>0 then 
-		Duel.Destroy(dg,REASON_EFFECT) 
-	end
+function s.condition2_1(e,tp,eg,ep,ev,re,r,rp)
+	return re==e:GetLabelObject()
 end
-function s.filter2(c,e,tp)
+function s.operation2_1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.Damage(1-tp,200,REASON_EFFECT)
+end
+function s.filter3(c,e,tp)
 	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	if chk==0 then
 		return Duel.IsExistingTarget(aux.FaceupFilter(Card.IsSetCard,0xb06),tp,LOCATION_MZONE,0,1,nil)
@@ -91,17 +101,17 @@ function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	g:Merge(Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil))
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
-function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetTargetCards(e)
 	if #g>0 then
 		Duel.Destroy(g,REASON_EFFECT)
 	end
 end
-function s.filter3(c)
+function s.filter4(c)
 	return c:IsFaceup() and c:IsRace(RACE_BEAST|RACE_WINGEDBEAST) and c:IsSetCard(0xb06)
 		and c:IsType(TYPE_PENDULUM) and c:IsAbleToExtra()
 end
-function s.cost3(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cost4(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetDecktopGroup(tp,1)
 	if chk==0 then return g:IsExists(Card.IsAbleToRemoveAsCost,1,nil) end
 	if g:GetCount()>0 then
@@ -109,13 +119,13 @@ function s.cost3(e,tp,eg,ep,ev,re,r,rp,chk)
 		Duel.Remove(g,POS_FACEUP,REASON_COST)
 	end
 end
-function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter3,tp,LOCATION_REMOVED,0,1,nil) end
+function s.target4(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter4,tp,LOCATION_REMOVED,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOEXTRA,nil,1,tp,LOCATION_REMOVED)
 end
-function s.operation3(e,tp,eg,ep,ev,re,r,rp)
+function s.operation4(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3))
-	local g=Duel.SelectMatchingCard(tp,s.filter3,tp,LOCATION_REMOVED,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter4,tp,LOCATION_REMOVED,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoExtraP(g,nil,REASON_EFFECT)
 	end
