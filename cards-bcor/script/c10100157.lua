a6cd29f24aa146b629c1ddc9cea0b9b62287e829
utso <cccygodev@gmail.com>
2024-09-01 11:32:44 +0200

    Mana Ko: remove mistaken disintegration aspect, instead roll an extra banish into tagout (for explosive blasts)

--- cards-bcor/script/c10100157.lua
+++ cards-bcor/script/c10100157.lua
@@ -19,29 +19,18 @@ function s.initial_effect(c)
 	e2:SetTarget(function(e,c) return c~=e:GetHandler() end)
 	e2:SetValue(aux.indoval)
 	c:RegisterEffect(e2)
-	--Reduce ATK/DEF
-	local e3a=Effect.CreateEffect(c)
-	e3a:SetType(EFFECT_TYPE_FIELD)
-	e3a:SetRange(LOCATION_MZONE)
-	e3a:SetTargetRange(0,LOCATION_MZONE)
-	e3a:SetCode(EFFECT_UPDATE_ATTACK)
-	e3a:SetValue(-2000)
-	c:RegisterEffect(e3a)
-	local e3b=e3a:Clone()
-	e3b:SetCode(EFFECT_UPDATE_DEFENSE)
-	c:RegisterEffect(e3b)
-	--Special Summon
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e4:SetType(EFFECT_TYPE_QUICK_O)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCode(EVENT_FREE_CHAIN)
-	e4:SetCost(s.cost4)
-	e4:SetTarget(s.target4)
-	e4:SetOperation(s.operation4)
-	e4:SetCountLimit(1,id)
-	c:RegisterEffect(e4)
+	--Special Summon and banish
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetCost(s.cost4)
+	e3:SetTarget(s.target4)
+	e3:SetOperation(s.operation4)
+	e3:SetCountLimit(1,id)
+	c:RegisterEffect(e3)
 end
 function s.filter4(c,e,tp)
 	return c:IsSetCard(0xb06) and c:IsRace(RACE_FISH|RACE_SEASERPENT|RACE_AQUA) and c:IsLevelBelow(10)
@@ -68,8 +57,13 @@ function s.operation4(e,tp,eg,ep,ev,re,r,rp)
 		if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then lc=1 end
 		local rg=Duel.GetMatchingGroup(s.filter4,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil,e,tp)
 		local g=aux.SelectUnselectGroup(rg,e,tp,1,lc,s.rescon4,1,tp,HINTMSG_SPSUMMON)
-		if #g>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
+			Duel.Hint(tp,HINT_SELECTMSG,HINTMSG_REMOVE)
+			local bg=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+			if #bg>0 then
+				Duel.HintSelection(bg,true)
+				Duel.Remove(bg,POS_FACEUP,REASON_EFFECT)
+			end
 		end
 	end
 end
