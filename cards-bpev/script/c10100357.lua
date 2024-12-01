dce27adc219b98cf753629c769b7841ab9fe847e
utso <cccygodev@gmail.com>
2024-12-01 12:53:09 +0100

    Silver Shield: Target Krana, return to hand OR attach

--- cards-bpev/script/c10100357.lua
+++ cards-bpev/script/c10100357.lua
@@ -1,8 +1,9 @@
 --Bohrok Silver Shield
 local s,id=GetID()
 function s.initial_effect(c)
-    --Attach
+    --Add or attach
     local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOHAND)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e1:SetCode(EVENT_FREE_CHAIN)
@@ -22,27 +23,37 @@ function s.initial_effect(c)
     e2:SetCountLimit(1,{id,1})
     c:RegisterEffect(e2)
 end
-function s.filter1a(c,e,tp)
-    return c:IsFaceup() and c:IsSetCard(0xb08) and c:IsType(TYPE_XYZ)
-        and Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,c)
+function s.filter1a(c,tp)
+    return c:IsSetCard(0xb09) and c:IsMonster() and (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup())
+        and (c:IsAbleToHand() or Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_MZONE,0,1,nil,c))
 end
 function s.filter1b(c,tc)
-    return c:IsSetCard(0xb09) and c:IsMonster()
+    return c:IsFaceup() and c:IsSetCard(0xb08) and c:IsType(TYPE_XYZ)
         and c~=tc -- you know, for the totally real Bohrok/Krana hybrid
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return s.filter1a(chkc,e,tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) end
-    if chk==0 then return Duel.IsExistingTarget(s.filter1a,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+    if chkc then return s.filter1a(chkc,tp) and chkc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE) and chkc:IsControler(tp) end
+    if chk==0 then return Duel.IsExistingTarget(s.filter1a,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,tp) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-    Duel.SelectTarget(tp,s.filter1a,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+    local g=Duel.SelectTarget(tp,s.filter1a,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil,tp)
+    if g:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE) then
+        Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
+    end
+    Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if tc:IsRelateToEffect(e) then
-        local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter1b),tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil,tc)
-        if #g>0 then
-            Duel.Overlay(tc,g,true)
-        end
+        aux.ToHandOrElse(tc,tp,
+            function(c) return Duel.IsExistingMatchingCard(s.filter1b,tp,LOCATION_MZONE,0,1,nil,c) end,
+            function (tc)
+                local g=Duel.SelectMatchingCard(tp,s.filter1b,tp,LOCATION_MZONE,0,1,1,nil,tc)
+                if #g>0 then
+                    Duel.Overlay(g:GetFirst(),tc,true)
+                end
+            end,
+            aux.Stringid(id,2)
+        )
     end
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
