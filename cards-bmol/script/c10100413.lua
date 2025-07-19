b9178989a79e0afd16bed099831d0381dc288858
utso <cccygodev@gmail.com>
2025-07-19 20:38:41 +0200

    Purity: Swap target pools, relax HOPT, negate any card, optional destruction

--- cards-bmol/script/c10100413.lua
+++ cards-bmol/script/c10100413.lua
@@ -21,23 +21,23 @@ function s.initial_effect(c)
     e2:SetCondition(s.condition2)
     e2:SetCost(aux.bfgcost)
     e2:SetOperation(s.operation2)
-    e2:SetCountLimit(1,id)
+    e2:SetCountLimit(1,{id,1})
     c:RegisterEffect(e2)
 end
 function s.filter1(c)
-    return (c:IsAttribute(ATTRIBUTE_WATER) or c:IsRace(RACE_WARRIOR)) and c:IsNegatableMonster()
+    return (c:IsSetCard(0xb01) or c:IsSetCard(0xb02) or c:IsSetCard(0xb03)) and c:IsNegatableMonster()
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return false end
     if chk==0 then
         return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil)
-           and Duel.IsExistingTarget(Card.IsNegatableMonster,tp,0,LOCATION_MZONE,1,nil)
+           and Duel.IsExistingTarget(Card.IsNegatable,tp,0,LOCATION_ONFIELD,1,nil)
     end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
     local g1=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
-    local g2=Duel.SelectTarget(tp,Card.IsNegatableMonster,tp,0,LOCATION_MZONE,1,1,nil)
-    e:SetLabelObject(g2:GetFirst()) --"that opponent's monster"
+    local g2=Duel.SelectTarget(tp,Card.IsNegatable,tp,0,LOCATION_ONFIELD,1,1,nil)
+    e:SetLabelObject(g2:GetFirst()) --"that opponent's card"
 end
 function s.operation1(e,tp,eg,ep,ev,re,r,rp)
     local g=Duel.GetTargetCards(e):Match(Card.IsFaceup,nil):Match(Card.IsCanBeDisabledByEffect,nil,e)
@@ -45,7 +45,7 @@ function s.operation1(e,tp,eg,ep,ev,re,r,rp)
         g:ForEach(Card.NegateEffects,e:GetHandler(),RESET_PHASE|PHASE_END)
         if Duel.GetMatchingGroup(Card.IsMonster,tp,LOCATION_GRAVE,0,nil):GetClassCount(Card.GetAttribute)==1 then
             local tc=e:GetLabelObject()
-            if tc:IsRelateToEffect(e) then
+            if tc:IsRelateToEffect(e) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
                 Duel.Destroy(tc,REASON_EFFECT)
             end
         end
@@ -53,8 +53,8 @@ function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.condition2(e,tp,eg,ep,ev,re,r,rp)
     local bc=Duel.GetBattleMonster(tp)
-    return bc and bc:IsFaceup() and bc==Duel.GetAttacker() and bc:CanChainAttack(0)
-        and (bc:IsSetCard(0xb01) or bc:IsSetCard(0xb02) or bc:IsSetCard(0xb03))
+    return bc and bc:IsFaceup() and bc==Duel.GetAttacker() and bc:CanChainAttack()
+        and (bc:IsAttribute(ATTRIBUTE_WATER) or bc:IsRace(RACE_WARRIOR))
 end
 function s.operation2(e,tp,eg,ep,ev,re,r,rp)
     local bc=Duel.GetBattleMonster(tp)
