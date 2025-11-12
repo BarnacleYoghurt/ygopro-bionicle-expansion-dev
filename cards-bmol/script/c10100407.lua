56dee72692234f2f3607a231958f2986d0f8a76a
utso <cccygodev@gmail.com>
2025-11-12 20:48:50 +0100

    Implement ASS, Graalok

---
+++ cards-bmol/script/c10100407.lua
@@ -0,0 +1,98 @@
+Duel.LoadScript("util-bmol.lua")
+--A Seventh Star
+local s,id=GetID()
+local COUNTER_SEVEN=0xb03
+function s.initial_effect(c)
+    c:EnableCounterPermit(COUNTER_SEVEN)
+    --Special Summon
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetTarget(s.target1)
+    e1:SetOperation(s.operation1)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+    c:RegisterEffect(e1)
+    --Place counters
+    local e2a=Effect.CreateEffect(c)
+    e2a:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_CONTINUOUS)
+    e2a:SetRange(LOCATION_SZONE)
+    e2a:SetCode(EVENT_SUMMON_SUCCESS)
+    e2a:SetOperation(s.operation2)
+    c:RegisterEffect(e2a)
+    local e2b=e2a:Clone()
+    e2b:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2b)
+    --Draw
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_DRAW)
+    e3:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_TRIGGER_O)
+    e3:SetRange(LOCATION_SZONE)
+    e3:SetCode(EVENT_PHASE|PHASE_END)
+    e3:SetCondition(s.condition3)
+    e3:SetCost(Cost.SelfToGrave)
+    e3:SetTarget(s.target3)
+    e3:SetOperation(s.operation3)
+    c:RegisterEffect(e3)
+end
+s.listed_names={CARD_AVOHKII}
+function s.filter1(c,e,tp)
+    return c:IsSetCard(SET_AVOHKII) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return s.filter1(chkc,e,tp) end
+    if chk==0 then return true end
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingTarget(s.filter1,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+        and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+        e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+        e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+        Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+    else
+        e:SetCategory(0)
+        e:SetProperty(0)
+    end
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+        tc:NegateEffects(e:GetHandler())
+    end
+    Duel.SpecialSummonComplete()
+end
+function s.filter2a(c,tp)
+    return c:IsFaceup() and c:IsControler(tp) and c:ListsCode(CARD_AVOHKII)
+end
+function s.filter2b(c)
+    --Garbage Collector uses IsAbleToHandAsCost to check "began the Duel", but that can have false positives here ...
+    return c:IsType(TYPE_FUSION) or c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_XYZ) or c:IsType(TYPE_LINK)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+    local g=eg:Filter(s.filter2a,nil,tp)
+    local ct=(g:IsExists(Card.IsRace,1,nil,RACE_WARRIOR) and 1 or 0)
+            +(g:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_LIGHT) and 1 or 0)
+            +(g:IsExists(Card.IsLevelAbove,1,nil,7) and 1 or 0)
+            +(g:IsExists(s.filter2b,1,nil) and 1 or 0)
+    if ct>0 then
+        e:GetHandler():AddCounter(COUNTER_SEVEN,ct)
+    end
+end
+function s.condition3(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():GetCounter(COUNTER_SEVEN)>=7
+end
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+    local h=Duel.GetFieldGroupCount(tp,LOCATION_HAND|LOCATION_ONFIELD,0)
+    if e:GetHandler():IsLocation(LOCATION_SZONE) then h=h-1 end --not sent for cost yet
+    if chk==0 then return h<7 and Duel.IsPlayerCanDraw(tp,7-h) end
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(7-h)
+    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,7-h)
+end
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
+    local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+    local h=Duel.GetFieldGroupCount(p,LOCATION_HAND|LOCATION_ONFIELD,0)
+    if h<7 then
+        Duel.Draw(p,7-h,REASON_EFFECT)
+    end
+end
---
+++ cards-bmol/script/c10100429.lua
@@ -0,0 +1,98 @@
+Duel.LoadScript("util-bmol.lua")
+--Graalok, Ash Bear Rahi
+local s,id=GetID()
+function s.initial_effect(c)
+    --Special Summon (Avohkii)
+    local e1a=Effect.CreateEffect(c)
+    e1a:SetDescription(aux.Stringid(id,0))
+    e1a:SetCategory(CATEGORY_DESTROY|CATEGORY_SPECIAL_SUMMON)
+    e1a:SetType(EFFECT_TYPE_QUICK_O)
+    e1a:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1a:SetRange(LOCATION_HAND|LOCATION_GRAVE)
+    e1a:SetCode(EVENT_FREE_CHAIN)
+    e1a:SetCondition(function() return Duel.IsMainPhase() end)
+    e1a:SetCost(s.cost1a)
+    e1a:SetTarget(s.target1)
+    e1a:SetOperation(s.operation1)
+    e1a:SetHintTiming(TIMING_MAIN_END)
+    e1a:SetCountLimit(1,id)
+    c:RegisterEffect(e1a)
+    --Special Summon (Rahi)
+    local e1b=e1a:Clone()
+    e1b:SetDescription(aux.Stringid(id,1))
+    e1b:SetCost(s.cost1b)
+    c:RegisterEffect(e1b)
+    --To hand
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,2))
+    e2:SetCategory(CATEGORY_TOHAND)
+    e2:SetType(EFFECT_TYPE_SINGLE|EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_DAMAGE_STEP_END)
+    e2:SetTarget(s.target2)
+    e2:SetOperation(s.operation2)
+    c:RegisterEffect(e2)
+end
+s.listed_names={CARD_AVOHKII}
+function s.filter1a(c)
+    return c:IsCode(CARD_AVOHKII) and not c:IsPublic()
+end
+function s.filter1b(c)
+    return c:IsRace(RACE_BEAST|RACE_WINGEDBEAST) and c:IsSetCard(0xb06) and c:IsType(TYPE_PENDULUM)
+end
+function s.filter1c(c,tp)
+    return c:IsFaceup() and Duel.GetMZoneCount(c:GetControler(),c,tp)>0
+end
+function s.cost1a(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.filter1a,tp,LOCATION_HAND,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+    local g=Duel.SelectMatchingCard(tp,s.filter1a,tp,LOCATION_HAND,0,1,1,nil)
+    Duel.ConfirmCards(1-tp,g)
+end
+function s.cost1b(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.filter1b,1,true,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+    local g=Duel.SelectReleaseGroupCost(tp,s.filter1b,1,1,true,nil)
+    Duel.Release(g,REASON_COST)
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return s.filter1c(chkc,tp) end
+    local c=e:GetHandler()
+    if chk==0 then
+        return Duel.IsExistingTarget(s.filter1c,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil,tp)
+            and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+    end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,s.filter1c,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil,tp)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        local p=tc:GetControler()
+        if Duel.Destroy(tc,REASON_EFFECT)>0 and c:IsRelateToEffect(e) and Duel.GetLocationCount(p,LOCATION_MZONE)>0 then
+            if Duel.SpecialSummonStep(c,0,tp,p,false,false,POS_FACEUP_ATTACK) and c:IsPreviousLocation(LOCATION_GRAVE) then
+                local e1=Effect.CreateEffect(c)
+                e1:SetDescription(3301)
+                e1:SetType(EFFECT_TYPE_SINGLE)
+                e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE|EFFECT_FLAG_CLIENT_HINT)
+                e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+                e1:SetValue(LOCATION_DECKBOT)
+                e1:SetReset(RESET_EVENT|RESETS_REDIRECT)
+                c:RegisterEffect(e1)
+            end
+            Duel.SpecialSummonComplete()
+        end
+    end
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.SendtoHand(c,nil,REASON_EFFECT)
+    end
+end
