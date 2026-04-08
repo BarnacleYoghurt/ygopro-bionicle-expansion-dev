4596f68322151afc248c02993785967006d39df5
utso <cccygodev@gmail.com>
2026-04-08 20:57:14 +0200

    Krana Ca: protect *each* Bohrok once

--- cards-bbts/script/c10100212.lua
+++ cards-bbts/script/c10100212.lua
@@ -14,17 +14,10 @@ function s.initial_effect(c)
 	e2:SetTargetRange(LOCATION_MZONE,0)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
 	e2:SetCondition(bbts.krana_condition_equipped)
-	e2:SetTarget(s.target2)
-	e2:SetValue(s.value2)
-	e2:SetCountLimit(1)
+	e2:SetTarget(function(e,c) return c:IsSetCard(0xb08) end)
+	e2:SetValue(function(e,re,r,rp) return (r&REASON_BATTLE)~=0 end)
 	c:RegisterEffect(e2)
 	--Summon
 	local e3=bbts.krana_summon(c)
 	c:RegisterEffect(e3)
-end
-function s.target2(e,c)
-	return c:IsFaceup() and c:IsSetCard(0xb08)
-end
-function s.value2(e,re,r,rp)
-	return bit.band(r,REASON_BATTLE)~=0
 end
